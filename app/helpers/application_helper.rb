1bb79d3a93a7b17db637c90defe6f96b18b78a25
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-02-11 20:41:39 +0000

    RedCloth, minor view work.
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@21 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def format_html(*args)
+    str = RedCloth.new(args.join(' '))
+    str.filter_html = true
+    str.filter_styles = true
+    str.filter_classes = true
+    str.filter_ids = true
+    str.to_html
+  end
+
 end
