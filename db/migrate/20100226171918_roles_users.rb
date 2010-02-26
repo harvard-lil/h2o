e4899b00c16b82249987def5dc800c04ee739b69
jcallina <jcallina@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-02-26 17:31:48 +0000

    Role_users migration for ACL9
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@43 ac1452a4-9358-4c15-a3b0-de1119e254d8

---
+++ db/migrate/20100226171918_roles_users.rb
@@ -0,0 +1,13 @@
+class RolesUsers < ActiveRecord::Migration
+  def self.up
+    create_table :roles_users, :id => false, :force => true do |t|
+      t.integer  "user_id"
+      t.integer  "role_id"
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :roles_users
+  end
+end
