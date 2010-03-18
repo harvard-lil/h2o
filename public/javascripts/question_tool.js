4c0e831cc773df55c97f998cde20b56e76d70422
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-03-18 21:09:28 +0000

    Replies now include stateful container visibility status!
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@63 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- public/javascripts/question_tool.js
+++ public/javascripts/question_tool.js
@@ -1,47 +1,92 @@
 /* goo-goo gajoob */
 jQuery(function(){
     jQuery.extend({
+      showReplyContainer: function(questionId,repliesContainer,toggleSpeed){
+        jQuery.ajax({
+          type: 'GET',
+          url: jQuery.rootPath() + 'questions/replies/' + questionId,
+          beforeSend: function(){jQuery('#spinner_block').show()},
+          success: function(html){
+            jQuery('#spinner_block').hide();
+            repliesContainer.html(html).toggle(toggleSpeed);
+            //We only want to observe the new replies, so set a root selector to ensure we aren't double-observing.
+            jQuery.observeNewQuestionControl('#replies-for-question-' + questionId);
+          },
+          error: function(xhr){
+            jQuery('#spinner_block').hide();
+            jQuery('div.ajax-error').show().append(xhr.responseText);
+          }
+        });
+      },
+      serializeHash: function(hashVals){
+        var vals = [];
+        for(var val in hashVals){
+          if(val != undefined){
+            vals.push(val);
+          }
+        }
+        return vals.join(',');
+      },
+      unserializeHash: function(stringVal){
+        if(stringVal && stringVal != undefined){
+          var hashVals = [];
+          var arrayVals = stringVal.split(',');
+          for(var i in arrayVals){
+            hashVals[arrayVals[i]]=1;
+          }
+          return hashVals;
+        } else {
+          return new Array();
+        }
+      },
+      removeReplyContainerFromCookie: function(cookieName,replyContainer){
+        var currentVals = jQuery.unserializeHash(jQuery.cookie(cookieName));
+        delete currentVals[replyContainer];
+        var cookieVal = jQuery.serializeHash(currentVals);
+        jQuery.cookie(cookieName,cookieVal);
+      },
+      addReplyContainerToCookie: function(cookieName,replyContainer){
+        var currentVals = jQuery.unserializeHash(jQuery.cookie(cookieName));
+        currentVals[replyContainer] = 1;
+        var cookieVal = jQuery.serializeHash(currentVals);
+        jQuery.cookie(cookieName,cookieVal);
+      },
       observeShowReplyControls: function(){
-        jQuery('a.show-replies').click(function(e){
+        jQuery('a.show-replies').each(function(){
+          var questionId = jQuery(this).attr('id').split('-')[3];
+          var repliesContainer = jQuery('#replies-container-' + questionId);
+          var openReplyContainers = jQuery.unserializeHash(jQuery.cookie('show-reply-containers'));
+          jQuery(this).click(function(e){
             e.preventDefault();
-            var questionId = jQuery(this).attr('id').split('-')[3];
-            var repliesContainer = jQuery('#replies-container-' + questionId);
             if(repliesContainer.html().length > 0 && repliesContainer.is(':visible')){
               //There's content in here and it's visible. Just hide it.
               repliesContainer.toggle('fast');
+              jQuery.removeReplyContainerFromCookie('show-reply-containers','#replies-container-' + questionId);
             } else {
               //There's no content, or there's content and it's invisible. 
               //Get the replies again to ensure fresh content.
-              jQuery.ajax({
-                type: 'GET',
-                url: jQuery.rootPath() + 'questions/replies/' + questionId,
-                beforeSend: function(){jQuery('#spinner_block').show()},
-                success: function(html){
-                  jQuery('#spinner_block').hide();
-                  repliesContainer.html(html).toggle('fast');
-                  jQuery.observeNewQuestionControl();
-                },
-                error: function(xhr){
-                  jQuery('#spinner_block').hide();
-                  jQuery('div.ajax-error').show().append(xhr.responseText);
-                }
-              });
+              jQuery.addReplyContainerToCookie('show-reply-containers','#replies-container-' + questionId);
+              jQuery.showReplyContainer(questionId,repliesContainer,'fast');
             }
           });
+          if(openReplyContainers['#replies-container-' + questionId] == 1){
+            jQuery.showReplyContainer(questionId,repliesContainer,0);
+          }
+        });
       },
-
-      observeNewQuestionControl: function(){
-        jQuery('a.new-question-for').each(function(){
-          var questionInstanceId = jQuery(this).attr('id').split('-')[3];
-          var questionId = jQuery(this).attr('id').split('-')[4];
+      observeNewQuestionControl: function(rootSelector){
+        jQuery(((rootSelector) ? rootSelector + ' ' : '') + 'a.new-question-for').each(function(){
           jQuery(this).click(function(e){
+            var questionInstanceId = jQuery(this).attr('id').split('-')[3];
+            var questionId = jQuery(this).attr('id').split('-')[4];
+            var dialogTitle = 'Add to the discussion';
             jQuery('#new-question-form').dialog({
               bgiframe: true,
               autoOpen: false,
               minWidth: 300,
               width: 450,
               modal: true,
-              title: ((questionId == 0) ? 'Add a question' : 'Add a reply'),
+              title: dialogTitle,
               buttons: {
                 'Ask Question': function(){
                   jQuery('#new-question-form form').ajaxSubmit({
@@ -85,7 +130,8 @@ jQuery(function(){
       },
 
       observeVoteControls: function() {
-        jQuery("a[id*='vote-for']").click(function(){
+        jQuery("a[id*='vote-for']").click(function(e){
+          e.preventDefault();
           var questionInstanceId = jQuery(this).attr('id').split('-')[2];
           var questionId = jQuery(this).attr('id').split('-')[3];
           jQuery.ajax({
