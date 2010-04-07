4f0710634e5d58b031ff33c3f03b56ef562170be
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-04-07 03:08:06 +0000

    Refactor javascript for efficiency and to make it easier to layer in admin controls.
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@94 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- public/javascripts/question_tool.js
+++ public/javascripts/question_tool.js
@@ -1,7 +1,7 @@
 /* goo-goo gajoob */
 jQuery(function(){
     jQuery.extend({
-      showReplyContainer: function(questionId,repliesContainer,toggleSpeed){
+      showReplyContainer: function(questionInstanceId,questionId,repliesContainer,toggleSpeed){
         jQuery.ajax({
           type: 'GET',
           url: jQuery.rootPath() + 'questions/replies/' + questionId,
@@ -9,8 +9,8 @@ jQuery(function(){
           success: function(html){
             jQuery('#spinner_block').hide();
             repliesContainer.html(html).toggle(toggleSpeed);
-            //We only want to observe the new replies, so set a root selector to ensure we aren't double-observing.
-            jQuery.observeNewQuestionControl('#replies-for-question-' + questionId);
+            //We only want to observe the new replies.
+            jQuery.observeNewQuestionControl(jQuery('#replies-for-question-' + questionId), questionInstanceId, questionId);
           },
           error: function(xhr){
             jQuery('#spinner_block').hide();
@@ -30,86 +30,59 @@ jQuery(function(){
         var cookieVal = jQuery.serializeHash(currentVals);
         jQuery.cookie(cookieName,cookieVal);
       },
-      observeShowReplyControls: function(){
-        jQuery('a.show-replies').each(function(){
-          var questionId = jQuery(this).attr('id').split('-')[3];
-          var repliesContainer = jQuery('#replies-container-' + questionId);
-          var openReplyContainers = jQuery.unserializeHash(jQuery.cookie('show-reply-containers'));
-          jQuery(this).click(function(e){
-            e.preventDefault();
-            if(repliesContainer.html().length > 0 && repliesContainer.is(':visible')){
-              //There's content in here and it's visible. Just hide it.
-              repliesContainer.toggle('fast');
-              jQuery.removeReplyContainerFromCookie('show-reply-containers','#replies-container-' + questionId);
-            } else {
-              //There's no content, or there's content and it's invisible. 
-              //Get the replies again to ensure fresh content.
-              jQuery.addReplyContainerToCookie('show-reply-containers','#replies-container-' + questionId);
-              jQuery.showReplyContainer(questionId,repliesContainer,'fast');
+      observeNewQuestionControl: function(element,questionInstanceId,questionId){
+        jQuery(element).find('a.new-question-for').click(function(e){
+          var interiorQuestionId = jQuery(this).attr('id').split('-')[4];
+          var dialogTitle = 'Add to the discussion';
+          jQuery('#new-question-form').dialog({
+            bgiframe: true,
+            autoOpen: false,
+            minWidth: 300,
+            width: 450,
+            modal: true,
+            title: dialogTitle,
+            buttons: {
+              'Ask Question': function(){
+                  jQuery('#new-question-form form').ajaxSubmit({
+                    error: function(xhr){
+                      jQuery('#spinner_block').hide();
+                      jQuery('#new-question-error').show().append(xhr.responseText);
+                    },
+                    beforeSend: function(){
+                      jQuery('#spinner_block').show();
+                      jQuery('#new-question-error').html('').hide();
+                    },
+                    success: function(responseText){
+                      jQuery('#spinner_block').hide();
+                      //TODO - resolve the "parent" question id and return it in response text.
+                      jQuery.updateQuestionInstanceView(questionInstanceId,responseText)
+                      jQuery('#new-question-form').dialog('close');
+                    }
+                  });
+              },
+              'Cancel': function(){
+                jQuery('#new-question-error').html('').hide();
+                jQuery(this).dialog('close');
+              }
             }
           });
-          if(openReplyContainers['#replies-container-' + questionId] == 1){
-            jQuery.showReplyContainer(questionId,repliesContainer,0);
-          }
-        });
-      },
-      observeNewQuestionControl: function(rootSelector){
-        jQuery(((rootSelector) ? rootSelector + ' ' : '') + 'a.new-question-for').each(function(){
-          jQuery(this).click(function(e){
-            var questionInstanceId = jQuery(this).attr('id').split('-')[3];
-            var questionId = jQuery(this).attr('id').split('-')[4];
-            var dialogTitle = 'Add to the discussion';
-            jQuery('#new-question-form').dialog({
-              bgiframe: true,
-              autoOpen: false,
-              minWidth: 300,
-              width: 450,
-              modal: true,
-              title: dialogTitle,
-              buttons: {
-                'Ask Question': function(){
-                  jQuery('#new-question-form form').ajaxSubmit({
-                      error: function(xhr){
-                        jQuery('#spinner_block').hide();
-                        jQuery('#new-question-error').show().append(xhr.responseText);
-                      },
-                      beforeSend: function(){
-                        jQuery('#spinner_block').show();
-                        jQuery('#new-question-error').html('').hide();
-                      },
-                      success: function(responseText){
-                        jQuery('#spinner_block').hide();
-                        //TODO - resolve the "parent" question id and return it in response text.
-                        jQuery.updateQuestionInstanceView(questionInstanceId,responseText)
-                        jQuery('#new-question-form').dialog('close');
-                      }
-                    });
-                },
-                'Cancel': function(){
-                  jQuery('#new-question-error').html('').hide();
-                  jQuery(this).dialog('close');
-                }
-              }
-            });
-            e.preventDefault();
-            jQuery.ajax({
-              type: 'GET',
-              url: jQuery.rootPath() + 'questions/new',
-              data: {'question[question_instance_id]': questionInstanceId, 'question[parent_id]': questionId},
-              beforeSend: function(){
-                jQuery('#spinner_block').show();
-              },
-              success: function(html){
-                jQuery('#spinner_block').hide();
-                jQuery('#new-question-form').html(html);
-                jQuery('#new-question-form').dialog('open');
-              }
-            });
+          e.preventDefault();
+          jQuery.ajax({
+            type: 'GET',
+            url: jQuery.rootPath() + 'questions/new',
+            data: {'question[question_instance_id]': questionInstanceId, 'question[parent_id]': interiorQuestionId},
+            beforeSend: function(){
+              jQuery('#spinner_block').show();
+            },
+            success: function(html){
+              jQuery('#spinner_block').hide();
+              jQuery('#new-question-form').html(html);
+              jQuery('#new-question-form').dialog('open');
+            }
           });
         });
       },
-
-      observeVoteControls: function() {
+      toggleVoteControls: function(){
         // Do an ajax request to get the votes this user has submitted. Kick ass-feature (if you get the reference,
         // you win a cookie).
         // This  allows us to cache on the question-question-instance level, instead of on the
@@ -123,40 +96,36 @@ jQuery(function(){
           success: function(html){
             var votes = html;
             for(i in votes){
-              jQuery("#votes-for-" + i + ' a').each(function(){
-                jQuery(this).attr('class','already-voted'); 
-              });
+              jQuery("#votes-for-" + i + ' a').addClass('already-voted');
             }
           }
         });
-        jQuery("a[id*='vote-']").each(function(){
-          var for_or_against = jQuery(this).attr('id').split('-')[1];
-          var questionInstanceId = jQuery(this).attr('id').split('-')[2];
-          var questionId = jQuery(this).attr('id').split('-')[3];
+      },
 
-          jQuery(this).click(function(e){
-            e.preventDefault();
-            if(jQuery(this).hasClass('already-voted')){
-              return;
+      observeVoteControls: function(element,questionInstanceId,questionId) {
+        jQuery(element).find("a[id*='vote-']").click(function(e){
+          e.preventDefault();
+          var for_or_against = jQuery(this).attr('id').split('-')[1];
+          if(jQuery(this).hasClass('already-voted')){
+            return;
+          }
+          var dispatch_url = (for_or_against == 'for') ? jQuery.rootPath() + 'questions/vote_for' : jQuery.rootPath() + 'questions/vote_against';
+          jQuery.ajax({
+            type: 'POST',
+            url: dispatch_url,
+            data: {question_id: questionId, authenticity_token: AUTH_TOKEN},
+            beforeSend: function(){
+              jQuery('#spinner_block').show();
+              jQuery('#ajax-error-' + questionInstanceId).html('').hide();
+            },
+            error: function(xhr){
+              jQuery('#spinner_block').hide();
+              jQuery('#ajax-error-' + questionInstanceId).show().append(xhr.responseText);
+            },
+            success: function(){
+              jQuery('#spinner_block').hide();
+              jQuery.updateQuestionInstanceView(questionInstanceId,questionId);
             }
-            var dispatch_url = (for_or_against == 'for') ? jQuery.rootPath() + 'questions/vote_for' : jQuery.rootPath() + 'questions/vote_against';
-            jQuery.ajax({
-              type: 'POST',
-              url: dispatch_url,
-              data: {question_id: questionId, authenticity_token: AUTH_TOKEN},
-              beforeSend: function(){
-                jQuery('#spinner_block').show();
-                jQuery('#ajax-error-' + questionInstanceId).html('').hide();
-              },
-              error: function(xhr){
-                jQuery('#spinner_block').hide();
-                jQuery('#ajax-error-' + questionInstanceId).show().append(xhr.responseText);
-              },
-              success: function(){
-                jQuery('#spinner_block').hide();
-                jQuery.updateQuestionInstanceView(questionInstanceId,questionId);
-              }
-            });
           });
         });
       },
@@ -167,9 +136,7 @@ jQuery(function(){
           data: {updated_question_id: questionId},
           success: function(html){
             jQuery('#questions-' + questionInstanceId).html(html); 
-            jQuery.observeVoteControls();
-            jQuery.observeNewQuestionControl();
-            jQuery.observeShowReplyControls();
+            jQuery.observeQuestionControls();
             if(questionId.length > 0){
               jQuery('#question-' + questionId).stop().css("background-color", "#FFFF9C").animate({ backgroundColor: "#FFFFFF"}, 2000);
             }
@@ -209,75 +176,106 @@ jQuery(function(){
         });
       },
       observeQuestionInstanceControl: function(){
-        jQuery('a.question-instance-control').each(function(){
-          jQuery(this).click(function(e){
-            jQuery('#question-instance-form-container').dialog({
-              bgiframe: true,
-              autoOpen: false,
-              minWidth: 300,
-              width: 450,
-              modal: true,
-              title: 'Question Tool',
-              buttons: {
-                'Save Question Tool': function(){
-                  jQuery('#question-instance-form').ajaxSubmit({
-                      error: function(xhr){
-                        jQuery('#spinner_block').hide();
-                        jQuery('#question-instance-error').show().append(xhr.responseText);
-                      },
+        jQuery('a.question-instance-control').click(function(e){
+          jQuery('#question-instance-form-container').dialog({
+            bgiframe: true,
+            autoOpen: false,
+            minWidth: 300,
+            width: 450,
+            modal: true,
+            title: 'Question Tool',
+            buttons: {
+              'Save Question Tool': function(){
+                jQuery('#question-instance-form').ajaxSubmit({
+                  error: function(xhr){
+                    jQuery('#spinner_block').hide();
+                    jQuery('#question-instance-error').show().append(xhr.responseText);
+                  },
+                  beforeSend: function(){
+                    jQuery('#spinner_block').show();
+                    jQuery('#question-instance-error').html('').hide();
+                  },
+                  success: function(responseText){
+                    jQuery('#spinner_block').hide();
+                    jQuery.ajax({
+                      type: 'GET',
+                      url: jQuery.rootPath() + 'question_instances',
                       beforeSend: function(){
                         jQuery('#spinner_block').show();
-                        jQuery('#question-instance-error').html('').hide();
                       },
-                      success: function(responseText){
+                      success: function(html){
                         jQuery('#spinner_block').hide();
-                        jQuery.ajax({
-                          type: 'GET',
-                          url: jQuery.rootPath() + 'question_instances',
-                          beforeSend: function(){
-                            jQuery('#spinner_block').show();
-                          },
-                          success: function(html){
-                            jQuery('#spinner_block').hide();
-                            jQuery('#question-instance-list').html(html);
-                            jQuery("#question-instance-chooser").tablesorter();
-                            jQuery.observeQuestionInstanceControl();
-                          }
-                        });
-                        jQuery('#question-instance-form-container').dialog('close');
+                        jQuery('#question-instance-list').html(html);
+                        jQuery("#question-instance-chooser").tablesorter();
+                        jQuery.observeQuestionInstanceControl();
                       }
                     });
-                },
-                'Cancel': function(){
-                  jQuery('#question-instance-error').html('').hide();
-                  jQuery(this).dialog('close');
-                }
-              }
-            });
-            e.preventDefault();
-            var dispatchUrl = '';
-            if(jQuery(this).attr('id').match(/^edit\-question\-instance/) ){
-              var question_instance_id = jQuery(this).attr('id').split('-')[3];
-              dispatchUrl = jQuery.rootPath() + 'question_instances/' + question_instance_id + '/edit'
-            } else {
-              dispatchUrl = jQuery.rootPath() + 'question_instances/new'
-            }
-            jQuery.ajax({
-              type: 'GET',
-              url: dispatchUrl,
-              beforeSend: function(){
-                jQuery('#spinner_block').show();
+                    jQuery('#question-instance-form-container').dialog('close');
+                  }
+                });
               },
-              success: function(html){
-                jQuery('#spinner_block').hide();
-                jQuery('#question-instance-form-container').html(html);
-                jQuery('#question-instance-form-container').dialog('open');
+              'Cancel': function(){
+                jQuery('#question-instance-error').html('').hide();
+                jQuery(this).dialog('close');
               }
-            });
+            }
+          });
+          e.preventDefault();
+          var dispatchUrl = '';
+          if(jQuery(this).attr('id').match(/^edit\-question\-instance/) ){
+            var question_instance_id = jQuery(this).attr('id').split('-')[3];
+            dispatchUrl = jQuery.rootPath() + 'question_instances/' + question_instance_id + '/edit'
+          } else {
+            dispatchUrl = jQuery.rootPath() + 'question_instances/new'
+          }
+          jQuery.ajax({
+            type: 'GET',
+            url: dispatchUrl,
+            beforeSend: function(){
+              jQuery('#spinner_block').show();
+            },
+            success: function(html){
+              jQuery('#spinner_block').hide();
+              jQuery('#question-instance-form-container').html(html);
+              jQuery('#question-instance-form-container').dialog('open');
+            }
           });
         });
+      },
+      observeShowReplyControls: function(element,questionInstanceId,questionId,openReplyContainers){
+        var repliesContainer = jQuery('#replies-container-' + questionId);
+        jQuery(element).find('a.show-replies').click(function(e){
+          e.preventDefault();
+          if(repliesContainer.html().length > 0 && repliesContainer.is(':visible')){
+            //There's content in here and it's visible. Just hide it.
+            repliesContainer.toggle('fast');
+            jQuery.removeReplyContainerFromCookie('show-reply-containers','#replies-container-' + questionId);
+          } else {
+            //There's no content, or there's content and it's invisible. 
+            //Get the replies again to ensure fresh content.
+            jQuery.addReplyContainerToCookie('show-reply-containers','#replies-container-' + questionId);
+            jQuery.showReplyContainer(questionInstanceId,questionId,repliesContainer,'fast');
+          }
+        });
+        if(openReplyContainers['#replies-container-' + questionId] == 1){
+          jQuery.showReplyContainer(questionInstanceId,questionId,repliesContainer,0);
+        }
+      },
+      observeQuestionControls: function(){
+        var questionInstanceId = jQuery('div.questions').attr('id').split('-')[1];
+        jQuery.toggleVoteControls();
+        jQuery.observeNewQuestionControl(jQuery('#controls-' + questionInstanceId),questionInstanceId,0); 
+        jQuery("div[id*='question-']").each(function(){
+          var questionId = jQuery(this).attr('id').split('-')[1];
+          var openReplyContainers = jQuery.unserializeHash(jQuery.cookie('show-reply-containers'));
+          if(jQuery(this).hasClass('question')){
+          // It's a question. Init the reply toggles and voting
+            jQuery.observeShowReplyControls(this,questionInstanceId,questionId,openReplyContainers);
+            jQuery.observeVoteControls(this,questionInstanceId,questionId);
+          }
+          jQuery.observeNewQuestionControl(this,questionInstanceId,questionId); 
+        });
       }
-
 
   });
 
@@ -290,9 +288,7 @@ jQuery(function(){
         }
       } else {
         // We're viewing a question instance.
-        jQuery.observeVoteControls();
-        jQuery.observeNewQuestionControl();
-        jQuery.observeShowReplyControls();
+        jQuery.observeQuestionControls();
 //        jQuery.observeUpdateTimers();
         // By default we update every 5 seconds.
         setInterval("jQuery.updateAutomatically()",10000);
