772e3f7695586cf04ebbc1f3bf7c4d0e28b2afd1
jcallina <jcallina@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-04-16 18:07:17 +0000

    Locking down default sort order
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@119 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- app/models/rotisserie_instance.rb
+++ app/models/rotisserie_instance.rb
@@ -1,7 +1,7 @@
 class RotisserieInstance < ActiveRecord::Base
   acts_as_authorization_object
 
-  has_many :rotisserie_discussions
+  has_many :rotisserie_discussions, :order => :id
   #has_many :roles, :foreign_key => :authorizable_id, :conditions => {:authorizable_type => self.class.to_string}
   
   validates_presence_of :title
