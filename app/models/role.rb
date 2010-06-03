d97de1d6be6b5389684e37f05ae70c2e76ee7b59
jcallina <jcallina@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-06-03 15:45:57 +0000

    Added polymorphic association to roles
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@212 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,6 @@ class Role < ActiveRecord::Base
   acts_as_authorization_role
   has_many :roles_users
   has_many :users, :through => :roles_users
+
+  belongs_to :authorizable, :polymorphic => true
 end
