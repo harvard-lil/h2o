d00af6e573b69975fb235d80bb51278b1b7ad8f6
jcallina <jcallina@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-06-03 15:34:51 +0000

    Added relationship to user
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@211 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,8 @@ class User < ActiveRecord::Base
   acts_as_voter
   acts_as_authentic 
   acts_as_authorization_subject
-
+  
+  has_and_belongs_to_many :roles
   has_many :rotisserie_assignments
 
   validates_format_of_email :email_address, :allow_blank => true
