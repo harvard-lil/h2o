310c8303ab1f65faa8e47f175b5e2ccf015c3715
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-06-25 17:27:53 +0000

    Fixes and case listing work.
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@265 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- app/models/case_citation.rb
+++ app/models/case_citation.rb
@@ -4,5 +4,8 @@ class CaseCitation < ActiveRecord::Base
   validates_presence_of   :volume, :reporter, :page
   validates_length_of     :volume, :reporter, :page,  :in => 1..200
 
+  def display_name
+    "#{self.volume} #{self.reporter} #{self.page}"
+  end
 
 end
