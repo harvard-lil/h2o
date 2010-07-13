e3281766f30f0287626095287d09192b63f1715f
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-07-13 17:11:51 +0000

    Formtastic upgrade.
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@294 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,6 +1,9 @@
 # Set the default text field size when input is a string. Default is 50.
 # Formtastic::SemanticFormBuilder.default_text_field_size = 50
 
+# Set the default text area height when input is a text. Default is 20.
+# Formtastic::SemanticFormBuilder.default_text_area_height = 5
+
 # Should all fields be considered "required" by default?
 # Defaults to true, see ValidationReflection notes below.
 # Formtastic::SemanticFormBuilder.all_fields_required_by_default = true
@@ -25,7 +28,7 @@
 # Formtastic::SemanticFormBuilder.inline_errors = :sentence
 
 # Set the method to call on label text to transform or format it for human-friendly
-# reading when formtastic is user without object. Defaults to :humanize.
+# reading when formtastic is used without object. Defaults to :humanize.
 # Formtastic::SemanticFormBuilder.label_str_method = :humanize
 
 # Set the array of methods to try calling on parent objects in :select and :radio inputs
