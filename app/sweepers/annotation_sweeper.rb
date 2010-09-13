68ed01aa35a115550c673ba80051e7839a9da9dc
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-09-13 15:44:15 +0000

    Caching.
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@319 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- app/sweepers/annotation_sweeper.rb
+++ app/sweepers/annotation_sweeper.rb
@@ -6,16 +6,22 @@ class AnnotationSweeper < ActionController::Caching::Sweeper
 
   def after_save(record)
     expire_fragment("collage-annotatable-content-#{record.collage.id}")
+    expire_fragment("collage-index-view-#{record.collage.id}")
+    expire_fragment("collage-show-meta-#{record.collage.id}")
   end
 
   def after_create(record)
     # We are adding a new annotation and must therefore regenerate annotation decorations.
     expire_fragment("collage-annotatable-content-#{record.collage.id}")
+    expire_fragment("collage-index-view-#{record.collage.id}")
+    expire_fragment("collage-show-meta-#{record.collage.id}")
   end
 
   def after_destroy(record)
     # We are removing an annotation and must remove it from annotation decorations.
     expire_fragment("collage-annotatable-content-#{record.collage.id}")
+    expire_fragment("collage-index-view-#{record.collage.id}")
+    expire_fragment("collage-show-meta-#{record.collage.id}")
   end
 
 end
--- app/sweepers/collage_sweeper.rb
+++ app/sweepers/collage_sweeper.rb
@@ -6,11 +6,15 @@ class CollageSweeper < ActionController::Caching::Sweeper
   def after_save(record)
     # Changing a case shouldn't effect where annotations are located. 
     # expire_fragment("collage-annotatable-content-#{record.id}")
+    expire_fragment("collage-index-view-#{record.id}")
+    expire_fragment("collage-show-meta-#{record.id}")
   end
 
   def before_destroy(record)
     # Probably not really needed, but whatever.
     expire_fragment("collage-annotatable-content-#{record.id}")
+    expire_fragment("collage-index-view-#{record.id}")
+    expire_fragment("collage-show-meta-#{record.id}")
   end
 
 end
