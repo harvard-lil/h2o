d6d0da507a26b2d1f5257eaba5743a2fb3c4a88a
djcp <djcp@ac1452a4-9358-4c15-a3b0-de1119e254d8>
2010-10-06 15:50:27 +0000

    Tweak to improve efficiency of the "hide whitespace only nodes" function. Looks like this is the best compromise.
    git-svn-id: https://adam.law.harvard.edu/svn/h2o/trunk@362 ac1452a4-9358-4c15-a3b0-de1119e254d8

--- public/javascripts/collages.js
+++ public/javascripts/collages.js
@@ -231,12 +231,16 @@ showPleaseWait: function(){
 },
 
 hideEmptyElements: function(){
-  //TODO - Optimize this function, and exclude the annotation id printing removal.
-                     jQuery('#annotatable-content :hidden').remove();
-   jQuery('#annotatable-content tt').filter(function(){
-//       console.log('Display: ' + this.currentStyle.display);
-       return (document.defaultView.getComputedStyle(this,null).getPropertyValue('display') == 'none') ? true : false
-    }).remove();
+  
+  // So - as brute force as this would appear, this seems to represent the best compromise between performance and cross-browser compatibility.
+  jQuery('#annotatable-content tt:hidden').remove();
+
+ //jQuery('#annotatable-content :hidden').filter(':not(.print-inline)').remove();
+ /*   jQuery('#annotatable-content tt').filter(function(){
+     return (document.defaultView.getComputedStyle(this,null).getPropertyValue('display') == 'none') ? true : false
+//     return (jQuery(this).css('display') == 'none') ? true : false
+   }).remove(); */
+
   jQuery('#annotatable-content center, #annotatable-content p').filter(function(){
     var text = jQuery(this).text();
     return (text == '' || text == ' ' || text.match(/^(<br\/?>|\s|<\/?center>|<\/?b>|<\/?strong>)+$/im));
