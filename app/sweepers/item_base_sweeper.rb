5e447f67926dbf289ee26eafa576fcedc0f3683d
Steph Skardal <steph@endpoint.com>
2011-07-15 14:02:31 -0400

    Fixing remix playlist caching bug.

--- app/sweepers/item_base_sweeper.rb
+++ app/sweepers/item_base_sweeper.rb
@@ -4,37 +4,39 @@ class ItemBaseSweeper < ActionController::Caching::Sweeper
   observe ItemCollage, ItemCase, ItemPlaylist, ItemAnnotation, ItemQuestion, ItemQuestionInstance, ItemTextBlock, ItemDefault
 
   def after_create(record)
-	expire_fragment "playlist-block-#{params[:container_id]}-anon"
-	expire_fragment "playlist-block-#{params[:container_id]}-editable"
+    if params && params.has_key?(:container_id)
+      expire_fragment "playlist-block-#{params[:container_id]}-anon"
+      expire_fragment "playlist-block-#{params[:container_id]}-editable"
 
-	Playlist.find(params[:container_id]).relation_ids.each do |p|
-	  expire_fragment "playlist-block-#{p}-anon"
-	  expire_fragment "playlist-block-#{p}-editable"
-	end
+      Playlist.find(params[:container_id]).relation_ids.each do |p|
+        expire_fragment "playlist-block-#{p}-anon"
+        expire_fragment "playlist-block-#{p}-editable"
+      end
+    end
   end
 
   def after_update(record)
     if record && record.playlist_item
-	  expire_fragment "playlist-block-#{record.playlist_item.playlist_id}-anon"
-	  expire_fragment "playlist-block-#{record.playlist_item.playlist_id}-editable"
+      expire_fragment "playlist-block-#{record.playlist_item.playlist_id}-anon"
+      expire_fragment "playlist-block-#{record.playlist_item.playlist_id}-editable"
 
-	  record.playlist_item.playlist.relation_ids.each do |p|
-	    expire_fragment "playlist-block-#{p}-anon"
-	    expire_fragment "playlist-block-#{p}-editable"
-	  end
-	end
+      record.playlist_item.playlist.relation_ids.each do |p|
+        expire_fragment "playlist-block-#{p}-anon"
+        expire_fragment "playlist-block-#{p}-editable"
+      end
+    end
   end
 
   def before_destroy(record)
     if record && record.playlist_item
       playlist_id = record.playlist_item.playlist_id
-	  expire_fragment "playlist-block-#{playlist_id}-anon"
-	  expire_fragment "playlist-block-#{playlist_id}-editable"
+      expire_fragment "playlist-block-#{playlist_id}-anon"
+      expire_fragment "playlist-block-#{playlist_id}-editable"
 
-	  record.playlist_item.playlist.relation_ids.each do |p|
-	    expire_fragment "playlist-block-#{p}-anon"
-	    expire_fragment "playlist-block-#{p}-editable"
-	  end
-	end
+      record.playlist_item.playlist.relation_ids.each do |p|
+        expire_fragment "playlist-block-#{p}-anon"
+        expire_fragment "playlist-block-#{p}-editable"
+      end
+    end
   end
 end
