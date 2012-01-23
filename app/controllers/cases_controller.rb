c8db4a24f3cbfafebfb40c97a20cba30d23c5892
djcp <dan@collispuro.net>
2012-01-23 13:51:29 -0500

    Remove ajax from case editing as it was causing a really odd client-side error that meant content was only partially submitted.

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -165,7 +165,7 @@ class CasesController < BaseController
     respond_to do |format|
       if @case.update_attributes(params[:case])
         flash[:notice] = 'Case was successfully updated.'
-        format.html { redirect_to(cases_url) }
+        format.html { redirect_to(case_url(@case)) }
         format.json  { render :json => {:type => 'cases', :id => @case.id} }
       else
         format.html { render :action => "edit" }
