be3338a76a6945a0a1a764ab2536527c70c0efdc
Steph Skardal <steph@endpoint.com>
2012-07-27 12:50:00 -0400

    Google Analytics config.

---
+++ config/initializers/h2o_custom.rb
@@ -0,0 +1,5 @@
+if Rails.env == 'production'
+  GOOGLE_ANALYTICS_CODE = 'UA-223559-15'
+else
+  GOOGLE_ANALYTICS_CODE = 'UA-223559-16'
+end
--- config/initializers/h2o_defaults.rb
+++ config/initializers/h2o_defaults.rb
@@ -3,6 +3,3 @@ Formtastic::SemanticFormBuilder.escape_html_entities_in_hints_and_labels = false
 #yes, this is probably too coarse.
 WHITELISTED_TAGS = %w|a b br center del div em h1 h2 h3 h4 h5 h6 img li ol p strike strong ul blockquote|
 WHITELISTED_ATTRIBUTES = %w|class src href height width target title|
-
-# You should probably put this into an "h2o_custom.rb" initializer to make upgrades easier.
-# GOOGLE_ANALYTICS_CODE = 'your-analytics-code'
