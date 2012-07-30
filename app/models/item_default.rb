b82d1e9c2a699cdda9dcbb01d6dc324fa4dd45b4
Steph Skardal <steph@endpoint.com>
2012-07-30 11:26:01 -0400

    Added validation on default.name.

--- app/models/item_default.rb
+++ app/models/item_default.rb
@@ -4,12 +4,9 @@ class ItemDefault < ActiveRecord::Base
   acts_as_authorization_object
 
   has_one :playlist_item, :as => :resource_item, :dependent => :destroy
+  validates_presence_of :name
 
   def preview(size_indicator = "S")
-#    preview_content = <<-PREVIEW_CONTENT
-#       <a href='#{self.url}' target='_blank' class='item_default_link'><img src='http://images.websnapr.com/?size=#{size_indicator}&key=#{WEBSNAPR_KEY}&url=#{self.url}' style='border: 1px solid #607887;' /></a>
-#    PREVIEW_CONTENT
-
   end
 
   def bookmark_name
