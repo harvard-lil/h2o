16633a3dd5d2daa2fb3dea36b1e74ef5541a6246
Steph Skardal <steph@endpoint.com>
2012-08-21 16:32:17 -0400

    Migration fix on permissions.

--- db/migrate/20120815140931_create_collages_user_collections.rb
+++ db/migrate/20120815140931_create_collages_user_collections.rb
@@ -9,7 +9,7 @@ class CreateCollagesUserCollections < ActiveRecord::Migration
     [:position_update, :edit_descriptions, :edit_notes].each do |key|
       Permission.find_by_key(key.to_s).update_attribute(:permission_type, "playlist")
     end
-    [:edit_collages, :edit_annotations].each do |key|
+    [:edit_collage, :edit_annotations].each do |key|
       Permission.find_by_key(key.to_s).update_attribute(:permission_type, "collage")
     end
   end
