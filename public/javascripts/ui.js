a5403bf876f24bc9706562e1235b8172a0cc6ca4
Tim Case <tim@endpoint.com>
2013-01-18 10:59:17 -0500

    removing broken js and duplicated input

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -704,15 +704,6 @@ jQuery.extend({
           }
         }
         jQuery.toggleVisibilitySelector();
-              if (jQuery(this).attr("checked") == "checked"){
-                jQuery('#terms_require').html("<div style='text-align: left; padding-left: 100px'>Submitting this item will allow others to see, copy, and create derivative works from this item in accordance with H2O's <a href=\"/p/terms\" target=\"_blank\">Terms of Service</a>.</div>")
-              } else {
-                jQuery('#terms_require').html("<div style='text-align:left; padding-left: 100px'>If this item is submitted as a non-public item, other users will not be able to see, copy, or create derivative works from it, unless you change the item's setting to \"Public.\"</div>");
-              }
-            });
-            
-          }
-        }
       },
       buttons: {
         Submit: function() {
