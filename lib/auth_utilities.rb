3298c933e7b28420991c0631c85f24c023b53bd5
Tim Case <tim@endpoint.com>
2013-03-27 14:39:52 -0700

    merge changes

--- lib/auth_utilities.rb
+++ lib/auth_utilities.rb
@@ -36,4 +36,9 @@ module AuthUtilities
     (editor_list.blank?) ? nil : editor_list.first.users.compact.uniq
   end
 
+  def author
+    owner_list = self.accepted_roles.reject{|r| r.name != 'owner'}
+    (owner_list.blank?) ? nil : owner_list.first.users.compact.uniq.first.login.downcase
+  end
+
 end
