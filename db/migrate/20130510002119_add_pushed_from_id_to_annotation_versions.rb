3defabaa97829f6f4501e9934164459b6a99254d
Tim Case <tim@endpoint.com>
2013-05-20 20:30:58 -0400

    comment out migration that is broken due to version pulling

--- db/migrate/20130510002119_add_pushed_from_id_to_annotation_versions.rb
+++ db/migrate/20130510002119_add_pushed_from_id_to_annotation_versions.rb
@@ -1,9 +1,11 @@
 class AddPushedFromIdToAnnotationVersions < ActiveRecord::Migration
   def self.up
-    add_column :collage_link_versions, :pushed_from_id, :integer
+    #TIMCASE: This migration is commented out because we
+    #pulled versioning and it's dependent on versioning migrations
+    #add_column :collage_link_versions, :pushed_from_id, :integer
   end
 
   def self.down
-    remove_column :collage_link_versions, :pushed_from_id
+    #remove_column :collage_link_versions, :pushed_from_id
   end
 end
