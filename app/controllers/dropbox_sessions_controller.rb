7e5d69deed73713c21b10544347bdeea53f121b7
Tim Case <tim@2drops.net>
2013-05-23 15:53:00 -0700

    add dropbox sessions controller

--- app/controllers/dropbox_sessions_controller.rb
+++ app/controllers/dropbox_sessions_controller.rb
@@ -6,14 +6,12 @@ class DropboxSessionsController < ApplicationController
         dbsession = DropboxSession.new(DROPBOXCONFIG[:app_key], DROPBOXCONFIG[:app_secret])
 
         session[:dropbox_session] = dbsession.serialize 
-
         redirect_to dbsession.get_authorize_url url_for(:action => 'create')
     else
         # the user has returned from Dropbox
         dbsession = DropboxSession.deserialize(session[:dropbox_session])
         dbsession.get_access_token  #we've been authorized, so now request an access_token
         session[:dropbox_session] = dbsession.serialize
-
         redirect_to :controller => :bulk_uploads, :action => :new
     end
   end
