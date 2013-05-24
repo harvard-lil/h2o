8e2eade61409d17ef39ceeaf56eb15a9cbbfcbc6
Tim Case <tim@2drops.net>
2013-05-23 21:15:20 -0700

    refactor bulk upload

--- db/migrate/20130523003854_add_dropbox_filepath_to_cases.rb
+++
@@ -1,9 +0,0 @@
-class AddDropboxFilepathToCases < ActiveRecord::Migration
-  def self.up
-    add_column :cases, :dropbox_filepath, :string
-  end
-
-  def self.down
-    remove_column :cases, :dropbox_filepath
-  end
-end
