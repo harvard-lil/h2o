bf5ca82b5bb46312095e8eb59feaab2c7eb5b941
Tim Case <tim@2drops.net>
2013-05-28 18:32:10 -0700

    add delayed job to bulk upload

--- app/controllers/bulk_uploads_controller.rb
+++ app/controllers/bulk_uploads_controller.rb
@@ -3,37 +3,40 @@ class BulkUploadsController < ApplicationController
   before_filter :require_dropbox_session
   before_filter :initialize_dropbox_client
   before_filter :add_assets, :only => [:show, :new]
-  
+
   def show
     @bulk_upload = BulkUpload.find(params[:id])
   end
-  
-  def new    
+
+  def new
     @files_to_import = @dh2o.import_file_paths
   end
-  
+
   def create
-    bulk_upload = @dh2o.import(Case) 
-    redirect_to bulk_upload_path(bulk_upload)   
+    delayed_job = @dh2o.delay.import(Case)
+    bulk_upload = BulkUpload.last
+    bulk_upload.update_attribute('delayed_job_id', delayed_job.id)
+    flash[:notice] = "Download started. You will receive an email when it's finished.  You can also refresh this page to check progress."
+    redirect_to bulk_upload_path(bulk_upload)
   end
-  
+
   def require_dropbox_session
     unless current_dropbox_session
-      redirect_to(:controller => 'dropbox_sessions', :action => 'create') 
+      redirect_to(:controller => 'dropbox_sessions', :action => 'create')
       false
     end
   end
-  
+
+  def initialize_dropbox_client
+    @dh2o = DropboxH2o.new(current_dropbox_session)
+  end
+
   def current_dropbox_session
     session[:dropbox_session]
   end
-  
-  def initialize_dropbox_client
-    @dh2o = DropboxH2o.new(session[:dropbox_session])
-  end
-  
+
   def add_assets
     add_javascripts ['tiny_mce/tiny_mce.js', 'h2o_wysiwig', 'switch_editor', 'cases']
-    add_stylesheets ['new_case']    
+    add_stylesheets ['new_case']
   end
 end
