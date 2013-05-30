71cb6a50672aad105c28157410d6999f88f0bd4b
Tim Case <tim@endpoint.com>
2013-05-30 01:29:24 -0400

    fix issues with delayed job

--- app/controllers/bulk_uploads_controller.rb
+++ app/controllers/bulk_uploads_controller.rb
@@ -1,7 +1,7 @@
 class BulkUploadsController < ApplicationController
   before_filter :require_user
   before_filter :require_dropbox_session
-  before_filter :initialize_dropbox_client
+  before_filter :initialize_dropbox_client, :only => [:new, :show]
   before_filter :add_assets, :only => [:show, :new]
 
   def show
@@ -13,8 +13,10 @@ class BulkUploadsController < ApplicationController
   end
 
   def create
-    delayed_job = @dh2o.delay.import(Case)
-    bulk_upload = BulkUpload.last
+    bulk_upload = BulkUpload.create!
+    delayed_job = DropboxH2o.delay.do_import(Case, 
+                                             current_dropbox_session,
+                                             bulk_upload)
     bulk_upload.update_attribute('delayed_job_id', delayed_job.id)
     flash[:notice] = "Download started. You will receive an email when it's finished.  You can also refresh this page to check progress."
     redirect_to bulk_upload_path(bulk_upload)
