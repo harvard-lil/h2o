0928083d4d5ea91ad063fe0e7023e4ba182ece16
Tim Case <tim@2drops.net>
2013-05-30 10:38:40 -0700

    Move dropboxh2o to lib

--- config/initializers/dropbox_h2o.rb
+++ config/initializers/dropbox_h2o.rb
@@ -1,6 +1,3 @@
-require "#{RAILS_ROOT}/app/models/dropbox_error_log"
-require "#{RAILS_ROOT}/app/models/dropbox_h2o"
-require "#{RAILS_ROOT}/app/models/dropbox_importer"
 DropboxErrorLog
 DropboxH2o
 DropboxImporter
