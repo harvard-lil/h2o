b1d4bf2a000d395c6677395f5796107e822992c2
Tim Case <tim@endpoint.com>
2013-05-30 15:15:38 -0400

    readd explicit requires

--- config/initializers/dropbox_h2o.rb
+++ config/initializers/dropbox_h2o.rb
@@ -1,3 +1,7 @@
+require "#{RAILS_ROOT}/lib/dropbox/dropbox_error_log"
+require "#{RAILS_ROOT}/lib/dropbox/dropbox_h2o"
+require "#{RAILS_ROOT}/lib/dropbox/dropbox_importer"
+
 DropboxErrorLog
 DropboxH2o
 DropboxImporter
