9609b51817dc5c884e46a6bdd9e6df5e1a7c92a3
Tim Case <tim@endpoint.net>
2014-02-12 00:20:28 -0500

    fix bad param pass

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,11 +3,11 @@ class UserSessionsController < ApplicationController
   before_filter :require_user, :only => :destroy
   before_filter :display_first_time_canvas_notice, :only => [:new]
   protect_from_forgery :except => [:create]
-  
+
   def show
     redirect_to root_url, :status => 301
   end
-  
+
   def new
     @user_session = UserSession.new
     render :layout => (request.xhr?) ? false : true
@@ -17,7 +17,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     @user_session.save do |result|
       if result
-        apply_user_preferences(@user_session.user)
+        apply_user_preferences(@user_session.user, false)
         if request.xhr?
           #Text doesn't matter, status code does.
           render :text => 'Success!', :layout => false
@@ -25,9 +25,9 @@ class UserSessionsController < ApplicationController
           if first_time_canvas_login?
             save_canvas_id_to_user(@user_session.user)
             flash[:notice] = "You canvas id was attached to this account"
-			redirect_to user_path(@user_session.user)
-		  else
-			redirect_back_or_default "/"
+            redirect_to user_path(@user_session.user)
+          else
+            redirect_back_or_default "/"
           end
         end
       else
