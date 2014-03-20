d58f22e8b5090f69477077911a7938dd1b09296b
Steph Skardal <steph@endpoint.com>
2014-03-20 13:57:13 -0400

    Fixed dropbox path hardcoded.

--- config/initializers/dropbox.rb
+++ config/initializers/dropbox.rb
@@ -1,3 +1,3 @@
 DROPBOXCONFIG = YAML.load(File.read(File.expand_path('../../dropbox.yml', __FILE__)))
 DROPBOXCONFIG.symbolize_keys!
-DROPBOX_ACCESS_TOKEN_DIR = '/web/murk/home/tcase/tcase01/config/dropbox_access_tokens'
+DROPBOX_ACCESS_TOKEN_DIR = '/web/halfrek/rails3prod/docs/h2o-prod/tmp/dropbox'
