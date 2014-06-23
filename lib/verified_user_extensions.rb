80c3bd3fa57722c7e0e156ae937b5f68ef92b893
Steph Skardal <steph@endpoint.com>
2014-06-23 08:40:07 -0400

    Added exception handling when user not logged in for item creation.

--- lib/verified_user_extensions.rb
+++ lib/verified_user_extensions.rb
@@ -7,7 +7,7 @@ module VerifiedUserExtensions
   end
   
   def verify_user
-    if !current_user.verified
+    if !(current_user && current_user.verified)
       self.errors.add(:base, "Your account must be verified to contribute to H2O.")
     end
   end
