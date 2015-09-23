3055f4a3cf2a517f6bc97e1f1b200f61ff818884
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2015-09-23 16:47:02 -0400

    Skip captcha check if running in development mode

--- lib/captcha_extensions.rb
+++ lib/captcha_extensions.rb
@@ -5,9 +5,10 @@ module CaptchaExtensions
     attr_accessor :valid_recaptcha
     validate :captcha, :if => Proc.new { |f| f.new_record? }
   end
-  
+
   def captcha
-    return if current_user && current_user.preverified?
+    return if current_user.try(:preverified?)
+    return if Rails.env.development?
 
     if self.valid_recaptcha.nil?
       self.errors.add(:base, "Captcha failed. Please try again.")
