2ce6f32a04b7c64b2c2e8dd0a2f3d20c2885c4a9
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2015-12-04 19:40:00 -0500

    Add more fonts to fallback font lists

--- app/assets/javascripts/fonts.js
+++ app/assets/javascripts/fonts.js
@@ -1,21 +1,21 @@
 var h2o_fonts = {
   font_map: {
-	  'goudy' : 'sorts-mill-goudy',
-	  'leitura' : 'leitura-news',
-	  'garamond' : 'adobe-garamond-pro',
-	  'futura' : 'futura-pt',
-	  'dagny' : 'ff-dagny-web-pro',
-	  'proxima' : 'proxima-nova',
-	  'verdana' : 'Verdana'
+	goudy: 'sorts-mill-goudy',
+	leitura: 'leitura-news',
+	garamond: 'adobe-garamond-pro',
+	futura: 'futura-pt',
+	dagny: 'ff-dagny-web-pro',
+	proxima: 'proxima-nova',
+	verdana: 'Verdana',
   },
   font_map_fallbacks: {
-	'goudy' : '"sorts-mill-goudy", "Sorts Mill Goudy", "Goudy", "Goudy Old Style"',
-	'leitura' : '"leitura-news", "Leitura News"',
-	'garamond' : '"adobe-garamond-pro", "Garamond", "Adobe Garamond Pro", "GaramondNo8"',
-	'futura' : '"futura-pt", "Futura PT", "Futura"',
-	'dagny' : '"ff-dagny-web-pro", "FF Dagny Web Pro", "FF Dagny Pro", "FF Dagny"',
-	'proxima' : '"proxima-nova", "Proxima Nova"',
-	'verdana' : '"Verdana", "Arial", "Helvetica", "sans-serif"',
+	goudy: ["sorts-mill-goudy", "Sorts Mill Goudy", "Goudy", "Goudy Old Style"].join(', '),
+	leitura: ["leitura-news", "Leitura News", "adobe-garamond-pro", "Garamond", "Adobe Garamond Pro", "GaramondNo8"].join(', '),
+	garamond: ["adobe-garamond-pro", "Garamond", "Adobe Garamond Pro", "GaramondNo8"].join(', '),
+	futura: ["futura-pt", "Futura PT", "Futura"].join(', '),
+	dagny: ["ff-dagny-web-pro", "FF Dagny Web Pro", "FF Dagny Pro", "FF Dagny", "Verdana", "Arial", "Helvetica", "sans-serif"].join(', '),
+	proxima: ["proxima-nova", "Proxima Nova", "futura-pt", "Futura PT", "Futura"].join(', '),
+	verdana: ["Verdana", "Arial", "Helvetica", "sans-serif"].join(', '),
   },
   //sizes are in pixels
   base_font_sizes: {
