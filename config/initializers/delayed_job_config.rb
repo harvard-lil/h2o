43ac9c0a0904c803dee864d2cbf2f970e3f1be89
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2016-01-08 18:47:42 -0500

    Increase Delayed::Worker.max_run_time to 20 minutes

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,3 +1,3 @@
-Delayed::Worker.sleep_delay = 5
+Delayed::Worker.sleep_delay = 10
 Delayed::Worker.max_attempts = 1
-Delayed::Worker.max_run_time = 10.minutes
+Delayed::Worker.max_run_time = 20.minutes
