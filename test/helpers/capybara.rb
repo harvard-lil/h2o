35ab63ce302fa2d1136dac9c0db65758b13aaa01
Emma Cushman <acushman@gmail.com>
2017-03-23 21:17:04 -0700

    support Solr during marked tests, example cases search test

---
+++ test/helpers/capybara.rb
@@ -0,0 +1,47 @@
+
+class Capybara::Rails::TestCase
+  include ActionView::Helpers::JavaScriptHelper
+  def assert_path_changes
+    # assert a redirect has occurred during block
+    path = current_path
+    yield
+    assert_no_current_path path
+  end
+  def random_token
+    # generate a short random string
+    SecureRandom.base64 8
+  end
+  def select_text text
+    node = page.execute_script <<-JS
+        var range = rangy.createRange();
+        range.findText('#{escape_javascript text}');
+        rangy.getSelection().addRange(range);
+        range.startContainer.parentElement.className += ' selected-container';
+    JS
+    find('.selected-container').trigger :mouseup
+  end
+  def sign_in user
+    # This directly logs in the user.
+    # Don't use this when testing login itself!
+    user.set_password = password = random_token
+    user.save!
+    case page.driver
+    when Capybara::RackTest::Driver
+      page.driver.post user_sessions_path, user_session: {login: user.login, password: password}
+    else # e.g. Capybara::Selenium::Driver
+      visit new_user_session_path
+      fill_in 'Login', with: user.login
+      fill_in 'Password', with: password
+      click_button 'Login'
+    end
+  end
+  def sign_out
+      visit log_out_path
+  end
+end
+
+class Capybara::Session
+  def submit(element)
+    Capybara::RackTest::Form.new(driver, element.native).submit({})
+  end
+end
