f6681a3cdc5ce2814a172e0ccdd9d54719942060
Emma Cushman <acushman@gmail.com>
2017-03-23 21:13:06 -0700

    disable annotation on texts, since it was silently failing anyway

--- app/assets/javascripts/text_blocks.js
+++ app/assets/javascripts/text_blocks.js
@@ -1,5 +1,5 @@
 var text_blocks_show = {
   initialize: function() {
-    h2o_global.initiate_annotator('text_blocks', report_options, can_edit);
+    // h2o_global.initiate_annotator('text_blocks', report_options, can_edit);
   }
 };
