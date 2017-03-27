4d82629d844e4bfbef53165a63908a4795569973
Emma Cushman <acushman@gmail.com>
2017-03-27 12:54:16 -0700

    ran app:update, errors fixed, rails c runs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,21 @@
 H2o::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # rails 5 configs:
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+
+  config.action_mailer.perform_caching = false
+  config.assets.quiet = true
+  # config.action_view.raise_on_missing_translations = true
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  # config.cache_classes = false
+
+
 
   # Do not eager load code on boot.
   config.eager_load = false
