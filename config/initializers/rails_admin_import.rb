55142423877c62bb9a95b799cee5767f023e078a
Emma Cushman <acushman@gmail.com>
2017-03-27 12:54:16 -0700

    ran app:update, errors fixed, rails c runs

--- config/initializers/rails_admin_import.rb
+++ config/initializers/rails_admin_import.rb
@@ -1,8 +1,9 @@
-RailsAdminImport.config do |config|
-  config.model Default do
-    label :name
-    excluded_fields do
-      [:tagging, :base_tag, :tag_tagging, :karma, :pushed_from_id, :content_type, :ancestry, :playlist_item]
-    end
-  end
-end
+# TODO: deprecated in 2.0
+# RailsAdminImport.config do |config|
+#   config.model Default do
+#     label :name
+#     excluded_fields do
+#       [:tagging, :base_tag, :tag_tagging, :karma, :pushed_from_id, :content_type, :ancestry, :playlist_item]
+#     end
+#   end
+# end
