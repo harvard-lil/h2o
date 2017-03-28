3c5109ce4b932355dc4e815af3278ce8fc82297c
Emma Cushman <acushman@gmail.com>
2017-03-27 17:21:38 -0700

    don't require a current_user to create objects in rails console

--- lib/verified_user_extensions.rb
+++ lib/verified_user_extensions.rb
@@ -5,10 +5,15 @@ module VerifiedUserExtensions
     attr_accessor :verified_user
     validate :verify_user, :if => Proc.new { |f| f.new_record? }
   end
-  
+
   def verify_user
-    if !(current_user && current_user.verified) && !(self.class == Playlist && self.name == "Your Bookmarks")
-      self.errors.add(:base, "Your account must be verified to contribute to H2O.")
+    # TODO: Checking current user here is weird.
+    if !defined?(Rails::Console) # Convenience for running in console w/o current user
+      if !(current_user && current_user.verified) && !(self.class == Playlist && self.name == "Your Bookmarks")
+        self.errors.add(:base, "Your account must be verified to contribute to H2O.")
+      end
+    else
+      puts 'WARNING: Skipping user verification because Rails::Console is defined'
     end
   end
 end
