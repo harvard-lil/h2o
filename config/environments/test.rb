78c0cb958acd62063dc708b0da90b3b4bb1dac3d
Emma Cushman <acushman@gmail.com>
2017-03-29 11:55:17 -0700

    enable CSRF in tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,8 +35,8 @@ H2o::Application.configure do
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  # Include forgery protection for system tests
+  config.action_controller.allow_forgery_protection = true
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
