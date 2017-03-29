23d50f6bc1f71fccc4afc354e292adcbdf651102
Emma Cushman <acushman@gmail.com>
2017-03-29 15:25:59 -0700

    remove defects code

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -23,7 +23,6 @@ class Ability
       can [:playlist_lookup], :playlists
       can :collage_lookup, :collages
       can [:user_lookup, :playlists, :disconnect_canvas, :disconnect_dropbox], :users
-      can :create, :defects
       can :quick_collage, :base
       can :create, :responses
 
