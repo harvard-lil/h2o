3b8360cd5aa6f8ed8dc272ff50d1de2e4f87d324
Emma Cushman <acushman@gmail.com>
2017-04-03 18:16:00 -0700

    test export for playlists

--- test/features/export_test.rb
+++ test/features/export_test.rb
@@ -23,4 +23,24 @@ feature 'exporting' do
 
       visit root_path
   end
+
+    scenario 'exporting a case', js:true do
+      sign_in users(:verified_student)
+      visit playlist_path public_playlist = cases(:public_playlist_1)
+
+      click_link 'Print'
+
+      assert_content public_playlist.name
+
+      select 'DOC', from: 'export_format'
+
+      # TODO: Actual exporting is very hard to test. Should be rebuilt
+      assert_enqueued_jobs 1 do
+        click_link 'export-form-submit'
+        assert_content 'H2O is exporting your content to DOC format.'
+        sleep 0.1
+      end
+
+      visit root_path
+  end
 end
