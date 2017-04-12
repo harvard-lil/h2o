0199ee2a005d5f9cab2cd5ab806ccdc5e812c5f4
Emma Cushman <git.commit@auto.emmalem.ma>
2017-04-12 15:36:19 -0700

    correct annotation test fixtures

--- test/system/admin_test.rb
+++ test/system/admin_test.rb
@@ -17,7 +17,7 @@ class AdminSystemTest < ApplicationSystemTestCase
 
     fill_in 'Slug', with: 'test-page'
     wait_until(wait: 10.seconds) {evaluate_script "CKEDITOR.instances['page_content'].loaded"}
-    sleep 2 # trying to make this a bit more reliable
+    sleep 3 # trying to make this a bit more reliable
     evaluate_script "CKEDITOR.instances['page_content'].insertHtml('This is some page content.')"
     assert { evaluate_script("CKEDITOR.instances['page_content'].execCommand('image')") }
     assert_content 'Image Properties'
