e33f729e9cd6dd8bb234c2309c3db61d7019effa
Emma Cushman <git.commit@auto.emmalem.ma>
2017-04-12 12:14:47 -0700

    fix test command for travis

--- test/system/admin_test.rb
+++ test/system/admin_test.rb
@@ -16,6 +16,7 @@ class AdminSystemTest < ApplicationSystemTestCase
 
     fill_in 'Slug', with: 'test-page'
     wait_until(wait: 10.seconds) {evaluate_script "CKEDITOR.instances['page_content'].loaded"}
+    sleep 1 # trying to make this a bit more reliable
     evaluate_script "CKEDITOR.instances['page_content'].insertHtml('This is some page content.')"
     assert { evaluate_script("CKEDITOR.instances['page_content'].execCommand('image')") }
     assert_content 'Image Properties'
