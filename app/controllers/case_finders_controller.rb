3bc2818561be0bbf8d7a9cb5932ceba5f75766ad
Casey Gruppioni <cgruppioni@gmail.com>
2017-05-02 10:48:48 -0400

    CaseFinder system tests pass, CaseDownloader returns boolean.

--- app/controllers/case_finders_controller.rb
+++ app/controllers/case_finders_controller.rb
@@ -2,17 +2,17 @@ class CaseFindersController < BaseController
   protect_from_forgery unless: -> { request.format.json? } ### is this necessary?
 
   def new
-    if case_search_initiated
+    if case_search_initiated?
       @cases = CapApiSearchResults.perform(case_finder_params)
     end
   end
 
   def create
-    if case_downloaded
-      flash[:notice] = 'Import successful'
+    if case_download?
+      flash[:notice] = "Import successful"
+      ## pass new_case_id so that there can be a link to the new case
       redirect_back(fallback_location: new_case_finder_path)
-    else 
-      Notifier.case_import_failure(current_user, case_metadata)
+    else
       flash[:error] = 'Case import failed'
       redirect_back(fallback_location: new_case_finder_path)
     end
@@ -24,26 +24,15 @@ class CaseFindersController < BaseController
     params.require(:case_finder).permit(:name, :citation)
   end
 
-  def case_search_initiated
-    params[:case_finder]
+  def case_search_initiated?
+    params[:case_finder].present?
   end
 
   def case_metadata
-    metadata = JSON.parse(params[:case])
-    metadata['user_id'] = current_user.id
-    metadata
+    JSON.parse(params[:case])
   end
 
-   def case_downloaded
-    response = HTTParty.get("https://capapi.org/api/v1/cases/#{case_metadata["slug"]}/?type=download&max=1",
-                            query: { "type" => "download" },
-                            headers: { "Authorization" => "Token #{H2o::Application.config.cap_api_key}" }
-                            )
-
-    if response.code == 200
-      CaseDownloader.perform(response.body, case_metadata)
-    else
-      false
-    end
+  def case_download?
+    CaseDownloader.perform(current_user, case_metadata)
   end
 end
