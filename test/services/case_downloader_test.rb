3c942e564a03c0d9bc511f6cf8c80f6aa2f6904a
Casey Gruppioni <cgruppioni@gmail.com>
2017-05-03 15:15:30 -0400

    Fix spacing - tabs to spaces.
    Fix spacing - tabs to spaces.

--- test/services/case_downloader_test.rb
+++ test/services/case_downloader_test.rb
@@ -1,31 +1,31 @@
 require 'application_system_test_case'
 
 class CaseDownloaderTest < ApplicationSystemTestCase
-	before do
-		@current_user = users(:verified_professor)
-	end
+  before do
+    @current_user = users(:verified_professor)
+  end
 
-	scenario 'Returns true if successfully creates a new case' do
-		previous_number_of_cases = Case.count
+  scenario 'Returns true if successfully creates a new case' do
+    previous_number_of_cases = Case.count
 
-		import_case_from_cap(case_metadata)
-		case_downloader = CaseDownloader.perform(@current_user, case_metadata)
-		new_number_of_cases = Case.count
-		
-		refute_equal new_number_of_cases, previous_number_of_cases
-		assert_equal case_downloader, true
-	end
+    import_case_from_cap(case_metadata)
+    case_downloader = CaseDownloader.perform(@current_user, case_metadata)
+    new_number_of_cases = Case.count
+    
+    refute_equal new_number_of_cases, previous_number_of_cases
+    assert_equal case_downloader, true
+  end
 
-	scenario 'Returns false if creating a case fails and sends a notifier mailer' do
-		previous_number_of_cases = Case.count
+  scenario 'Returns false if creating a case fails and sends a notifier mailer' do
+    previous_number_of_cases = Case.count
 
-		import_case_from_cap(incomplete_case_metadata)
-		case_downloader = CaseDownloader.perform(@current_user, incomplete_case_metadata)
-		new_number_of_cases = Case.count
-		mail = ActionMailer::Base.deliveries.last
-		
-		assert_equal new_number_of_cases, previous_number_of_cases
-		assert_equal case_downloader, false
-		assert_equal H2o::Application.config.admin_email, mail['to'].to_s
-	end
+    import_case_from_cap(incomplete_case_metadata)
+    case_downloader = CaseDownloader.perform(@current_user, incomplete_case_metadata)
+    new_number_of_cases = Case.count
+    mail = ActionMailer::Base.deliveries.last
+    
+    assert_equal new_number_of_cases, previous_number_of_cases
+    assert_equal case_downloader, false
+    assert_equal H2o::Application.config.admin_email, mail['to'].to_s
+  end
 end
--- test/system/cap_api_import_test.rb
+++ test/system/cap_api_import_test.rb
@@ -2,58 +2,58 @@ require 'application_system_test_case'
 require 'minitest/mock'
 
 class CapApiImportSystemTest < ApplicationSystemTestCase
-	describe 'as a logged in user' do
-		before do
-			@case_name = 'Comer v. Titan Tool, Inc.'
-			@case_citation = '875 F. Supp. 255'
-			@case_metadata = { 'slug' => 'comer-v-titan-tool-inc-2293' }
-			@query_params = { name: @case_name, citation: @case_citation }
-		end
+  describe 'as a logged in user' do
+    before do
+      @case_name = 'Comer v. Titan Tool, Inc.'
+      @case_citation = '875 F. Supp. 255'
+      @case_metadata = { 'slug' => 'comer-v-titan-tool-inc-2293' }
+      @query_params = { name: @case_name, citation: @case_citation }
+    end
 
-		scenario 'search for a case by name, view results and download a selected case' do
-			sign_in users(:verified_professor)
+    scenario 'search for a case by name, view results and download a selected case' do
+      sign_in users(:verified_professor)
 
-			visit new_cap_api_import_path
+      visit new_cap_api_import_path
 
-			fill_in 'Name', with: @case_name
-			fill_in 'Citation', with: @case_citation
+      fill_in 'Name', with: @case_name
+      fill_in 'Citation', with: @case_citation
 
-			search_for_cases(@query_params)
+      search_for_cases(@query_params)
 
-			click_button 'Search'
+      click_button 'Search'
 
-			assert_content @case_name
+      assert_content @case_name
 
-			choose "case"
+      choose "case"
 
-			import_case_from_cap(@case_metadata)
+      import_case_from_cap(@case_metadata)
 
-			click_button 'Download new case'
+      click_button 'Download new case'
 
-			assert_content 'Import successful'
-		end
+      assert_content 'Import successful'
+    end
 
-		scenario 'can see an error message when there is a failed case download' do
-			sign_in user = users(:verified_professor)
+    scenario 'can see an error message when there is a failed case download' do
+      sign_in user = users(:verified_professor)
 
-			visit new_cap_api_import_path
+      visit new_cap_api_import_path
 
-			fill_in 'Name', with: @case_name
-			fill_in 'Citation', with: @case_citation
+      fill_in 'Name', with: @case_name
+      fill_in 'Citation', with: @case_citation
 
-			search_for_cases(@query_params)
+      search_for_cases(@query_params)
 
-			click_button 'Search'
+      click_button 'Search'
 
-			assert_content @case_name
+      assert_content @case_name
 
-			choose "case"
+      choose "case"
 
-			import_case_from_cap_failed_attempt(@case_metadata)
+      import_case_from_cap_failed_attempt(@case_metadata)
 
-			click_button 'Download new case'
+      click_button 'Download new case'
 
-			assert_content 'Case import failed'
-		end
-	end
+      assert_content 'Case import failed'
+    end
+  end
 end
