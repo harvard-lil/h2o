36de7645959203f4fc6959174cc98335d806f313
Emma Cushman <git.commit@auto.emmalem.ma>
2017-06-28 10:02:20 -0700

    create staging config as a copy of production

---
+++ config/environments/staging.rb
@@ -0,0 +1,13 @@
+require Rails.root.join("config/environments/production")
+
+H2o::Application.configure do
+  # log settings
+  config.log_level = :debug
+
+  # allow users without verified .edu addresses
+  config.disable_verification = true
+
+  # cap api settings
+  config.admin_email = 'cgruppioni@law.harvard.edu'
+  config.cap_api_key = '2c62c54b47e507b2eee20a70f29f1b4ae0ccd1a3'
+end
