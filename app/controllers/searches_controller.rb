b37f0476781a4fabec711c0187a65879663f555d
Emma Cushman <git.commit@auto.emmalem.ma>
2017-06-30 03:06:29 -0700

    pagination of search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,27 +1,42 @@
 class SearchesController < ApplicationController
+  PER_PAGE = 10
   layout 'main'
+
+  before_action :read_page
 
   def show
     @query = params[:q]
     @type = params[:type] || 'casebooks'
+
     @results = if @query
       result_groups @query
     else
       result_groups '*'
     end
-    @main_group = @results[@type.to_sym]
+    @pagination = paginate_group @results[@type.to_sym]
+
     if params[:partial]
-      render partial: 'results', layout: false, locals: {results: @main_group.results}
+      render partial: 'results', layout: false, locals: {pagination: @pagination}
     end
   end
 
   def index
     @results = result_groups '*'
-    @main_group = @results[:casebooks]
+    @pagination = paginate_group @results[:casebooks]
     render 'searches/show'
   end
 
   private
+
+  def paginate_group group
+    WillPaginate::Collection.create(@page, PER_PAGE, group.total) do |pager|
+       pager.replace(group.results)
+    end
+  end
+
+  def read_page
+    @page = (params[:page] || 1).to_i
+  end
 
   def result_groups query
     groups = search_query(@query).group(:klass).groups
@@ -33,6 +48,7 @@ class SearchesController < ApplicationController
   end
 
   def search_query query
+    page = @page
     Sunspot.search Case, Content::Casebook, User do
       keywords query
 
@@ -45,7 +61,11 @@ class SearchesController < ApplicationController
 
       order_by (params[:sort] || 'display_name').to_sym
       group :klass do
-        limit -1
+        limit PER_PAGE
+      end
+
+      adjust_solr_params do |params|
+        params['group.offset'] = (page - 1) * PER_PAGE
       end
     end
   end
