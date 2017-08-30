4f291fdc370076181f588bfb3958234b016ce3d1
Emma Cushman <git.commit@auto.emmalem.ma>
2017-08-30 14:44:37 -0700

    re-enable and fix export tests for docx and pdf

--- test/system/export_test.rb
+++ test/system/export_test.rb
@@ -1,246 +1,39 @@
 require 'application_system_test_case'
 
 class ExportSystemTest < ApplicationSystemTestCase
-  before :each do
-    skip 'no export tests'
+  scenario 'exporting a casebook to .docx', js:true do
+    export_test casebook_path(content_nodes(:public_casebook)), 'Word', 'test_export_casebook.docx'
+  end
+  scenario 'exporting a section to .docx', js:true do
+    export_test  section_path(content_nodes(:public_casebook), content_nodes(:public_casebook_section_1)), 'Word', 'test_export_section.docx'
+  end
+  scenario 'exporting a section to .docx', js:true do
+    export_test  resource_path(content_nodes(:public_casebook), content_nodes(:'public_casebook_section_1.1')), 'Word', 'test_export_resource.docx'
+  end
+  scenario 'exporting a casebook to .pdf', js:true do
+    export_test casebook_path(content_nodes(:public_casebook)), 'PDF', 'test_export_casebook.pdf'
+  end
+  scenario 'exporting a section to .pdf', js:true do
+    export_test  section_path(content_nodes(:public_casebook), content_nodes(:public_casebook_section_1)), 'PDF', 'test_export_section.pdf'
+  end
+  scenario 'exporting a section to .pdf', js:true do
+    export_test  resource_path(content_nodes(:public_casebook), content_nodes(:'public_casebook_section_1.1')), 'PDF', 'test_export_resource.pdf'
   end
 
-  scenario 'exporting a case to .docx', js:true do
+  def export_test path, format, file
     sign_in user = users(:verified_student)
-    visit case_path public_case = cases(:public_case_1)
-    click_link 'Print'
+    visit path
+    click_link 'Export'
 
-    assert_content public_case.short_name
-    assert_content public_case.content
-
-    select 'DOCX', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to DOCX format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
+    within '#export-modal' do
+      assert_content 'Export Casebook'
+      select format, from: 'export-format'
+      click_link 'Export'
     end
 
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.docx)/) }
-    exported_file_url = email.body.match(/(http.+?\.docx)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_case.docx'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_case.docx'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_case.docx')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a playlist to .docx', js:true do
-    sign_in user = users(:verified_student)
-    visit playlist_path public_playlist = playlists(:public_playlist_1)
-
-    click_link 'Print'
-
-    assert_content public_playlist.name
-
-    select 'DOCX', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to DOCX format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.docx)/) }
-    exported_file_url = email.body.match(/(http.+?\.docx)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_playlist.docx'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_playlist.docx'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_playlist.docx')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a case to .pdf', js:true do
-    sign_in user = users(:verified_student)
-    visit case_path public_case = cases(:public_case_1)
-
-    click_link 'Print'
-
-    assert_content public_case.short_name
-    assert_content public_case.content
-
-    select 'PDF', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to PDF format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.pdf)/) }
-    exported_file_url = email.body.match(/(http.+?\.pdf)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_case.pdf'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_case.pdf'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_case.pdf')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a playlist to .pdf', js:true do
-    sign_in user = users(:verified_student)
-    visit playlist_path public_playlist = playlists(:public_playlist_1)
-
-    click_link 'Print'
-
-    assert_content public_playlist.name
-
-    select 'PDF', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to PDF format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.pdf)/) }
-    exported_file_url = email.body.match(/(http.+?\.pdf)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_playlist.pdf'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_playlist.pdf'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_playlist.pdf')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a text to .docx', js:true do
-    sign_in user = users(:verified_student)
-    visit text_block_path public_text = text_blocks(:public_text_1)
-
-    click_link 'Print'
-
-    assert_content public_text.name
-    assert_content public_text.content
-
-    select 'DOCX', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to DOCX format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.docx)/) }
-    exported_file_url = email.body.match(/(http.+?\.docx)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_text.docx'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_text.docx'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_text.docx')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a text to .pdf', js:true do
-    sign_in user = users(:verified_student)
-    visit text_block_path public_text = text_blocks(:public_text_1)
-
-    click_link 'Print'
-
-    assert_content public_text.name
-    assert_content public_text.content
-
-    select 'PDF', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to PDF format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.pdf)/) }
-    exported_file_url = email.body.match(/(http.+?\.pdf)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_text.pdf'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_text.pdf'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_text.pdf')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a playlist to .pdf', js:true do
-    sign_in user = users(:verified_student)
-    visit playlist_path public_playlist = playlists(:public_playlist_1)
-
-    click_link 'Print'
-
-    assert_content public_playlist.name
-
-    select 'PDF', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to PDF format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.pdf)/) }
-    exported_file_url = email.body.match(/(http.+?\.pdf)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_playlist.pdf'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_playlist.pdf'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_playlist.pdf')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a text to .docx', js:true do
-    sign_in user = users(:verified_student)
-    visit text_block_path public_text = text_blocks(:public_text_1)
-
-    click_link 'Print'
-
-    assert_content public_text.name
-    assert_content public_text.content
-
-    select 'DOCX', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to DOCX format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.docx)/) }
-    exported_file_url = email.body.match(/(http.+?\.docx)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_text.docx'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_text.docx'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_text.docx')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
-  end
-
-  scenario 'exporting a text to .pdf', js:true do
-    sign_in user = users(:verified_student)
-    visit text_block_path public_text = text_blocks(:public_text_1)
-
-    click_link 'Print'
-
-    assert_content public_text.name
-    assert_content public_text.content
-
-    select 'PDF', from: 'export_format'
-
-    perform_enqueued_jobs do
-      click_link 'export-form-submit'
-      assert_content 'H2O is exporting your content to PDF format.'
-      assert_sends_emails 1, wait: 10.seconds # This needs to be in the block for some reason
-    end
-
-    email = ActionMailer::Base.deliveries.first
-    assert { email.to.include? user.email_address }
-    assert { email.body.match(/(http.+?\.pdf)/) }
-    exported_file_url = email.body.match(/(http.+?\.pdf)/)[1]
-
-    downloaded_path = download_file exported_file_url, to: 'test_export_text.pdf'
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path('test_export_text.pdf'))
-    assert_equal Digest::SHA256.file(expected_file_path('test_export_text.pdf')).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
+    exported_file_url = evaluate_script('_test_window_urls').last
+    downloaded_path = download_file exported_file_url, to: file
+    assert_equal File.size?(downloaded_path), File.size?(expected_file_path(file))
+    assert_equal Digest::SHA256.file(expected_file_path(file)).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
   end
 end
