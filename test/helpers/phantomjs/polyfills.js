b848f3627db4f955f7d2085a82a7b33f0759d11b
Emma Cushman <git.commit@auto.emmalem.ma>
2017-08-30 02:35:29 -0700

    reenable and fix casebook annotation test

--- test/helpers/phantomjs/polyfills.js
+++ test/helpers/phantomjs/polyfills.js
@@ -1,3 +1,11 @@
+window.disableTurbolinks = true;
+
+document.addEventListener('turbolinks:before-visit', function (e) {
+  e.preventDefault();
+  location.href = event.data.url;
+});
+
+
 if (!String.prototype.startsWith) {
     String.prototype.startsWith = function(searchString, position){
       position = position || 0;
