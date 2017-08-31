18aeae1d8174e22a01692f20c4a167ef454120a5
Emma Cushman <git.commit@auto.emmalem.ma>
2017-08-30 19:10:36 -0700

    remove unused StandardModelExtensions

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def klass
+    self.class.to_s
+  end
+
+  def user_display
+    self.user.nil? ? nil : self.user.display
+  end
 end
