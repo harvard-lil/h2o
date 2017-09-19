b06d69d9b8200a0f9d34a1c3e921666bb5b895ba
Ben Steinberg <bsteinberg@law.harvard.edu>
2017-09-19 10:56:25 -0400

    Interpolate default from: in Notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default from: 'noreply@opencasebook.org',
+  default from: ENV["EXCEPTION_EMAIL_SENDER"],
           sent_on: Proc.new { Time.now }
 
   def password_reset_instructions(user)
