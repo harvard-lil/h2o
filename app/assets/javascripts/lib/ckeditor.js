26e115f805aa0b2e1c43c8fcc9e92fcac68d21af
Casey Gruppioni <cgruppioni@gmail.com>
2017-10-04 17:05:27 -0400

    Fix ckeditor for textblocks.

--- app/assets/javascripts/lib/ckeditor.js
+++ app/assets/javascripts/lib/ckeditor.js
@@ -4,7 +4,7 @@ document.addEventListener('turbolinks:load', e => {
   for(name in CKEDITOR.instances) {
       CKEDITOR.instances[name].destroy(true);
   }
-  for (let el of document.querySelectorAll('[data-ckeditor]')) {
+  for (let el of document.querySelectorAll('.ckeditor')) {
     CKEDITOR.replace(el.id, {toolbar: 'mini'});
   }
 });
