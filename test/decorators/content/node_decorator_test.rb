27ab4538a0bbb6bdbb8663e2d953c9cef2c7c918
Casey Gruppioni <cgruppioni@gmail.com>
2017-10-18 18:43:48 -0400

    NodeDecorator test attempt.

---
+++ test/decorators/content/node_decorator_test.rb
@@ -0,0 +1,16 @@
+require 'application_system_test_case'
+
+class Content::NodeDecoratorTest < Draper::TestCase
+  describe 'action_buttons' do
+    # scenario 'renders casebook draft buttons' do
+      # casebook = content_nodes(:public_casebook)
+
+      # casebook = Content::Casebook.stub()
+
+      # content = casebook.decorate(context: {action_name: 'layout', casebook: casebook})
+
+      # assert_content content.action_buttons, ''
+    # end
+  end
+
+end
