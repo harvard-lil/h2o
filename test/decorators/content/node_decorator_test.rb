65d860a0a3f4854c68b50f22e70b3d4bb58bb289
Casey Gruppioni <cgruppioni@gmail.com>
2017-10-18 19:03:25 -0400

    NodeDecorator test attempt.

--- test/decorators/content/node_decorator_test.rb
+++ test/decorators/content/node_decorator_test.rb
@@ -1,16 +1,15 @@
 require 'application_system_test_case'
 
-class Content::NodeDecoratorTest < Draper::TestCase
+class Content::NodeDecoratorTest < ApplicationSystemTestCase
   describe 'action_buttons' do
-    # scenario 'renders casebook draft buttons' do
-      # casebook = content_nodes(:public_casebook)
+    scenario 'renders casebook draft buttons' do
+      casebook = content_nodes(:private_casebook)
 
-      # casebook = Content::Casebook.stub()
+      content = casebook.decorate(context: {action_name: 'layout', casebook: casebook})
+      ## undefined method casebook_path
+      ## node_decorator#28
 
-      # content = casebook.decorate(context: {action_name: 'layout', casebook: casebook})
-
-      # assert_content content.action_buttons, ''
-    # end
+      assert_content content.action_buttons
+    end
   end
-
 end
