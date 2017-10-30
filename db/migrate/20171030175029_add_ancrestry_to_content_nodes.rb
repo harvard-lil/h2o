d6ecf4a35b88e8bea5fa8307db4c597fe7d0aa2a
Casey Gruppioni <cgruppioni@gmail.com>
2017-10-30 17:38:59 -0400

    Make migration up/down.

--- db/migrate/20171030175029_add_ancrestry_to_content_nodes.rb
+++ db/migrate/20171030175029_add_ancrestry_to_content_nodes.rb
@@ -1,6 +1,11 @@
 class AddAncrestryToContentNodes < ActiveRecord::Migration[5.1]
-  def change
+  def up 
     add_column :content_nodes, :ancestry, :string
     add_index :content_nodes, :ancestry
+  end
+
+  def down
+    remove_column :content_nodes, :ancestry
+    remove_index :content_nodes, :ancestry
   end
 end
