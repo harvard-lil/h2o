d9b9bab905c918eb6f9924db819286de3f61e3fc
Casey Gruppioni <cgruppioni@gmail.com>
2017-11-17 16:58:17 -0500

    Correct export paths for section & resource.

--- app/decorators/content/node_decorator.rb
+++ app/decorators/content/node_decorator.rb
@@ -40,7 +40,7 @@ class Content::NodeDecorator < Draper::Decorator
     button_to(I18n.t('content.actions.add-section'), sections_path(casebook, params: {parent: section.try(:id)}), method: :post, class: 'action add-section') +
     link_to(I18n.t('content.actions.save'), edit_section_path(casebook, section), class: 'action one-line save submit-section-details') +
     link_to(I18n.t('content.actions.cancel'), edit_section_path(casebook, section), class: 'action one-line cancel') +
-    link_to(I18n.t('content.actions.export'), export_section_path(section), class: 'action one-line export')
+    link_to(I18n.t('content.actions.export'), section_export_path(section), class: 'action one-line export')
   end
 
   def resource_draft
@@ -48,10 +48,10 @@ class Content::NodeDecorator < Draper::Decorator
       link_to(I18n.t('content.actions.preview'), resource_path(casebook, resource), class: 'action one-line preview') +
       link_to(I18n.t('content.actions.save'), edit_resource_path(casebook, resource), class: 'action one-line save submit-edit-details') +
       link_to(I18n.t('content.actions.cancel'), edit_resource_path(casebook, resource), class: 'action one-line cancel') +
-      link_to(I18n.t('content.actions.export'), export_resource_path(resource), class: 'action one-line export')
+      link_to(I18n.t('content.actions.export'), resource_export_path(resource), class: 'action one-line export')
     else
       link_to(I18n.t('content.actions.preview'), resource_path(casebook, resource), class: 'action one-line preview') +
-      link_to(I18n.t('content.actions.export'), export_resource_path(resource), class: 'action one-line export')
+      link_to(I18n.t('content.actions.export'), resource_export_path(resource), class: 'action one-line export')
     end
   end
 
@@ -75,13 +75,13 @@ class Content::NodeDecorator < Draper::Decorator
   def section_preview
     link_to(I18n.t('content.actions.revise'), layout_section_path(casebook, section), class: 'action edit one-line') +
     button_to(I18n.t('content.actions.clone-casebook'), clone_casebook_path(casebook), method: :post, class: 'action clone-casebook') +
-    link_to(I18n.t('content.actions.export'), export_section_path(section), class: 'action one-line export') 
+    link_to(I18n.t('content.actions.export'), section_export_path(section), class: 'action one-line export') 
   end
 
   def resource_preview
     link_to(I18n.t('content.actions.revise'), annotate_resource_path(casebook, resource), class: 'action edit one-line') +
     button_to(I18n.t('content.actions.clone-casebook'), clone_casebook_path(casebook), method: :post, class: 'action clone-casebook') +
-    link_to(I18n.t('content.actions.export'), export_resource_path(resource), class: 'action one-line export') 
+    link_to(I18n.t('content.actions.export'), resource_export_path(resource), class: 'action one-line export') 
   end
 
   def published_action_buttons
@@ -109,10 +109,10 @@ class Content::NodeDecorator < Draper::Decorator
     if owner?
       link_to(I18n.t('content.actions.revise'), edit_section_path(casebook, section), class: 'action edit one-line') +
       button_to(I18n.t('content.actions.clone-casebook'), clone_casebook_path(casebook), method: :post, class: 'action clone-casebook') +
-      link_to(I18n.t('content.actions.export'), export_section_path(section), class: 'action one-line export') 
+      link_to(I18n.t('content.actions.export'), section_export_path(section), class: 'action one-line export') 
     else
       button_to(I18n.t('content.actions.clone-casebook'), clone_casebook_path(casebook), method: :post, class: 'action clone-casebook') +
-      link_to(I18n.t('content.actions.export'), export_section_path(section), class: 'action one-line export') 
+      link_to(I18n.t('content.actions.export'), section_export_path(section), class: 'action one-line export') 
     end
   end
 
@@ -120,10 +120,10 @@ class Content::NodeDecorator < Draper::Decorator
     if owner?
       link_to(I18n.t('content.actions.revise'), annotate_resource_path(casebook, resource), class: 'action edit one-line') +
       button_to(I18n.t('content.actions.clone-casebook'), clone_casebook_path(casebook), method: :post, class: 'action clone-casebook') +
-      link_to(I18n.t('content.actions.export'), export_resource_path(resource), class: 'action one-line export') 
+      link_to(I18n.t('content.actions.export'), resource_export_path(resource), class: 'action one-line export') 
     else
       button_to(I18n.t('content.actions.clone-casebook'), clone_casebook_path(casebook), method: :post, class: 'action clone-casebook') +
-      link_to(I18n.t('content.actions.export'), export_resource_path(resource), class: 'action one-line export') 
+      link_to(I18n.t('content.actions.export'), resource_export_path(resource), class: 'action one-line export') 
     end
   end
 
