04860acb22fe008fd432b02bee850e87b178af54
Casey Gruppioni <cgruppioni@gmail.com>
2017-12-06 16:42:02 -0500

    Render author names.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,13 @@
 class AuthorsController < ApplicationController
   def index
-    a = ['ice cream', 'hamburger', 'potato']
+    users = []
+    owners = Content::Collaborator.where(role: 'owner')
+    
+    owners.each do |owner|
+      users << User.find(owner.user_id).display_name
+    end
+    
+    a = users.sort.uniq
     render json: a
   end
 end
