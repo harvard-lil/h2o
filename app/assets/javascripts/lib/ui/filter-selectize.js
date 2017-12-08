f5c20b4a58cc79ee870148d8a438ef5426fbd546
Casey Gruppioni <cgruppioni@gmail.com>
2017-12-08 17:00:42 -0500

    Set homogenous style for filter boxes (included sort by).

--- app/assets/javascripts/lib/ui/filter-selectize.js
+++ app/assets/javascripts/lib/ui/filter-selectize.js
@@ -1,11 +1,9 @@
 document.addEventListener('turbolinks:load', e => {
-  $( "#search_author" ).select2({
-    theme: "bootstrap"
-  });
+  $( "#search_author" ).select2({});
 
-  $( "#search_school" ).select2({
-    theme: "bootstrap"
-  });
+  $( "#search_school" ).select2({});
+
+  $( "#search_sort" ).select2({});
 
   $('#search_author').on('select2:select', function (e) {
     e.target.closest('form').submit();
