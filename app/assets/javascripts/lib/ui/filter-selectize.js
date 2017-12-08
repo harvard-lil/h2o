c774a37dcf02929f46030cff62d4efe059466dcf
Casey Gruppioni <cgruppioni@gmail.com>
2017-12-08 16:38:48 -0500

    Filters with selectize css show up on initial browse page direct -- and selecting one submits form.

--- app/assets/javascripts/lib/ui/filter-selectize.js
+++ app/assets/javascripts/lib/ui/filter-selectize.js
@@ -1,15 +1,18 @@
-$( "#search_author" ).select2({
-  theme: "bootstrap"
+document.addEventListener('turbolinks:load', e => {
+  $( "#search_author" ).select2({
+    theme: "bootstrap"
+  });
+
+  $( "#search_school" ).select2({
+    theme: "bootstrap"
+  });
+
+  $('#search_author').on('select2:select', function (e) {
+    e.target.closest('form').submit();
+  });
+
+  $('#search_school').on('select2:select', function (e) {
+    e.target.closest('form').submit();
+  });
 });
 
-$( "#search_school" ).select2({
-  theme: "bootstrap"
-});
-
-$('#search_author').on('select2:select', function (e) {
-  e.target.closest('form').submit();
-});
-
-$('#search_school').on('select2:select', function (e) {
-  e.target.closest('form').submit();
-});
