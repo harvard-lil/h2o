1f9eb19f0dc43a1d0bb6868e25dac5c0acc6ec7c
Casey Gruppioni <cgruppioni@gmail.com>
2018-01-05 14:13:50 -0500

    Remove urldomainformatter code that removed file path.

--- app/services/url_domain_formatter.rb
+++ app/services/url_domain_formatter.rb
@@ -3,8 +3,6 @@ class UrlDomainFormatter
     if !URI.parse(domain).host
       domain.prepend("http://")
     end
-    # remove file path
-    domain = URI.join(domain, "/").to_s
     
     domain
   end
