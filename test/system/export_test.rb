e1df3ded35dce0498d8d5f31408b8a919f151e39
Casey Gruppioni <cgruppioni@gmail.com>
2018-01-11 12:22:42 -0500

    Add puts statements.

--- test/system/export_test.rb
+++ test/system/export_test.rb
@@ -11,12 +11,15 @@ class ExportSystemTest < ApplicationSystemTestCase
     export_test  resource_path(content_nodes(:public_casebook), content_nodes(:'public_casebook_section_1_1')), 'Word', 'test_export_section_2.docx'
   end
   scenario 'exporting a casebook to .pdf', js:true do
+    puts "exporting a casebook to .pdf"
     export_test casebook_path(content_nodes(:public_casebook)), 'PDF', 'test_export_casebook.pdf'
   end
   scenario 'exporting a section to .pdf', js:true do
+    puts "exporting a section to .pdf"
     export_test  section_path(content_nodes(:public_casebook), content_nodes(:public_casebook_section_1)), 'PDF', 'test_export_section.pdf'
   end
   scenario 'exporting a section to .pdf', js:true do
+    puts "exporting a section to .pdf"
     export_test  resource_path(content_nodes(:public_casebook), content_nodes(:'public_casebook_section_1_1')), 'PDF', 'test_export_section_2.pdf'
   end
 
@@ -33,7 +36,14 @@ class ExportSystemTest < ApplicationSystemTestCase
 
     exported_file_url = evaluate_script('_test_window_urls').last
     downloaded_path = download_file exported_file_url, to: file
-    assert_equal File.size?(downloaded_path), File.size?(expected_file_path(file))
+
+    puts "actual"
+    puts File.size?(downloaded_path)
+    puts exported_file_url
+    puts "expected"
+    puts File.size?(expected_file_path(file))
+
+    assert_equal File.size?(expected_file_path(file)), File.size?(downloaded_path)
     assert_equal Digest::SHA256.file(expected_file_path(file)).hexdigest, Digest::SHA256.file(downloaded_path).hexdigest
   end
 end
