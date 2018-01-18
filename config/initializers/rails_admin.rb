12b5c85645fa09b98dd1d6c9e8785ff3f1ea8c49
Casey Gruppioni <cgruppioni@gmail.com>
2018-01-18 16:44:44 -0500

    Remove user field professor_verification_sent and used params to check logic instead.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -253,7 +253,6 @@ RailsAdmin.config do |config|
       field :verified_email
       field :verified_professor
       field :professor_verification_requested
-      field :professor_verification_sent
       field :login_count
       field :last_login_at
     end
--- db/migrate/20180116200429_add_professor_verification_sent_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProfessorVerificationSentToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :professor_verification_sent, :boolean, index: true, default: false
-  end
-end
