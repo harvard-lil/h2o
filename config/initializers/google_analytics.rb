1a42a81bbf07051cfa6a062887ea94ccedf83796
Ben Steinberg <bsteinberg@law.harvard.edu>
2018-01-18 10:24:41 -0500

    Remove Google Analytics, condition our analytics on tier

--- config/initializers/google_analytics.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env == 'production' || Rails.env == 'staging'
-  GOOGLE_ANALYTICS_CODE = nil
-elsif Rails.env == 'test'
-    GOOGLE_ANALYTICS_CODE = nil
-else
-  GOOGLE_ANALYTICS_CODE = 'UA-223559-16'
-end
