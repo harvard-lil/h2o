fba3119b1985c27dea5e8a37bcadfa876f39eeee
Casey Gruppioni <cgruppioni@gmail.com>
2018-01-26 13:18:34 -0500

    Add down scripts to migrations.

--- db/migrate/20180111182959_rename_users_verified_to_email_verified.rb
+++ db/migrate/20180111182959_rename_users_verified_to_email_verified.rb
@@ -1,5 +1,9 @@
 class RenameUsersVerifiedToEmailVerified < ActiveRecord::Migration[5.1]
-  def change
+  def up
     rename_column :users, :verified, :verified_email
+  end
+
+  def down
+    rename_column :users, :verified_email, :verified
   end
 end
--- db/migrate/20180116185712_add_verified_to_users.rb
+++ db/migrate/20180116185712_add_verified_to_users.rb
@@ -1,6 +1,11 @@
 class AddVerifiedToUsers < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :users, :verified_professor, :boolean, index: true, default: false
     add_column :users, :professor_verification_requested, :boolean, index: true, default: false
+  end
+
+  def down
+    remove_column :users, :verified_professor, :boolean, index: true, default: false
+    remove_column :users, :professor_verification_requested, :boolean, index: true, default: false
   end
 end
