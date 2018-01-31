4b0e2eae0a4bcb50811307fa79fc4114a0ce88b3
Casey Gruppioni <cgruppioni@gmail.com>
2018-01-31 11:36:17 -0500

    Delete old code in user_sweeper.

--- app/sweepers/user_sweeper.rb
+++ app/sweepers/user_sweeper.rb
@@ -3,28 +3,10 @@ class UserSweeper < ActionController::Caching::Sweeper
   include SweeperHelper
   observe User
 
-  def after_users_delete_bookmark_item
-    if current_user
-      Rails.cache.delete("user-bookmarks-#{current_user.id}")
-      Rails.cache.delete("user-bookmarks-map-#{current_user.id}")
-    end
-  end
-
-  def after_users_bookmark_item
-    if current_user
-      Rails.cache.delete("user-bookmarks-#{current_user.id}")
-      Rails.cache.delete("user-bookmarks-map-#{current_user.id}")
-    end
-  end
-
   def after_update(record)
     if record.saved_changes.keys.include?("attribution")
       Sunspot.index record.all_items
       Sunspot.commit
-    end
-
-    if record.saved_changes.keys.include?("description")
-      record.playlists.each {|playlist| playlist.clear_page_cache}
     end
   end
 end
