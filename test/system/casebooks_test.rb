757628821bfd6626325438abdb64adf587def9f8
Casey Gruppioni <cgruppioni@gmail.com>
2018-02-12 16:00:29 -0500

    Fix test - bad fixture name.

--- test/system/casebooks_test.rb
+++ test/system/casebooks_test.rb
@@ -77,7 +77,7 @@ class CasebookSystemTest < ApplicationSystemTestCase
 
     scenario 'annotating a casebook', js: true do
       casebook = content_nodes(:draft_casebook)
-      resource = content_nodes(:'draft_casebook_section_3')
+      resource = content_nodes(:'draft_casebook_section_1_2')
 
       visit annotate_resource_path casebook, resource
 
