bcf47af5091abb8e595a0c51a6cb55809b23f89f
Casey Gruppioni <cgruppioni@gmail.com>
2018-02-20 12:12:33 -0500

    Remove annotation copy_of_id from schema.

--- db/migrate/20180207220906_add_copy_of_id_to_annotations.rb
+++
@@ -1,9 +0,0 @@
-class AddCopyOfIdToAnnotations < ActiveRecord::Migration[5.1]
-  def up
-    add_column :content_annotations, :copy_of_id, :bigint, index: true
-  end
-
-  def down
-    remove_column :content_annotations, :copy_of_id
-  end
-end
