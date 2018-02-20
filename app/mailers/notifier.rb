10a3900d7f8c0788f1b622db9e821bc57fb4822c
Casey Gruppioni <cgruppioni@gmail.com>
2018-02-20 15:11:31 -0500

    Syntax fixes.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,5 @@
-class Notifier < ActionMailer::Base default from: 'noreply@opencasebook.org',
+class Notifier < ActionMailer::Base
+  default from: 'noreply@opencasebook.org',
           sent_on: Proc.new { Time.now }
 
   def password_reset_instructions(user)
@@ -10,7 +11,8 @@ class Notifier < ActionMailer::Base default from: 'noreply@opencasebook.org',
   def verification_request(user)
     @verification_url = verify_user_url(user, token: user.perishable_token)
     @user_name = user.display_name
-    mail(to: user.email_address, subject: "H2O: Verify your email address") ExceptionNotificationend
+    mail(to: user.email_address, subject: "H2O: Verify your email address")
+  end
 
   def verification_notice(user)
     mail(to: user.email_address, subject: "Welcome to H2O. Your account has been verified")
