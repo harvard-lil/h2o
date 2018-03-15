39d97356de3cd47112ae12efd84d6d04e657d129
Casey Gruppioni <cgruppioni@gmail.com>
2018-03-15 11:32:36 -0400

    Add back modals, add events to super so that super.destroy() works.

--- app/assets/javascripts/lib/ui/content/clone_modal.js
+++ app/assets/javascripts/lib/ui/content/clone_modal.js
@@ -1,44 +1,48 @@
-// import {html} from 'es6-string-html-template';
-// import delegate from 'delegate';
-// import Component from 'lib/ui/component';
+import {html} from 'es6-string-html-template';
+import delegate from 'delegate';
+import Component from 'lib/ui/component';
 
-// delegate(document, '.clone-casebook', 'submit', showCloneModal);
+delegate(document, '.clone-casebook', 'submit', showCloneModal);
 
-// let modal = null;
+let modal = null;
 
-// document.addEventListener("turbolinks:before-cache", function() {
-//   if (modal) { modal.destroy(); }
-// });
+document.addEventListener("turbolinks:before-cache", function() {
+  if (modal) { modal.destroy(); }
+});
 
-// function showCloneModal (e) {
-//   modal = new CloneModal;
-// }
+function showCloneModal (e) {
+  modal = new CloneModal;
+}
 
-// class CloneModal extends Component {
-//   constructor () {
-//     super({id: 'clone-modal'});
-//     document.body.appendChild(this.el);
-//     this.render();
-//   }
+class CloneModal extends Component {
+  constructor () {
+    super({
+      id: 'clone-modal',
+      events: {}
+    });
+    document.body.appendChild(this.el);
+    this.render();
+  }
 
-//   destroy () {
-//     modal = null;
-//   }
+  destroy () {
+    super.destroy();
+    modal = null;
+  }
 
-//   template () {
-//     return html`<div class="modal fade in" id="publish-modal" style="display: block">
-//       <div class="modal-dialog" role="document">
-//         <div class="modal-content">
-//           <div class="spinner-group">
-//             <p class="spinner-header">Cloning casebook</p>
-//             <div class="spinner">
-//               <div class="bounce1"></div>
-//               <div class="bounce2"></div>
-//               <div class="bounce3"></div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>`
-//   }
-// }
+  template () {
+    return html`<div class="modal fade in" id="publish-modal" style="display: block">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="spinner-group">
+            <p class="spinner-header">Cloning casebook</p>
+            <div class="spinner">
+              <div class="bounce1"></div>
+              <div class="bounce2"></div>
+              <div class="bounce3"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+  }
+}
