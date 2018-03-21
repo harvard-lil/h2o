d76d4e79b7e02344f0275f56711c24bc7ae02922
Casey Gruppioni <cgruppioni@gmail.com>
2018-03-21 13:14:55 -0400

    Remove turbolinks from assets, destroy modal.

--- app/assets/javascripts/lib/turbolinks/requests.js
+++ app/assets/javascripts/lib/turbolinks/requests.js
@@ -1,18 +1,11 @@
-import Turbolinks from 'turbolinks';
 import Axios from 'axios';
 
 export function request (url, method, data = {}, options = {scroll: true}) {
-    let progressBar = new Turbolinks.ProgressBar;
-    progressBar.show();
-
     let promise = Axios.post(url, data, {
       headers: {
         'X-HTTP-Method-Override': method,
         'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').getAttribute('content')
       },
-      onDownloadProgress: progress => {
-        progressBar.setValue(progress.loaded / (progress.total || 10000))
-      }
     });
 
     promise.catch(e => {
@@ -22,13 +15,13 @@ export function request (url, method, data = {}, options = {scroll: true}) {
     .then(response => {
       let html = response.data;
       let location = response.request.responseURL;
-      Turbolinks.controller.cache.put(Turbolinks.Location.wrap(location), Turbolinks.Snapshot.fromHTML(html));
-      if (!options.scroll) {
-        Turbolinks.keepScrollPosition();
+      window.location.replace(location);
+
+      if (options["modal"]){
+        options["modal"].destroy();
       }
-      Turbolinks.visit(location, {action: 'restore'});
+
     })
-    .finally(_ => progressBar.hide())
     .done();
 
     return promise;
