51f8ecb3b98ace35620fe52419016572845204aa
Casey Gruppioni <cgruppioni@gmail.com>
2018-04-12 22:17:57 -0400

    Save page location when annotating.

--- app/assets/javascripts/lib/requests.js
+++ app/assets/javascripts/lib/requests.js
@@ -15,7 +15,7 @@ export function request (url, method, data = {}, options = {scroll: true}) {
     .then(response => {
       let html = response.data;
       let location = response.request.responseURL;
-      window.location.replace(location);
+      document.location.reload(true);
 
       if (options["modal"]){
         options["modal"].destroy();
@@ -26,6 +26,7 @@ export function request (url, method, data = {}, options = {scroll: true}) {
 
     return promise;
 }
+
 export var get = (url, data = {}, options = {}) => request(url, 'get', data, options)
 export var post = (url, data = {}, options = {}) => request(url, 'post', data, options)
 export var rest_delete = (url, data = {}, options = {}) => request(url, 'delete', data, options)
