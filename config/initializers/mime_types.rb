4c05791bb265bc878e2a130e5074d2e063e2083b
Casey Gruppioni <casey@appcues.com>
2018-04-20 14:39:52 -0400

    Delete comment.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,3 +1,2 @@
 Mime::Type.register "image/svg+xml", :svg
 Mime::Type.register "application/vnd.openxmlformats-officedocument.wordprocessingml.document", :docx
-commented out b/c of https://github.com/karnov/htmltoword#with-rails
