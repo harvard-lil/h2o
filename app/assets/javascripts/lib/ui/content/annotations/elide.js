7c335c41a4357c2a7124ec9611cfd3810c50ade9
Rebecca Cremona <rcremona@law.harvard.edu>
2018-05-30 16:20:31 -0400

    First pass, elision accessibility.

--- app/assets/javascripts/lib/ui/content/annotations/elide.js
+++ app/assets/javascripts/lib/ui/content/annotations/elide.js
@@ -8,7 +8,14 @@ delegate(document, '.annotate.elide', 'click', e => {
   let annotationId = e.target.dataset.annotationId;
   let elisions = document.querySelectorAll(`.annotate.elided[data-annotation-id="${annotationId}"]`);
 
-  e.target.classList.toggle('revealed')
+  e.target.classList.toggle('revealed');
+  if (e.target.classList.contains('revealed')){
+    e.target.setAttribute('aria-expanded', 'true');
+    elisions[elisions.length - 1].insertAdjacentHTML('afterend', `<span class="annotate elided revealed sr-only" data-annotation-id="${annotationId}">(end of elided text)</span>`);
+  } else {
+    e.target.setAttribute('aria-expanded', 'false');
+    elisions[elisions.length - 1].remove();
+  }
   for (let el of elisions) {
     el.classList.toggle('revealed');
   }
