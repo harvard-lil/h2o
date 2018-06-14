b04d144288a8b89511e22c2541073c74e7ea4e3b
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-14 13:28:58 -0400

    clicking note text highlights all annotated lines, not seperated by p tag.

--- app/assets/javascripts/lib/ui/content/annotations/note.js
+++ app/assets/javascripts/lib/ui/content/annotations/note.js
@@ -2,15 +2,25 @@ import delegate from 'delegate';
 
 delegate(document, '.annotate.note', 'click', e => {
   let annotationId = e.target.dataset.annotationId;
+
   document.querySelector(`.annotate.note-content-wrapper[data-annotation-id="${annotationId}"]`)
   .classList.toggle('revealed');
-  document.querySelector(`.annotate.note[data-annotation-id="${annotationId}"]`).classList.toggle('revealed');
+
+  let annotatedLines = document.querySelectorAll(`.annotate.note[data-annotation-id="${annotationId}"]`);
+  for (let line of annotatedLines) {
+    line.classList.toggle('revealed');
+  }
 });
 
 
 delegate(document, '.note-icon', 'click', e => {
   let annotationId = e.delegateTarget.dataset.annotationId;
+  
   document.querySelector(`.annotate.note-content-wrapper[data-annotation-id="${annotationId}"]`)
   .classList.toggle('revealed');
-  document.querySelector(`.annotate.note[data-annotation-id="${annotationId}"]`).classList.toggle('revealed');
+
+  let annotatedLines = document.querySelectorAll(`.annotate.note[data-annotation-id="${annotationId}"]`);
+  for (let line of annotatedLines) {
+    line.classList.toggle('revealed');
+  }
 });
