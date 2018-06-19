04bf31d45652d2ba6349cb000e959de3ac0529a8
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-19 18:21:02 -0400

    VerifySinglePlaylist able to run in console but with errors.
    WIP loopin through.
    
    WIP

---
+++ lib/variables_for_playlist_items.rb
@@ -0,0 +1,9 @@
+# Variables
+casebook = Content::Casebook.find(17493)
+resource = casebook.resources.find_by(ordinals: [1, 1, 1])
+playlist = Migrate::Playlist.find(casebook.playlist_id)
+section_1 = Migrate::Playlist.find(playlist.playlist_items.first.actual_object_id)
+item = Migrate::Playlist.find(section_1.playlist_items.find_by(position: 1).actual_object_id).playlist_items.find_by(position: 1)
+casebook_default = resource.resource
+playlist_default = item.actual_object
+reload! && VerifySinglePlaylist.verify(casebook)
