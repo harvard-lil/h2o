0d7e1f002451e88ba4018d345e601b59642c2ff2
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-20 16:13:25 -0400

    Successfully looping through a playlist thanks to previous Migrate code.

--- lib/verify_single_playlist.rb
+++ lib/verify_single_playlist.rb
@@ -1,5 +1,5 @@
 class VerifySinglePlaylist
-  attr_accessor :casebook, :playlist, :casebook_sections, :free_resources, :bad_resources
+  attr_accessor :casebook, :playlist, :casebook_sections, :free_resources, :bad_resources, :comments
 
   def self.verify(casebook)
     new(casebook).verify
@@ -10,83 +10,84 @@ class VerifySinglePlaylist
     @playlist = Migrate::Playlist.find casebook.playlist_id
     @casebook_sections = Content::Section.where(casebook_id: casebook.id)
     @bad_resources = []
+    @comments = true
   end
 
   def verify
-    loop_playlist_items(playlist, path: [])
+    loop_playlist_items(playlist.playlist_items, path: [])
   end
 
-  def loop_playlist_items(playlist, path:)
-    puts "*************"
-    puts "1 *~*"
-    puts "starting loop_playlist_items"
-    puts "playlist"
-    puts "+ #{playlist.inspect}"
-    puts "path"
-    puts "+ #{path}"
-    playlist.playlist_items.sort.each_with_index do |item, index|
-      puts "*************"
-      puts "2 *~*"
-      puts "item"
-      puts "+ #{item.inspect}"
-      ordinals = path + [index + 1]
-      puts "ordinals"
-      puts "+ #{ordinals}"
-      if item.actual_object_type == "Playlist"
-        puts "*************"
-        puts "3 *~*"
-        puts "TYPE"
-        puts "Playlist"
-        item_playlist = Migrate::Playlist.find(item.actual_object_id)
-        loop_playlist_items(item_playlist, path: ordinals)
-      else
-        puts "*************"
-        puts "4 *~*"
-        puts "TYPE"
-        puts item.actual_object_type
-        casebook_content = casebook.contents.find_by(ordinals: ordinals)
-
-        puts "*************"
-        puts "5 *~*"
-        puts "casebook content"
-        puts "+ #{casebook_content.inspect}"
-        # ordinals contain sections but sections don't have resources so not sure what I'm trying to do here. I don't think return is working? 
-        # need to think about looping through playlist playlit items that are sections and also using the items that aren't in a section 
-
-        if casebook_content.is_a? Content::Section
-          puts "*************"
-          puts "6 *~*"
-          puts "In section suposed to be returning"
-          return
-        else
-          resource = casebook_content.resource
-        end
-
-        puts "*************"
-        puts "7 *~*"
-        puts "outside of if/else"
-
-        playlist_item = item.actual_object
-        puts "*************"
-        puts "8 *~*"
-        puts "playlist item"
-        puts " + #{playlist_item.inspect}"
-
-        puts "resource "
-        puts "+ #{resource.inspect}"
-
-        if casebook_content != playlist_item
-          puts "*************"
-          puts "9 *~*"
-          puts "BAD resource"
-          bad_resources << [resource: resource, playlist: playlist_item]
-        end
+  def loop_playlist_items(playlist_items, path:)
+    # if comments == true
+    #   puts "*************"
+    #   puts "1 *~*"
+    #   puts "starting loop_playlist_items"
+    #   puts "playlist"
+    #   puts "+ #{playlist.inspect}"
+    #   puts "path"
+    #   puts "+ #{path}"
+    # end
+    playlist_items.order(:position).each_with_index do |item, index|
+      if item.actual_object_type.in? %w{Playlist Collage Media}
+        item.actual_object_type = "Migrate::#{item.actual_object_type}"
       end
-    end
+
+      object = item.actual_object
+      ordinals = path + [index + 1]
+
+      if object.is_a? Migrate::Playlist
+        loop_playlist_items object.playlist_items, path: ordinals
+      else
+        imported_resource = object
+
+        # if comment == true
+        #   # if imported_resource.nil?
+        #   #   bad_resources << [message: "nil imported_resource", type: item.actual_object_type, id: item.actual_object_id, url: "https://h2o.law.harvard.edu/#{item.actual_object_type.downcase}s/#{item.actual_object_id}"]
+        #   # end
+        # end
+
+        casebook_resource = casebook.resources.find_by(ordinals: ordinals)
+
+        puts "*****"
+        puts ordinals
+        puts casebook_resource.inspect
+      end
+      # if comment == true
+        # if imported_resource.is_a? Migrate::Collage
+        #   if imported_resource.annotatable_type == "Case"
+        #     casebook.resources.find_by(ordinals: ordinals)
+        #   elsif imported_resource.annotatable_type == "TextBlock"
+        #   end
+
+          # if imported_resource.annotatable_type.in? %w{Playlist Collage Media}
+          #   imported_resource.annotatable_type = "Migrate::#{object.annotatable_type}"
+          # end
+          # imported_resource = imported_resource.annotatable
+
+          # if imported_resource.nil?
+          #   bad_resources << [message: "nil imported_resource", type: item.actual_object_type, id: item.actual_object_id, url: "https://h2o.law.harvard.edu/#{item.actual_object_type.downcase}s/#{item.actual_object_id}"]
+          # end
+        end
+      # end
+
+      # if imported_resource.is_a? Migrate::Media # this is a default
+      # end
+
+      # if casebook_content.is_a? Content::Section
+      #   return
+      # else
+      #   resource = casebook_content.resource
+      # end
+
+      # if casebook_content != playlist_item
+      #   # if comments == true
+      #   #   puts "*************"
+      #   #   puts "8 *~*"
+      #   #   puts "BAD resource"
+      #   # end
+      #   bad_resources << [resource: resource, playlist: playlist_item]
+      # end
   end
-
-  private
-
 end
 
 
