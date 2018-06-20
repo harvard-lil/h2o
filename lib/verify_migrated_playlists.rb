ffcaf2a5940b466cb851f0df63c4a31d15550a91
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-20 16:58:04 -0400

    Loop through a playlist and return false if there are mismatched resources.
    WIP not returning anything in mismatched when running all playlists.

--- lib/verify_migrated_playlists.rb
+++ lib/verify_migrated_playlists.rb
@@ -1,9 +1,28 @@
 module VerifyMigratedPlaylists
   class << self
 
-  def verify
-    casebooks.each do |casebook|
-      VerifySinglePlaylist.verify casebook
+    def verify
+      bad_casebooks = []
+      bad_casebook_items = []
+
+      ## Find way to put query inside of where query 
+
+      Content::Casebook.where.not(playlist_id: nil).where(ancestry: nil).each do |casebook|
+        result = VerifySinglePlaylist.verify(casebook.id)
+
+        if result[:mismatched]
+          binding.pry
+          bad_casebooks << casebook.id
+        end
+
+        if result[:items_without_resources].present?
+          bad_casebook_items << {casebook_id: casebook.id, items: result[:items_without_resources]}
+        end
+      end
+
+      puts "*******"
+      puts bad_casebooks
+      puts "******"
     end
   end
 end
--- lib/verify_single_playlist.rb
+++ lib/verify_single_playlist.rb
@@ -1,32 +1,26 @@
 class VerifySinglePlaylist
-  attr_accessor :casebook, :playlist, :casebook_sections, :free_resources, :bad_resources, :comments
+  attr_accessor :casebook, :playlist, :matches, :comments, :mismatches, :mismatched, :items_without_resources
 
-  def self.verify(casebook)
-    new(casebook).verify
+  def self.verify(casebook_id)
+    new(casebook_id).verify
   end
 
-  def initialize(casebook)
-    @casebook = casebook
+  def initialize(casebook_id)
+    @casebook = Content::Casebook.find(casebook_id)
     @playlist = Migrate::Playlist.find casebook.playlist_id
-    @casebook_sections = Content::Section.where(casebook_id: casebook.id)
-    @bad_resources = []
+    @matches = []
+    @mismatches = []
+    @mismatched = false
     @comments = true
+    @items_without_resources = []
   end
 
   def verify
     loop_playlist_items(playlist.playlist_items, path: [])
+    {mismatched: mismatched, items_without_resources: items_without_resources}
   end
 
   def loop_playlist_items(playlist_items, path:)
-    # if comments == true
-    #   puts "*************"
-    #   puts "1 *~*"
-    #   puts "starting loop_playlist_items"
-    #   puts "playlist"
-    #   puts "+ #{playlist.inspect}"
-    #   puts "path"
-    #   puts "+ #{path}"
-    # end
     playlist_items.order(:position).each_with_index do |item, index|
       if item.actual_object_type.in? %w{Playlist Collage Media}
         item.actual_object_type = "Migrate::#{item.actual_object_type}"
@@ -36,57 +30,24 @@ class VerifySinglePlaylist
       ordinals = path + [index + 1]
 
       if object.is_a? Migrate::Playlist
+        section = casebook.contents.find_by(ordinals: ordinals)
+
+        if section.nil?
+          items_without_resources << object
+        elsif section.title != object.name
+          mismatched = true
+        end
+
         loop_playlist_items object.playlist_items, path: ordinals
       else
-        imported_resource = object
-
-        # if comment == true
-        #   # if imported_resource.nil?
-        #   #   bad_resources << [message: "nil imported_resource", type: item.actual_object_type, id: item.actual_object_id, url: "https://h2o.law.harvard.edu/#{item.actual_object_type.downcase}s/#{item.actual_object_id}"]
-        #   # end
-        # end
-
-        casebook_resource = casebook.resources.find_by(ordinals: ordinals)
-
-        puts "*****"
-        puts ordinals
-        puts casebook_resource.inspect
-      end
-      # if comment == true
-        # if imported_resource.is_a? Migrate::Collage
-        #   if imported_resource.annotatable_type == "Case"
-        #     casebook.resources.find_by(ordinals: ordinals)
-        #   elsif imported_resource.annotatable_type == "TextBlock"
-        #   end
-
-          # if imported_resource.annotatable_type.in? %w{Playlist Collage Media}
-          #   imported_resource.annotatable_type = "Migrate::#{object.annotatable_type}"
-          # end
-          # imported_resource = imported_resource.annotatable
-
-          # if imported_resource.nil?
-          #   bad_resources << [message: "nil imported_resource", type: item.actual_object_type, id: item.actual_object_id, url: "https://h2o.law.harvard.edu/#{item.actual_object_type.downcase}s/#{item.actual_object_id}"]
-          # end
+        resource = casebook.resources.find_by(ordinals: ordinals)
+        if resource.nil?
+          items_without_resources << item
+        elsif object != resource.resource
+          mismatched = true
         end
-      # end
-
-      # if imported_resource.is_a? Migrate::Media # this is a default
-      # end
-
-      # if casebook_content.is_a? Content::Section
-      #   return
-      # else
-      #   resource = casebook_content.resource
-      # end
-
-      # if casebook_content != playlist_item
-      #   # if comments == true
-      #   #   puts "*************"
-      #   #   puts "8 *~*"
-      #   #   puts "BAD resource"
-      #   # end
-      #   bad_resources << [resource: resource, playlist: playlist_item]
-      # end
+      end
+    end
   end
 end
 
