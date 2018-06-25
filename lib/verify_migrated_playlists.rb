69a440d2154c216e60d475cce34bed0869264b16
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-25 15:26:44 -0400

    Look at casebooks not only in timeframe of Jan 3rd.
    WIP
    
    Delete files.

--- lib/verify_migrated_playlists.rb
+++ lib/verify_migrated_playlists.rb
@@ -4,16 +4,7 @@ module VerifyMigratedPlaylists
     def verify
       results = {}
 
-      # only doing updated at on jan 3rd for now.... presumably they would have noticed? Cause now it just shows all changes and they could be purposeful
-      casebooks = Content::Casebook.where.not(playlist_id: nil).where(ancestry: nil).where(updated_at: DateTime.new(2018, 1, 3)..DateTime.new(2018, 1, 4))
-      # irb(main):084:0> a.keys
-      # => [18234, 17378, 20733, 18813, 23698, 23461, 22079, 24799, 26260, 28565]
-      # irb(main):085:0> a.keys.count
-      # => 10
-      # casebooks = Content::Casebook.where.not(playlist_id: nil).where(ancestry: nil)
-      # => 44
-      # irb(main):095:0> a.keys
-      # => [16133, 16530, 16723, 18234, 19353, 23052, 18560, 18672, 17493, 23114, 27225, 29980, 17378, 17751, 20926, 16066, 19750, 20733, 20611, 18813, 20234, 18331, 24680, 20414, 19006, 22411, 23698, 23461, 26936, 22079, 24172, 24799, 26562, 25610, 23303, 23416, 26260, 28024, 27674, 28325, 28565, 32567, 32667, 21268]
+      casebooks = Content::Casebook.where.not(playlist_id: nil).where(ancestry: nil)
 
       casebooks.each do |casebook|
         mismatched = VerifySinglePlaylist.verify(casebook.id)
