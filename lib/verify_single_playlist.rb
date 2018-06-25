4d47277f9a9609cbd70b6268d238142698fa8f6b
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-25 12:56:35 -0400

    Remove items_without_resources & refactor out methods to make more readable.

--- lib/verify_single_playlist.rb
+++ lib/verify_single_playlist.rb
@@ -1,5 +1,5 @@
 class VerifySinglePlaylist
-  attr_accessor :casebook, :playlist, :mismatched, :items_without_resources
+  attr_accessor :casebook, :playlist, :mismatched, :object, :item, :resource, :section
 
   def self.verify(casebook_id)
     new(casebook_id).verify
@@ -7,51 +7,71 @@ class VerifySinglePlaylist
 
   def initialize(casebook_id)
     @casebook = Content::Casebook.find(casebook_id)
-    @playlist = Migrate::Playlist.find casebook.playlist_id
+    @playlist = Migrate::Playlist.find(casebook.playlist_id)
     @mismatched = {}
-    @items_without_resources = {}
   end
 
   def verify
     loop_playlist_items(playlist.playlist_items, path: [])
-    {mismatched: mismatched, items_without_resources: items_without_resources}
+    mismatched
   end
 
   def loop_playlist_items(playlist_items, path:)
     playlist_items.order(:position).each_with_index do |item, index|
-      if item.actual_object_type.in? %w{Playlist Collage Media}
-        item.actual_object_type = "Migrate::#{item.actual_object_type}"
-      end
-
-      object = item.actual_object
+      @item = item
+      @object = item.actual_object
       ordinals = path + [index + 1]
 
-      if object.is_a? Migrate::Playlist
-        section = casebook.contents.find_by(ordinals: ordinals)
+      set_class_names_for_playlist_items
 
-        if section.nil?
-          items_without_resources[ordinals] = object
-        elsif section.title != object.name
+      if loop_again?
+        @section = casebook.contents.find_by(ordinals: ordinals)
+
+        if mismatched_section?
           mismatched[ordinals.join('.')] = {section: section, object: object}
         end
 
-        loop_playlist_items object.playlist_items, path: ordinals
+        loop_playlist_items(object.playlist_items, path: ordinals)
       else
-        resource = casebook.resources.find_by(ordinals: ordinals)
+        @resource = casebook.resources.find_by(ordinals: ordinals)
 
-        if resource.nil? || item.nil? || object.nil? || item.actual_object_type == "Migrate::Media"
-          # videos weren't transported over so any Migrate::Media is counted as missing
-          items_without_resources[ordinals] = item
+        if bad_item_or_resource?
+          return
         elsif item.actual_object_type == "Migrate::Collage"
-          if object.annotatable.nil? 
-            items_without_resources[ordinals] = item
-          elsif object.annotatable.id != resource.resource.id
+          if mismatched_resource?
             mismatched[ordinals.join('.')] = {resource: resource, item: item}
           end
-        elsif object.id != resource.resource.id
+        elsif mismatched_link?
           mismatched[ordinals.join('.')] = {resource: resource, item: item}
         end
       end
+    end
+
+    def set_class_names_for_playlist_items
+      if item.actual_object_type.in? %w{Playlist Collage Media}
+        item.actual_object_type = "Migrate::#{item.actual_object_type}"
+      end
+    end
+
+    def loop_again?
+      object.is_a? Migrate::Playlist
+    end
+
+    def bad_item_or_resource?
+      resource.nil? || item.nil? || object.nil? || item.actual_object_type == "Migrate::Media" || object.annotatable.nil?
+      # videos weren't transported over so any Migrate::Media is counted as missing
+    end
+
+    def mismatched_section?
+      ! section.nil? && section.title != object.name 
+    end
+
+    def mismatched_resource?
+      object.annotatable.id != resource.resource.id 
+    end
+
+    def mismatched_link?
+      object.id != resource.resource.id
     end
   end
 end
