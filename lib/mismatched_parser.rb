366a7ea6c3e1145becfd5902128bca63af976a64
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-25 15:03:09 -0400

    VerifyMigratedPlaylists works.
    Add mismatched parser.

---
+++ lib/mismatched_parser.rb
@@ -0,0 +1,15 @@
+module MismatchedParser
+  class << self
+
+    def parse
+      mismatched = VerifyMigratedPlaylists.verify
+
+      mismatched.each do |entry|
+
+      end
+
+
+
+    end
+  end
+end
--- lib/variables_for_playlist_items.rb
+++ lib/variables_for_playlist_items.rb
@@ -1,11 +1,6 @@
 # Variables
 casebook = Content::Casebook.find(17493)
-resource = casebook.resources.find_by(ordinals: [1, 1, 1])
-playlist = Migrate::Playlist.find(casebook.playlist_id)
-section_1 = Migrate::Playlist.find(playlist.playlist_items.first.actual_object_id)
-item = Migrate::Playlist.find(section_1.playlist_items.find_by(position: 1).actual_object_id).playlist_items.find_by(position: 1)
-casebook_default = resource.resource
-playlist_default = item.actual_object
-reload! && response = VerifySinglePlaylist.verify()
+casebook = Content::Casebook.find(17493)
+mismatched = VerifySinglePlaylist.verify
 
-justin_hughes_link = Default.find(5536)
+
