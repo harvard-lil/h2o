efe392d558aeafd5ccf7c65422283e40879cdaa0
Casey Gruppioni <cgruppioni@gmail.com>
2018-06-25 15:03:09 -0400

    VerifyMigratedPlaylists works.
    Add mismatched parser.

--- lib/verify_single_playlist.rb
+++ lib/verify_single_playlist.rb
@@ -1,5 +1,5 @@
 class VerifySinglePlaylist
-  attr_accessor :casebook, :playlist, :mismatched, :object, :item, :resource, :section
+  attr_accessor :casebook, :playlist, :mismatched, :object, :item, :resource, :section, :ordinals
 
   def self.verify(casebook_id)
     new(casebook_id).verify
@@ -16,14 +16,44 @@ class VerifySinglePlaylist
     mismatched
   end
 
+  def set_variables(item, path, index)
+    @item = item
+    set_class_names_for_playlist_items
+    @object = item.actual_object
+    @ordinals = path + [index + 1]
+  end
+
+  def set_class_names_for_playlist_items
+    if item.actual_object_type.in? %w{Playlist Collage Media}
+      item.actual_object_type = "Migrate::#{item.actual_object_type}"
+    end
+  end
+
+  def loop_again?
+    object.is_a? Migrate::Playlist
+  end
+
+  def bad_item_or_resource?
+    resource.nil? || item.nil? || object.nil? || item.actual_object_type == "Migrate::Media" || object.try(:annotatable)
+    # videos weren't transported over so any Migrate::Media is counted as missing
+  end
+
+  def mismatched_section?
+    ! section.nil? && section.title != object.name 
+  end
+
+  def mismatched_resource?
+    object.annotatable.id != resource.resource.id 
+  end
+
+  def mismatched_link?
+    object.id != resource.resource.id
+  end
+
   def loop_playlist_items(playlist_items, path:)
     playlist_items.order(:position).each_with_index do |item, index|
-      @item = item
-      @object = item.actual_object
-      ordinals = path + [index + 1]
-
-      set_class_names_for_playlist_items
-
+      set_variables(item, path, index)
+      
       if loop_again?
         @section = casebook.contents.find_by(ordinals: ordinals)
 
@@ -45,33 +75,6 @@ class VerifySinglePlaylist
           mismatched[ordinals.join('.')] = {resource: resource, item: item}
         end
       end
-    end
-
-    def set_class_names_for_playlist_items
-      if item.actual_object_type.in? %w{Playlist Collage Media}
-        item.actual_object_type = "Migrate::#{item.actual_object_type}"
-      end
-    end
-
-    def loop_again?
-      object.is_a? Migrate::Playlist
-    end
-
-    def bad_item_or_resource?
-      resource.nil? || item.nil? || object.nil? || item.actual_object_type == "Migrate::Media" || object.annotatable.nil?
-      # videos weren't transported over so any Migrate::Media is counted as missing
-    end
-
-    def mismatched_section?
-      ! section.nil? && section.title != object.name 
-    end
-
-    def mismatched_resource?
-      object.annotatable.id != resource.resource.id 
-    end
-
-    def mismatched_link?
-      object.id != resource.resource.id
     end
   end
 end
