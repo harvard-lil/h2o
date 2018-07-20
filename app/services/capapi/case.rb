513b196137965ad17524721431b40bfc5ecd65a3
Greg Leppert <greg@leppert.me>
2018-07-20 10:28:12 -0400

    Spin out capapi service into a gem

--- app/services/capapi/case.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Capapi
-  class Case < APIResource
-    extend Capapi::APIOperations::List
-    OBJECT_NAME = "case".freeze
-  end
-end
--- app/services/capapi/court.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Capapi
-  class Court < APIResource
-    extend Capapi::APIOperations::List
-    OBJECT_NAME = "court".freeze
-  end
-end
