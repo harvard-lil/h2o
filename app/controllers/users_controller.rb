6d4f225d5270d18add68f6bca64b56a3b775f7dd
Casey Gruppioni <cgruppioni@gmail.com>
2018-07-23 14:42:27 -0400

    Remove verify method from user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,20 +11,6 @@ class UsersController < ApplicationController
     redirect_to search_path(type: 'users')
   end
 
-  def verify
-    if current_user.present? && current_user == User.where(id: params[:id]).first && params[:token] == current_user.perishable_token
-      current_user.update_column(:email_confirmed, true)
-      flash[:notice] = 'Thank you. Your account has been verified. You may now contribute to H2O.'
-      redirect_to user_path(current_user)
-    elsif current_user.present?
-      flash[:notice] = 'Your account has not been verified. Please try again by requesting an email verification <a href="' + verify_user_url(current_user)  + '" target="blank">here</a>.'
-      redirect_to user_path(current_user)
-    else
-      flash[:notice] = 'Your account has not been verified. Please login and try visiting the link in the email again.'
-      redirect_to '/user_sessions/new'
-    end
-  end
-
   def create
     @user = User.new(permitted_user_params)
 
