e65dfa5f2997a37402174ee150bf15963d067768
Casey Gruppioni <cgruppioni@gmail.com>
2018-07-23 14:44:34 -0400

    Remove verified_email field. Remove verification_notice methods
    Remove verified_email field. Remove verification_notice methods.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -238,7 +238,6 @@ RailsAdmin.config do |config|
       field :email_address
       field :created_at
       field :verified_professor
-      field :email_confirmed
       field :professor_verification_requested
       field :login_count
       field :last_login_at
