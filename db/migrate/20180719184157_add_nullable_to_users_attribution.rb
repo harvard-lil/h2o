c9c33f76fa6757250fe6d1a2d928aadae5ec3729
Casey Gruppioni <cgruppioni@gmail.com>
2018-07-23 17:30:44 -0400

    Consolidate user attribution migrations & add down script.

--- db/migrate/20180719184157_add_nullable_to_users_attribution.rb
+++
@@ -1,5 +0,0 @@
-class AddNullableToUsersAttribution < ActiveRecord::Migration[5.1]
-  def change
-    change_column_null :users, :attribution, false
-  end
-end
--- db/migrate/20180719190825_add_default_to_users_attribution.rb
+++ db/migrate/20180719190825_add_default_to_users_attribution.rb
@@ -1,5 +1,11 @@
 class AddDefaultToUsersAttribution < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    change_column_null :users, :attribution, false
     change_column_default :users, :attribution, 'Anonymous'
+  end
+
+  def down
+    change_column_null :users, :attribution, true
+    change_column_default :users, :attribution, nil
   end
 end
