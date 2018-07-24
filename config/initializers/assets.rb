b649a3a7cae1700950b5e01faf5402efde32aec0
Casey Gruppioni <casey@appcues.com>
2018-07-24 19:28:56 -0400

    Delete export css files. They were not being used, lib/htmltoword/xslt/base was.
    Add forgotten paranthesis

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(main.css jquery.ui.custom.css ui.css print.css main.js export.js export.css export/with-annotations.css export/no-annotations.css ckeditor/*)
+Rails.application.config.assets.precompile += %w(main.css jquery.ui.custom.css ui.css print.css main.js ckeditor/*)
