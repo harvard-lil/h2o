536cf2f17c2d01c2c4dcede42727a2d9b5c98089
Casey <cgruppioni@users.noreply.github.com>
2018-07-25 15:55:02 -0400

    Revert "Update copy for new user."
    This reverts commit bd442902f420505fb8b00fd5cad672269c7a92b0.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -35,11 +35,8 @@ class PasswordResetsController < ApplicationController
   def update
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
-    if @user.save && @user.new_user?
+    if @user.save
       confirm_user
-      flash[:notice] = "Thank you. Your account has been verified. You may now contribute to H2O."
-      redirect_to user_path(@user.id)
-    elsif @user.save
       flash[:notice] = "Password successfully updated"
       redirect_to user_path(@user.id)
     else
@@ -61,6 +58,8 @@ class PasswordResetsController < ApplicationController
   end
 
   def confirm_user
-    @user.update(confirmed: true, active: true)
+    if @user.new_user?
+      @user.update(confirmed: true, active: true)
+    end
   end
 end
