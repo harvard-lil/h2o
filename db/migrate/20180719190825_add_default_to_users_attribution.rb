93305ef5a5423c8d67b780d9545704ff72ecc4b5
Greg Leppert <greg@leppert.me>
2018-07-30 15:51:50 -0400

    Add data migration in order to allow schema migration to proceed

--- db/migrate/20180719190825_add_default_to_users_attribution.rb
+++ db/migrate/20180719190825_add_default_to_users_attribution.rb
@@ -1,7 +1,9 @@
 class AddDefaultToUsersAttribution < ActiveRecord::Migration[5.1]
   def up
+    default = 'Anonymous'
+    User.where(attribution: nil).update_all(attribution: default)
     change_column_null :users, :attribution, false
-    change_column_default :users, :attribution, 'Anonymous'
+    change_column_default :users, :attribution, default
   end
 
   def down
