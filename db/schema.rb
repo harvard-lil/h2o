4a2cac8af5b0f561664979ffca80e18783c7a3a0
Greg Leppert <greg@leppert.me>
2018-08-03 10:07:22 -0400

    Remove bookmark_id from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180730203315) do
+ActiveRecord::Schema.define(version: 20180803140207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
