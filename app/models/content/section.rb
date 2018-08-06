5c5865d7691c1a541f193b8dd52c85d849ff9a1c
Casey Gruppioni <cgruppioni@gmail.com>
2018-08-06 10:24:34 -0400

    When destroying a section/resource - destroy all of it's related unpublished revisions.
    When destroying a section/resource - destroy all of it's related unpublished revisions.
    
    TODO: Write Tests. Only delete annotations when merging in a draft if they are present.
    
    Delete unpublished_revisions as associations.

--- app/models/content/section.rb
+++ app/models/content/section.rb
@@ -2,6 +2,7 @@ class Content::Section < Content::Child
   default_scope {where(resource_id: nil)}
 
   has_many :contents, ->(section) {where(['ordinals[1:?] = ARRAY[?]', section.ordinals.length, section.ordinals]).where.not(id: section.id).order :ordinals}, class_name: 'Content::Child', primary_key: :casebook_id, foreign_key: :casebook_id
+  has_many :unpublished_revisions, dependent: :destroy
   include Content::Concerns::HasChildren
 
   def title
