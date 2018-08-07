f684ea3e2da20a0d57dc889a644fd0089ecd1473
Casey Gruppioni <cgruppioni@gmail.com>
2018-08-07 16:32:47 -0400

    Only show unpublished_revisions relaionship in node class.

--- app/models/content/section.rb
+++ app/models/content/section.rb
@@ -2,7 +2,6 @@ class Content::Section < Content::Child
   default_scope {where(resource_id: nil)}
 
   has_many :contents, ->(section) {where(['ordinals[1:?] = ARRAY[?]', section.ordinals.length, section.ordinals]).where.not(id: section.id).order :ordinals}, class_name: 'Content::Child', primary_key: :casebook_id, foreign_key: :casebook_id
-  has_many :unpublished_revisions, dependent: :destroy
   include Content::Concerns::HasChildren
 
   def title
