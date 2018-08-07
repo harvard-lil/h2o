6662424b41494136f5965cc0566ecfd00f3c4271
Greg Leppert <greg@leppert.me>
2018-08-07 12:15:10 -0400

    Add capapi_id to case_courts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20180803142424) do
+ActiveRecord::Schema.define(version: 20180807152558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "btree_gin"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180803142424) do
     t.text "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "capapi_id"
     t.index ["abbreviation"], name: "index_case_courts_on_abbreviation"
     t.index ["name"], name: "index_case_courts_on_name"
   end
