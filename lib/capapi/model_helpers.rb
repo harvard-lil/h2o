815ed0c8f9196ca0387d975c3ee2a856229ef29d
Greg Leppert <greg@leppert.me>
2018-08-08 15:11:09 -0400

    Add a transform between capapi data and h2o

--- lib/capapi/model_helpers.rb
+++ lib/capapi/model_helpers.rb
@@ -19,5 +19,33 @@ module Capapi
         capapi_id = nil
       end
     end
+
+    def self.to_model_attributes capapi_obj
+      case capapi_obj
+      when Capapi::Case
+        attributes = { capapi: capapi_obj,
+                       case_court: to_model_attributes(capapi_obj.court),
+                       name_abbreviation: capapi_obj.name_abbreviation,
+                       name: capapi_obj.name,
+                       decision_date: capapi_obj.decision_date,
+                       docket_number: capapi_obj.docket_number,
+                       citations: capapi_obj.citations.map(&method(:to_model_attributes)) }
+        if capapi_obj.casebody_loaded?
+          attributes.merge({ content: capapi_obj.casebody["data"],
+                             judges: capapi_obj.casebody["judges"],
+                             attorneys: capapi_obj.casebody["attorneys"],
+                             parties: capapi_obj.casebody["parties"],
+                             opinions: capapi_obj.casebody["opinions"] })
+        end
+        attributes
+      when Capapi::Court
+        { capapi: capapi_obj,
+          name: capapi_obj.name,
+          name_abbreviation: capapi_obj.name_abbreviation }
+      when Capapi::Citation
+        { type: capapi_obj.type,
+          cite: capapi_obj.cite }
+      end
+    end
   end
 end
