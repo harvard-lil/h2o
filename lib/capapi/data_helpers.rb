398b39f360314cc1918ea9eaa630397ef0d1728d
Greg Leppert <greg@leppert.me>
2018-08-10 14:13:57 -0400

    Add tests for date parsing and handle impossible dates

--- lib/capapi/data_helpers.rb
+++ lib/capapi/data_helpers.rb
@@ -1,10 +1,20 @@
 module Capapi
   module DataHelpers
     def self.parse_date date_str
-      fmt = "%Y"
-      fmt += "-%m" if date_str === /\d{4}-\d{2}/
-      fmt += "-%d" if date_str === /\d{4}-\d{2}-\d{2}/
-      Date.strptime(date_str, fmt)
+      fmt = default = "%Y"
+      fmt += "-%m" if /^\d{4}-\d{2}$/ === date_str
+      fmt += "-%d" if /^\d{4}-\d{2}-\d{2}$/ === date_str
+
+      begin
+        Date.strptime(date_str, fmt)
+      rescue ArgumentError => e
+        if fmt == default
+          puts e.class
+          raise e
+        else
+          parse_date(date_str[0..-4])
+        end
+      end
     end
   end
 end
