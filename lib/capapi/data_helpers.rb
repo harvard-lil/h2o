f60a46a3b75ac9ef2e00709f9bd113784836f0c2
Greg Leppert <greg@leppert.me>
2018-08-12 21:06:02 -0400

    Fix capapi date helper

--- lib/capapi/data_helpers.rb
+++ lib/capapi/data_helpers.rb
@@ -2,7 +2,7 @@ module Capapi
   module DataHelpers
     def self.parse_date date_str
       fmt = default = "%Y"
-      fmt += "-%m" if /^\d{4}-\d{2}$/ === date_str
+      fmt += "-%m" if /^\d{4}-\d{2}/ === date_str
       fmt += "-%d" if /^\d{4}-\d{2}-\d{2}$/ === date_str
 
       begin
