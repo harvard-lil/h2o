6504c365c50f4c82f72f8a68ce9743a8a25e9e15
Casey Gruppioni <cgruppioni@gmail.com>
2018-08-28 15:00:29 -0400

    Revising a resource or section has the original casebook owner as the owner of the new draft.

--- app/controllers/content/sections_controller.rb
+++ app/controllers/content/sections_controller.rb
@@ -53,7 +53,7 @@ class Content::SectionsController < Content::NodeController
   def edit
     # editing a section takes you to a cloned casebook and
     # the original casebook stays published
-    @casebook = @casebook.clone(owner: current_user, draft_mode: true)
+    @casebook = @casebook.clone(owner: @casebook.owner, draft_mode: true)
     @section = @casebook.contents.find_by(copy_of_id: @section.id)
     @decorated_content = @content.decorate(context: {action_name: action_name, casebook: @casebook, section: @section, type: 'section'})
     redirect_to layout_section_path(@casebook, @section) 
