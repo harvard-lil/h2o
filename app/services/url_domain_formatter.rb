95d67e4fae71c9e1c1ff6676d694208b86da4804
Casey Gruppioni <cgruppioni@gmail.com>
2018-09-11 15:24:09 -0400

    Don't strip out uri.path with urls

--- app/services/url_domain_formatter.rb
+++ app/services/url_domain_formatter.rb
@@ -1,6 +1,6 @@
 class UrlDomainFormatter
   def self.format(domain)
     uri = URI.parse(domain)
-    "#{uri.scheme || 'http'}://#{uri.host || uri.path}/"
+    "#{uri.scheme || 'http'}://#{uri.host + uri.path}/"
   end
 end
--- test/services/url_domain_formatter_test.rb
+++ test/services/url_domain_formatter_test.rb
@@ -1,12 +1,12 @@
 require "service_test_case"
 
 class UrlDomainFormatterTest < ServiceTestCase
-  scenario "returns only the domain" do
+  scenario "returns domain and it's params" do
   	passed_in_url = "https://cgruppioni.github.io/stuff/otherstuff"
 
   	formatted_domain = UrlDomainFormatter.format(passed_in_url)
 
-  	assert_equal "https://cgruppioni.github.io/", formatted_domain
+  	assert_equal "https://cgruppioni.github.io/stuff/otherstuff", formatted_domain
   end
 
   scenario "prepends a protocol if one is missing" do
