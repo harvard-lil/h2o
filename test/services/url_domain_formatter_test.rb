39ab029bce20438aaf73b0f641d998e33965d72f
Casey Gruppioni <cgruppioni@gmail.com>
2018-09-11 15:57:58 -0400

    Remove trailing and leading whitespace for new links.

--- test/services/url_domain_formatter_test.rb
+++ test/services/url_domain_formatter_test.rb
@@ -1,12 +1,12 @@
 require "service_test_case"
 
 class UrlDomainFormatterTest < ServiceTestCase
-  scenario "returns domain and it's params" do
+  scenario "returns domain and path" do
   	passed_in_url = "https://cgruppioni.github.io/stuff/otherstuff"
 
   	formatted_domain = UrlDomainFormatter.format(passed_in_url)
 
-  	assert_equal "https://cgruppioni.github.io/stuff/otherstuff", formatted_domain
+  	assert_equal "https://cgruppioni.github.io/stuff/otherstuff/", formatted_domain
   end
 
   scenario "prepends a protocol if one is missing" do
@@ -15,5 +15,13 @@ class UrlDomainFormatterTest < ServiceTestCase
   	formatted_domain = UrlDomainFormatter.format(passed_in_url)
 
   	assert_equal "http://cgruppioni.github.io/", formatted_domain
+  end
+
+  scenario "removes leading and trailing whitespace" do
+    passed_in_url = "  http://cgruppioni.github.io "
+
+    formatted_domain = UrlDomainFormatter.format(passed_in_url)
+
+    assert_equal "http://cgruppioni.github.io/", formatted_domain
   end
 end
