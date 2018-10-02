ed4252a5ba89f74219f7f2defa6ef9b0a5e18a2d
Greg Leppert <greg@leppert.me>
2018-10-01 22:02:11 -0400

    Begin Bootstrap 4 migration

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,7 +1,7 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.error_notification_class = 'form-error-message'
-  config.button_class = 'btn btn-default'
+  config.button_class = 'btn btn-secondary'
   config.boolean_label_class = nil
 
   config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
