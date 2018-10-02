6b41d39416a6b77b44a50dfc6102b7e8abdc593d
Greg Leppert <greg@leppert.me>
2018-10-01 22:02:11 -0400

    Begin Bootstrap 4 migration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
+const { resolve } = require("path");
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery'
 }));
+
+environment.loaders
+  .get('sass')
+  .use.find(item => item.loader === 'sass-loader').options.includePaths = [
+    resolve("app", "assets", "stylesheets")
+  ];
 
 module.exports = environment;
