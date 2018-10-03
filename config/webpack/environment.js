f9aacd6671ed0f522a26d34e042de1a6cb70f763
Greg Leppert <greg@leppert.me>
2018-10-03 16:52:40 -0400

    Fix image urls in sass

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,10 +7,16 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   jQuery: 'jquery'
 }));
 
+// add the stylesheets directory to the sass loader so that @import's resolve
 environment.loaders
   .get('sass')
   .use.find(item => item.loader === 'sass-loader').options.includePaths = [
     resolve("app", "assets", "stylesheets")
   ];
+
+// allow img() paths in sass/css to be written relative to the sass file
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader'
+});
 
 module.exports = environment;
