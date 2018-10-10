f736573facf4e9a730783a65c42a1ba4efde45a9
Greg Leppert <greg@leppert.me>
2018-10-10 11:12:01 -0400

    Use tilde syntax for path resolution in SASS files
    This obviates the need for resolve-url-loader and works better with
    Vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,17 +3,15 @@ const webpack = require('webpack');
 const { resolve } = require("path");
 const vue = require('./loaders/vue');
 
+// alias assets dir to ~assets for SASS url() styles
+environment.config.merge({resolve: {alias: {assets: resolve("app", "assets")}}});
+
 // add the stylesheets directory to the sass loader so that @import's resolve
 environment.loaders
   .get('sass')
   .use.find(item => item.loader === 'sass-loader').options.includePaths = [
     resolve("app", "assets", "stylesheets")
   ];
-
-// allow img() paths in sass/css to be written relative to the sass file
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader'
-});
 
 // parse erb files
 environment.loaders.append('erb', {
@@ -27,5 +25,5 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   jQuery: 'jquery'
 }));
 
-environment.loaders.append('vue', vue)
+environment.loaders.append('vue', vue);
 module.exports = environment;
