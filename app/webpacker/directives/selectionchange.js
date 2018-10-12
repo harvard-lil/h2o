4a274eddd2ffd38b6515d36807b431e14ab1fd51
Greg Leppert <greg@leppert.me>
2018-10-12 09:55:34 -0400

    Properly manage state on a per element basis in the directive

--- app/webpacker/directives/selectionchange.js
+++ app/webpacker/directives/selectionchange.js
@@ -1,18 +1,19 @@
 import Vue from 'vue/dist/vue.esm';
 
-var handler = null;
+const state = new WeakMap();
 
 Vue.directive('selectionchange', {
   inserted: function (el, binding) {
-    handler = function (evt) {
+    state.set(el, function (evt) {
       const sel = document.getSelection();
       if (el.contains(sel.anchorNode)) {
         binding.value(evt, sel);
       }
-    };
-    document.addEventListener('selectionchange', handler);
+    });
+    document.addEventListener('selectionchange', state.get(el));
   },
   unbind: function(el, binding) {
-    document.removeEventListener('selectionchange', handler);
+    document.removeEventListener('selectionchange', state.get(el));
+    state.delete(el);
   }
 });
