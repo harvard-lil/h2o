972984670f0b7c40e3a91be7f508d0aa76420542
Greg Leppert <greg@leppert.me>
2018-10-22 14:58:56 -0400

    Add method override to http requests

--- app/webpacker/config/axios.js
+++ app/webpacker/config/axios.js
@@ -6,4 +6,17 @@ const csrf_el = document.querySelector('meta[name=csrf-token]'),
       headers = csrf_el ? {'X-CSRF-Token': csrf_el.getAttribute('content')} : {};
 const Axios = AxiosConfig.create({headers: headers});
 
+// Add method override to request
+Axios.interceptors.request.use(config => {
+  const method = config.method.toUpperCase();
+  if (["PUT", "DELETE", "PATCH"].includes(method)) {
+    config.headers = {
+      ...config.headers,
+      ["X-HTTP-Method-Override"]: method,
+    };
+    config.method = "post";
+  }
+  return config;
+});
+
 Vue.use(VueAxios, Axios);
