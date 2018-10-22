18724bf9081238a5d60ea456394025490ecbdd5c
Greg Leppert <greg@leppert.me>
2018-10-22 17:10:43 -0400

    Move state modification out of component

--- app/webpacker/store/modules/annotations.js
+++ app/webpacker/store/modules/annotations.js
@@ -1,3 +1,11 @@
+import Axios from '../../config/axios';
+
+const helpers = {
+  path(rootState, annotation) {
+    return '/resources/$RESOURCE_ID/annotations/$ANNOTATION_ID'.replace('$RESOURCE_ID', rootState.resource.id).replace('$ANNOTATION_ID', annotation.id);
+  }
+};
+
 const state = {
   all: window.STATE_BOOTSTRAP ? window.STATE_BOOTSTRAP.annotations || [] : []
 };
@@ -8,9 +16,22 @@ const getters = {
   }
 };
 
-const actions = {};
+const actions = {
+  destroy({ commit, rootState }, annotation) {
+    Axios
+      .delete(helpers.path(rootState, annotation))
+      .then(resp => {
+        // commit('destroy', annotation);
+        window.location.reload();
+      });
+  }
+};
 
-const mutations = {};
+const mutations = {
+  destroy(state, annotation) {
+    state.all.splice(state.all.indexOf(annotation), 1);
+  }
+};
 
 export default {
   namespaced: true,
