d5094bd9a3a6fbcc79c08e5ceb74c01a22ca2d6e
Casey <cgruppioni@users.noreply.github.com>
2018-10-26 16:16:27 -0400

    Revert "Update comment."
    This reverts commit d3200776069d8ed5841c6d719188ad036d3f0e65.

--- app/assets/javascripts/lib/ui/content/annotations/note.js
+++ app/assets/javascripts/lib/ui/content/annotations/note.js
@@ -8,7 +8,7 @@ delegate(document, '.annotate.note', 'click', e => {
   .classList.toggle('revealed');
 });
 
-// Stage content for cancel to work and override the default select action which would keep the `create-menu` open
+// override default click action that would open create menu
 delegate(document, '.note-content', 'click', e => {
   handleNoteContentPressed(e)
 });
