b38de8e927e03cc59decf7ce63924fc1f4b2f64b
Casey Gruppioni <cgruppioni@gmail.com>
2018-11-09 16:47:10 -0500

    Authorizable gem doesn't exist in repo.

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,6 +2,4 @@ class Role < ApplicationRecord
   #acts_as_authorization_role
   has_many :roles_users
   has_one :user, :through => :roles_users
-
-  belongs_to :authorizable, :polymorphic => true, optional: true
 end
