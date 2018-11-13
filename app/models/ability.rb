71cdcd69c793928810059bf6d503008e0a8a9578
Casey Gruppioni <cgruppioni@gmail.com>
2018-11-13 12:37:11 -0500

    Transfer casebook ownership tab for casebook member. Empty view for now.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -72,6 +72,8 @@ class Ability
       can [:new, :edit, :update, :show], [Case, CaseCourt, Default, TextBlock, User]
 
       can :show, BulkUpload
+
+      can :transfer_casebook_ownership, [Content::Casebook]
     elsif user.has_role? :case_admin
       can :access, :rails_admin
       can [:index, :show, :export, :export_as, :export_unique, :show_in_app], Case
