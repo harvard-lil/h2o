ad4bdebd80332e6183f4d75373fec96aab25540c
Greg Leppert <greg@leppert.me>
2019-01-01 16:23:27 -0500

    Add initial elisions work

--- app/webpacker/packs/vue_app.js
+++ app/webpacker/packs/vue_app.js
@@ -5,14 +5,20 @@ import store from '../store/index.js.erb';
 import '../config/axios';
 import '../directives/selectionchange';
 
+import TheAnnotator from "../components/TheAnnotator.vue.erb";
 import AnnotationHandle from "../components/AnnotationHandle";
+import Elision from "../components/Elision";
+import TheGlobalElisionExpansionButton from "../components/TheGlobalElisionExpansionButton";
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
     el: '#app',
     store,
     components: {
-      AnnotationHandle
+      TheAnnotator,
+      AnnotationHandle,
+      Elision,
+      TheGlobalElisionExpansionButton
     }
   });
 
