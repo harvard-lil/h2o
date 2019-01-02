0916858487146074710b20dd7b1cebc9e3eeff8a
Greg Leppert <greg@leppert.me>
2019-01-02 16:35:05 -0500

    Componentize more of the annotation elements

---
+++ app/webpacker/components/Annotation.vue
@@ -0,0 +1,30 @@
+<template>
+</template>
+
+<script>
+import AnnotationHandle from './AnnotationHandle';
+import { createNamespacedHelpers } from 'vuex';
+const { mapActions } = createNamespacedHelpers('annotations');
+
+export default {
+  components: {
+    AnnotationHandle
+  },
+  props: ['annotationId',
+          'hasHandle'],
+  computed: {
+    annotation() {
+      return this.$store.getters['annotations/getById'](this.annotationId);
+    },
+    uiState() {
+      return this.$store.getters['annotations_ui/getById'](this.annotationId);
+    }
+  },
+  methods: {
+    ...mapActions(['update', 'destroy'])
+  }
+}
+</script>
+
+<style>
+</style>
--- app/webpacker/components/Replacement.vue
+++ app/webpacker/components/Replacement.vue
@@ -1,36 +1,40 @@
 <template>
 <span class="replacement">
-  <button v-if="hasHandle"
-          @click="toggleExpansion(ui_state)"
-          aria-label="elided text"
-          v-bind:aria-expanded="ui_state.expanded"
-          v-bind:class="{expanded: ui_state.expanded}">
-  </button>
-  <span v-if="ui_state.expanded"
-        class="selected-text">
-    <slot></slot>
-  </span>
-  <span v-else
+  <template v-if="hasHandle">
+    <AnnotationHandle>
+      <li>
+        <a @click="toggleExpansion(uiState)">
+          <template v-if="uiState.expanded">Hide</template>
+          <template v-else>Reveal</template>
+          original text
+        </a>
+      </li>
+      <li>
+        <a @click="destroy(annotation)">Remove elision</a>
+      </li>
+    </AnnotationHandle>
+    <AnnotationExpansionToggle :ui-state="uiState"/>
+  </template><!--
+  whitespace affects offset counts so using this comment for code formatting
+--><span v-show="uiState.expanded" class="selected-text"><slot></slot></span>
+  <span v-if="!uiState.expanded"
         class="text"
+        data-exclude-from-offset-calcs="true"
         contenteditable="true">{{escapedContent}}</span>
 </span>
 </template>
 
 <script>
+import Annotation from './Annotation';
+import AnnotationExpansionToggle from './AnnotationExpansionToggle';
 import { createNamespacedHelpers } from 'vuex';
 const { mapMutations } = createNamespacedHelpers('annotations_ui');
 
 export default {
-  props: ['annotationId',
-          'hasHandle',
-          'escapedContent'],
-  computed: {
-    annotation() {
-      return this.$store.getters['annotations/getById'](this.annotationId);
-    },
-    ui_state() {
-      return this.$store.getters['annotations_ui/getById'](this.annotationId);
-    }
+  extends: Annotation,
+  props: ['escapedContent'],
+  components: {
+    AnnotationExpansionToggle
   },
   methods: {
     ...mapMutations(['toggleExpansion'])
@@ -45,23 +49,9 @@ export default {
   margin: 0 6px;
   padding: 0 6px;
 }
-button {
-  display: inline-block;
-  cursor: zoom-in;
-  border: none;
-  background-color: $light-gray;
-  color: $light-blue;
+.toggle {
   &::before {
-    font-weight: $bold;
-  }
-  &:focus {
-    @include generic-focus-styles;
-  }
-  &.expanded {
-    cursor: zoom-out;
-    &::before {
-      content: 'hide';
-    }
+    content: '';
   }
 }
 .selected-text {
