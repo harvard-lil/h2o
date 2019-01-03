77dfe478187be8d7419ff851fed1a0af7783f2d6
Greg Leppert <greg@leppert.me>
2019-01-02 22:41:59 -0500

    Move note template to vue

--- app/webpacker/components/NoteAnnotation.vue
+++ app/webpacker/components/NoteAnnotation.vue
@@ -1,10 +1,21 @@
 <template>
 <span class="note">
-  <AnnotationHandle v-if="hasHandle">
-    <li>
-      <a @click="destroy(annotation)">Remove note</a>
-    </li>
-  </AnnotationHandle><!--
+  <template v-if="hasHandle">
+    <AnnotationHandle>
+      <li>
+        <a @click="destroy(annotation)">Remove note</a>
+      </li>
+    </AnnotationHandle>
+    <span class="note-content-wrapper"
+          data-exclude-from-offset-calcs="true">
+      <span class="note-icon">
+        <i class='fas fa-paperclip'></i>
+      </span>
+      <span class="note-content">
+        {{annotation.content}}
+      </span>
+    </span>
+  </template><!--
   whitespace affects offset counts; this comment is for code formatting
 --><span tabindex="-1" class="selected-text"><slot></slot></span>
 </span>
@@ -27,5 +38,36 @@ export default {
   &:hover {
     cursor: pointer;
   }
+}
+
+.note-content-wrapper {
+  @include square(0);
+  position: absolute;
+  right: -40px;
+  overflow: visible;
+  display: block;
+  margin: 33px 10px 0 0;
+
+  /* TODO is this needed? */
+  &.revealed {
+    display: none;
+  }
+}
+.note-icon {
+  position: absolute;
+  transform: translate(218%, -116%) rotate(180deg);
+  z-index: 1;
+  font-size: 17px;
+}
+.note-content {
+  @include sans-serif($regular, 14px, 20px);
+  display: block;
+  width: 200px;
+  padding: 10px;
+  position: relative;
+  top: -20px;
+  background-color: white;
+  color: black;
+  margin: 0 20px;
 }
 </style>
