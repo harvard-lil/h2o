f696d4852bba03a4ad55d32af5dbc0733d3c6ed7
Greg Leppert <greg@leppert.me>
2019-01-02 21:59:31 -0500

    Add link and note components

---
+++ app/webpacker/components/Note.vue
@@ -0,0 +1,28 @@
+<template>
+<span class="note">
+    <AnnotationHandle v-if="hasHandle">
+      <li>
+        <a @click="destroy(annotation)">Remove note</a>
+      </li>
+    </AnnotationHandle><!--
+  whitespace affects offset counts; this comment is for code formatting
+--><span tabindex="-1" class="selected-text"><slot></slot></span>
+</span>
+</template>
+
+<script>
+import Annotation from './Annotation';
+
+export default {
+  extends: Annotation
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/vars-and-mixins';
+
+.selected-text {
+  background-color: $highlight;
+  box-shadow: -1px 0 0 $highlight, 1px 0 0 $highlight;
+}
+</style>
