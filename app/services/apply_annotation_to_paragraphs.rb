75617ee3108a1f9434012e3597e4bf63f87e591d
Greg Leppert <greg@leppert.me>
2019-01-12 08:25:14 -0500

    Restore apply_annotation_to_paragraphs.rb to previous state
    We're bypassing this entirely for the Vue rendering, so we don't need
    those modifications anymore, and the previous code is still needed for
    export (docx) rendering.

--- app/services/apply_annotation_to_paragraphs.rb
+++ app/services/apply_annotation_to_paragraphs.rb
@@ -43,6 +43,10 @@ class ApplyAnnotationToParagraphs
     paragraph_node.traverse do |node|
       next unless node.text?
 
+      if node.parent['data-exclude-from-offset-calcs']&.downcase == "true"
+        next
+      end
+
       if noninitial
         if middle_paragraph?(node, paragraph_offset)
           wrap_middle_paragraph(node)
@@ -52,14 +56,16 @@ class ApplyAnnotationToParagraphs
         end
       else
         if paragraph_offset_ready?(node, paragraph_offset)
+          annotation_button = get_annotation_button_and_note_wrapper
+
           if only_paragraph? && partial_paragraph?(node, paragraph_offset)
-            selected_text = annotate_html(node.text[start_offset - paragraph_offset...end_offset - paragraph_offset])
-            node.replace "#{node.text[0...start_offset - paragraph_offset]}#{selected_text}#{node.text[end_offset - paragraph_offset..-1]}"
+            selected_text = annotate_html(node.text[start_offset - paragraph_offset...end_offset - paragraph_offset], final: true)
+            node.replace "#{node.text[0...start_offset - paragraph_offset]}#{annotation_button}#{selected_text}#{node.text[end_offset - paragraph_offset..-1]}"
             break
           else
             noninitial = true
             selected_text = annotate_html(node.text[start_offset - paragraph_offset..-1])
-            node.replace "#{node.text[0...start_offset - paragraph_offset]}#{selected_text}"
+            node.replace "#{node.text[0...start_offset - paragraph_offset]}#{annotation_button}#{selected_text}"
           end
         end
       end
@@ -69,9 +75,74 @@ class ApplyAnnotationToParagraphs
 
   private
 
-  def annotate_html(selected_text, handle: true)
-    component = {'elide' => 'elision', 'replace' => 'replacement'}[kind] || kind
-    "<#{component}-annotation :annotation-id='#{id}' :has-handle='#{handle}'>#{selected_text}</#{component}-annotation>"
+  def annotation_handle
+    "<annotation-handle v-bind:annotation-id='#{id}' data-exclude-from-offset-calcs='true'/>"
+  end
+
+  # The edit icon that shows up next to an annotation when in draft mode.
+  def get_annotation_button_and_note_wrapper
+    if kind == 'note'
+      if editable
+        "#{annotation_handle}<span class='annotate note-content-wrapper' data-annotation-id='#{id}'><span class='note-icon' data-annotation-id='#{id}' data-exclude-from-offset-calcs='true'><i class='fas fa-paperclip'></i></span><span class='note-content' data-exclude-from-offset-calcs='true'>#{escaped_content}</span></span>"
+      # Show notes only when not exporting, or exporting with annotations
+      elsif !exporting || (exporting && include_annotations)
+        "<span class='annotate note-content-wrapper' data-annotation-id='#{id}'><span class='note-icon' data-annotation-id='#{id}' data-exclude-from-offset-calcs='true'><i class='fas fa-paperclip'></i></span><span class='note-content' data-exclude-from-offset-calcs='true'>#{escaped_content}</span></span>"
+      end
+    elsif editable
+      annotation_handle
+    else
+      ""
+    end
+  end
+
+  # NB: the export to docx code is tightly coupled with this markup. Test thoroughly if altering.
+  def annotate_html(selected_text, handle: true, final: false)
+    case kind
+    when 'elide' then
+      "#{handle ? "<span role='button' tabindex='0' class='annotate elide' data-annotation-id='#{id}' aria-label='elided text' aria-expanded='false'></span>" : ''}" +
+      "<span class='annotate elided' data-annotation-id='#{id}'>#{selected_text}</span>"
+    when 'replace' then
+      "#{handle ? "<span role='button' tabindex='0' aria-expanded='false' class='annotate replacement' data-annotation-id='#{id}'><span class='text' data-annotation-id='#{id}' data-exclude-from-offset-calcs='true'>#{escaped_content}</span></span>" : ''}<span class='annotate replaced' data-annotation-id='#{id}'>#{selected_text}</span>"
+    when 'highlight' then
+      "<span tabindex='-1' class='annotate highlighted' data-annotation-id='#{id}'>#{selected_text}</span>"
+    when 'link' then
+      if exporting && paragraph_index == end_paragraph && include_annotations
+        "<a href='#{escaped_content}' target='_blank' class='annotate link' data-annotation-id='#{id}'>#{selected_text}</a>#{'*' * export_footnote_index}"
+      else
+        "<a href='#{escaped_content}' target='_blank' class='annotate link' data-annotation-id='#{id}'>#{selected_text}</a>"
+      end
+    when 'note' then
+      if exporting && paragraph_index == end_paragraph && include_annotations
+        "<span tabindex='-1' class='annotate note' data-annotation-id='#{id}'>#{selected_text}</span>#{'*' * export_footnote_index}"
+      else
+        "<span tabindex='-1' class='annotate note' data-annotation-id='#{id}'>#{selected_text}</span>"
+      end
+    end
+  end
+
+  def escaped_content
+    ApplicationController.helpers.send(:html_escape, content)
+  end
+
+  # If it's a multi-paragraph annotation and this paragraph is in the middle, annotate the entire paragraph
+  def full_paragraph_node_annotated?
+    (!first_paragraph? && !last_paragraph?) || (paragraph_index == end_paragraph && paragraph_index != start_paragraph && end_offset == paragraph_node.text.length)
+  end
+
+  def first_paragraph?
+    paragraph_index == start_paragraph
+  end
+
+  def last_paragraph?
+    paragraph_index == end_paragraph
+  end
+
+  def wrap_paragraph_node
+    paragraph_node.children = annotate_html(paragraph_node.inner_html, handle: false)
+    if kind.in? %w{elide replace}
+      paragraph_node['data-elided-annotation'] = id
+    end
+    return
   end
 
   def middle_paragraph?(node, paragraph_offset)
@@ -84,7 +155,7 @@ class ApplyAnnotationToParagraphs
 
   # This is the last paragraph, apply the annotation to the remaining characters.  
   def wrap_last_paragraph(node, paragraph_offset)
-    selected_text = annotate_html(node.text[0...end_offset - paragraph_offset], handle: false)
+    selected_text = annotate_html(node.text[0...end_offset - paragraph_offset], handle: false, final: true)
     node.replace "#{node.text[0...0]}#{selected_text}#{node.text[end_offset - paragraph_offset..-1]}"
   end
 
