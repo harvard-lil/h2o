33f6de592bd23dbd384bfff56fab031e3d65e0f6
Greg Leppert <greg@leppert.me>
2019-01-13 21:19:44 -0500

    Use short syntax

--- app/webpacker/directives/selectionchange.js
+++ app/webpacker/directives/selectionchange.js
@@ -7,7 +7,7 @@ const prev_in_el = new WeakMap();
 // where selection will be null in the event the user has selected
 // something outside of the element on which this directive has been placed.
 Vue.directive('selectionchange', {
-  inserted: function (el, binding) {
+  inserted(el, binding) {
     handlers.set(el, function (evt) {
       const sel = document.getSelection();
       const in_el = el.contains(sel.anchorNode);
@@ -19,7 +19,7 @@ Vue.directive('selectionchange', {
     });
     document.addEventListener('selectionchange', handlers.get(el));
   },
-  unbind: function(el, binding) {
+  unbind(el, binding) {
     document.removeEventListener('selectionchange', handlers.get(el));
     handlers.delete(el);
     prev_in_el.delete(el);
