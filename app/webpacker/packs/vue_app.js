1ef8ce21301c909a73770d63ae741b05528acedb
Greg Leppert <greg@leppert.me>
2019-01-18 13:53:30 -0500

    Style

--- app/webpacker/packs/vue_app.js
+++ app/webpacker/packs/vue_app.js
@@ -1,18 +1,18 @@
-import 'polyfills';
-import Vue from 'vue/dist/vue.esm';
+import "polyfills";
+import Vue from "vue/dist/vue.esm";
 
-import store from '../store/index.js.erb';
-import '../config/axios';
-import '../directives/selectionchange';
+import store from "../store/index.js.erb";
+import "../config/axios";
+import "../directives/selectionchange";
 
-import contenteditableDirective from 'vue-contenteditable-directive';
+import contenteditableDirective from "vue-contenteditable-directive";
 Vue.use(contenteditableDirective);
 
 import TheResourceBody from "../components/TheResourceBody";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const app = new Vue({
-    el: '#app',
+    el: "#app",
     store,
     components: {
       TheResourceBody
