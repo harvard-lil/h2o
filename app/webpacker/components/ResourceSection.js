b4d80e8d7e9ec16a5746a1143a4623ad0255717c
Greg Leppert <greg@leppert.me>
2019-01-18 21:20:28 -0500

    Helper function to improve readability

--- app/webpacker/components/ResourceSection.js
+++ app/webpacker/components/ResourceSection.js
@@ -43,6 +43,10 @@ const getAttrsMap = (el) => {
   }
   return attrmap;
 };
+
+const last = (array) => {
+  return array[array.length - 1];
+};
     
 ///////////////////////////
 // Munging and filtering //
@@ -53,7 +57,7 @@ const isValidNodeType = (node) =>
     
 const transformToTuplesWithOffsets = (parentStart) =>
   (tuples, node) => {
-    let [prevNode, prevStart, prevEnd] = tuples[tuples.length - 1] ||
+    let [prevNode, prevStart, prevEnd] = last(tuples) ||
         [null, null, parentStart];
     return tuples.concat([[node, prevEnd, prevEnd + getLength(node)]]);
   };
@@ -68,9 +72,9 @@ const splitTextAt = (breakpoints, [node, start, end]) =>
   // in our array, truncating it, and returns a new node with
   // the remaining text
     .reduce((tuples, breakpoint) => {
-      let [prevNode, prevStart, prevEnd] = tuples[tuples.length - 1];
+      let [prevNode, prevStart, prevEnd] = last(tuples);
       let node = prevNode.splitText(breakpoint - prevStart);
-      tuples[tuples.length - 1][2] = breakpoint;
+      last(tuples)[2] = breakpoint;
       return tuples.concat([[node, breakpoint, prevEnd]]);
     }, [[node, start, end]]);
 
@@ -89,11 +93,11 @@ const groupIntoAnnotation = (h, index, tuples, enclosingAnnotationIds) =>
     // Figure out how far to reach forward for elements to group into this annotation.
     // If the annotation extends beyond this section / pararaph,
     // use the end offset of this parent element
-    let annotationEndInSection = annotation.end_paragraph == index ? annotation.end_offset : tuples[tuples.length - 1][2];
+    let annotationEndInSection = annotation.end_paragraph == index ? annotation.end_offset : last(tuples)[2];
     // get the forward elements that fall within our range
     let childTuples = [prevTuple, ...tuples.filter(t => t[1] >= prevTuple[2] && t[2] <= annotationEndInSection)];
     let props = {startOffset: prevTuple[1],
-                 endOffset: childTuples[childTuples.length - 1][2]};
+                 endOffset: last(childTuples)[2]};
     
     return [h(kindToComponent(annotation.kind),
               {props: {...props,
@@ -107,7 +111,7 @@ const groupIntoAnnotation = (h, index, tuples, enclosingAnnotationIds) =>
 const insertAnnotations = (h, index, enclosingAnnotationIds) =>
   (modifiedTuples, tuple, idx, orgTuples) => {
     let [node, start, end] = tuple;
-    let [prevNode, prevStart, prevEnd] = modifiedTuples[modifiedTuples.length - 1] ||
+    let [prevNode, prevStart, prevEnd] = last(modifiedTuples) ||
         [null, null, orgTuples[0][1]];
 
     // if the previous tuple's end offset is greater than the current tuple's
@@ -168,7 +172,7 @@ const annotationBreakpoints = (index, start, end) =>
 const filterAndSplitNodeList = (nodeList, index, start, end) => {
   const breakpoints = annotationBreakpoints(index, start, end);
   return Array.from(nodeList)
-  // remove anything that isn't Element or an Text
+  // remove anything that isn't an Element or Text node
   // i.e. no script or comment tags etc
     .filter(isValidNodeType)
   // transform our Node array to an array of [Node, start, end] tuples
