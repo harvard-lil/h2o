74a3574d6953665b740e036c785987a4a39f9b36
Greg Leppert <greg@leppert.me>
2019-01-31 16:58:47 -0500

    Wrap bare inline tags rather than replace them
    Replacing them was causing us to lose footer links since P tags can't
    have href.

--- app/webpacker/libs/html_helpers.js
+++ app/webpacker/libs/html_helpers.js
@@ -12,6 +12,12 @@ const replaceTag = (el, newTag) => {
   el.parentNode.replaceChild(newEl, el);
 };
 
+const wrap = (el, wrapperTag) => {
+  let wrapper = document.createElement(wrapperTag);
+  el.parentNode.insertBefore(wrapper, el);
+  wrapper.appendChild(el);
+};
+
 const unwrap = (el) => {
   let parent = el.parentNode;
   while (el.firstChild) parent.insertBefore(el.firstChild, el);
@@ -32,7 +38,7 @@ export const emptyULToP = (doc) => {
 
 export const wrapBareInlineTags = (doc) => {
   doc.querySelectorAll("body > :not(p):not(center):not(blockquote):not(article)")
-      .forEach(el => replaceTag(el, "p"));
+      .forEach(el => wrap(el, "p"));
   return doc;
 };
 
