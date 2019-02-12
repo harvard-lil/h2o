3076a0e48d609f4300b0925c7f6d508a34f54115
Casey Gruppioni <cgruppioni@gmail.com>
2019-02-12 16:51:57 -0500

    WIP refactor node_decorator.

--- app/helpers/action_button_helper.rb
+++ app/helpers/action_button_helper.rb
@@ -1,206 +1,11 @@
-class Content::NodeDecorator < Draper::Decorator
-  include Draper::LazyHelpers
+module ActionButtonsHelper
 
-  def action_buttons
-    if draft_mode?
-      draft_action_buttons
-    elsif published_mode?
-      published_action_buttons
-    else
-      preview_action_buttons
-    end
+  def clone_and_export
+    clone_casebook + export_casebook
   end
 
-  private
-
-  def draft_action_buttons
-    if self.is_a?(Content::Casebook) && draft_mode_of_published_casebook
-      casebook_draft_of_published_casebook
-    elsif self.is_a? Content::Casebook
-      casebook_draft
-    elsif self.is_a? Content::Section
-      section_draft
-    else
-      resource_draft
-    end
-  end
-
-  def published_action_buttons
-    if self.is_a?(Content::Casebook) && has_live_draft?
-      casebook_published_with_draft
-    elsif self.is_a? Content::Casebook
-      casebook_published
-    elsif self.is_a?(Content::Section) && has_live_draft?
-      section_published_with_draft
-    elsif self.is_a? Content::Section
-      section_published
-    elsif has_live_draft?
-      resource_published_with_draft
-    else
-      resource_published
-    end
-  end
-
-  def preview_action_buttons
-    if self.is_a?(Content::Casebook) && draft_mode_of_published_casebook
-      casebook_preview_of_published_casebook
-    elsif self.is_a? Content::Casebook
-      casebook_preview
-    elsif self.is_a?(Content::Section) && draft_mode_of_published_casebook
-      section_preview_of_published_casebook
-    elsif self.is_a? Content::Section
-      section_preview
-    else
-      resource_preview
-    end
-  end
-
-  #####################
-  # Draft buttons
-
-  def casebook_draft_of_published_casebook
-    publish_changes_to_casebook +
-    preview_casebook +
-    add_resource +
-    add_section +
-    export_casebook +
-    save_casebook +
-    cancel_casebook
-  end
-
-  def casebook_draft
-    publish_casebook +
-    preview_casebook +
-    add_resource +
-    add_section +
-    export_casebook +
-    save_casebook +
-    cancel_casebook +
-    clone_casebook
-  end
-
-  def section_draft
-    preview_section +
-    add_resource +
-    add_section +
-    save_section +
-    cancel_section +
-    export_section
-  end
-
-  def resource_draft
-    if action_name == 'edit'
-      preview_resource +
-      save_resource +
-      cancel_resource +
-      export_resource
-    else
-      preview_resource +
-      export_resource
-    end
-  end
-
-  ###########
-  # Published buttons
-
-  def casebook_published_with_draft
-    if authorized?
-      edit_draft +
-      clone_casebook +
-      export_casebook
-    else
-      clone_casebook +
-      export_casebook
-    end
-  end
-
-  def casebook_published
-    if authorized?
-      create_draft +
-      clone_casebook +
-      export_casebook
-    else
-      clone_casebook +
-      export_casebook
-    end
-  end
-
-  def section_published_with_draft
-    if authorized?
-      revise_draft_section +
-      clone_casebook +
-      export_section
-    else
-      clone_casebook +
-      export_section
-    end
-  end
-
-  def section_published
-    if authorized?
-      create_section_draft +
-      clone_section +
-      export_section
-    else
-      clone_casebook +
-      export_section
-    end
-  end
-
-  def resource_published_with_draft
-    if authorized?
-      annotate_resource_draft +
-      clone_casebook +
-      export_resource
-    else
-      clone_casebook +
-      export_resource
-    end
-  end
-
-  def resource_published
-    if authorized?
-      create_draft +
-      clone_resource +
-      export_resource
-    else
-      clone_casebook +
-      export_resource
-    end
-  end
-
-  ##########
-  #Preview buttons
-
-  def casebook_preview_of_published_casebook
-    publish_changes_to_casebook +
-    edit_draft +
-    export_casebook
-  end
-
-  def casebook_preview
-    publish_casebook +
-    edit_casebook +
-    clone_casebook +
-    export_casebook
-  end
-
-  def section_preview_of_published_casebook
-    revise_draft_section +
-    clone_casebook +
-    export_section
-  end
-
-  def section_preview
-    revise_section +
-    clone_casebook +
-    export_section
-  end
-
-  def resource_preview
-    annotate_resource +
-    clone_casebook +
-    export_resource
+  def draft_buttons
+    add_resource + add_section + export_casebook + save_casebook + cancel_casebook
   end
 
   ####
@@ -356,7 +161,7 @@ class Content::NodeDecorator < Draper::Decorator
     casebook.draft_mode_of_published_casebook
   end
 
-  def has_live_draft?
+  def has_draft
     draft.present?
   end
 
@@ -387,15 +192,19 @@ class Content::NodeDecorator < Draper::Decorator
     context[:action_name]
   end
 
-  def draft_mode?
+  def draft_mode
     action_name.in? %w{edit layout annotate}
   end
 
-  def published_mode?
+  def published_mode
     casebook.public
   end
 
-  def preview_mode?
+  def draft_of_published_casebook
+    casebook.draft_mode_of_published_casebook
+  end
+
+  def preview_mode
     authorized? && action_name == 'show'
   end
 
