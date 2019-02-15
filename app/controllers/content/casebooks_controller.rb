b1d62b8be1c79f1571175588d434eb0ed5a2d404
Casey Gruppioni <cgruppioni@gmail.com>
2019-02-15 16:24:51 -0500

    Add question about decorator in export.
    Add node as to what node decorator does.

--- app/controllers/content/casebooks_controller.rb
+++ app/controllers/content/casebooks_controller.rb
@@ -59,6 +59,7 @@ class Content::CasebooksController < Content::NodeController
   end
 
   def export
+    # does this need to be decorated?
     @decorated_content = @casebook.decorate(context: {action_name: action_name, casebook: @casebook, type: 'casebook'})
     @include_annotations = (params["annotations"] == "true")
 
