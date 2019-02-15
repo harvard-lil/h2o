3a95721786567703321a08218de9267570604654
Casey Gruppioni <cgruppioni@gmail.com>
2019-02-15 17:08:59 -0500

    seperating logic between decorator and service class.
    get links from action button builder. Need to get over protect from forgery for buttons.

--- app/decorators/content/node_decorator.rb
+++ app/decorators/content/node_decorator.rb
@@ -1,16 +1,28 @@
 # decorates casebooks, sections and resources with the appropriate actions buttons
 class Content::NodeDecorator < Draper::Decorator
   include Draper::LazyHelpers
-  helper :action_buttons
+  attr_accessor :builder, :casebook, :section, :resource, :action
+
+  def build_params
+    @casebook = context[:casebook]
+    @section = context[:section]
+    @resource = context[:context_resource]
+    @action = context[:action_name]
+    @builder = ActionButtonBuilder.new(casebook, section, resource, action)
+  end
 
   def action_buttons
+    build_params
+
     if self.is_a?(Content::Casebook)
-      casebook_actions
+      buttons = casebook_actions
     elsif self.is_a? Content::Section
-      section_actions
+      buttons = section_actions
     else
-      resource_actions
+      buttons = resource_actions
     end
+
+    builder.perform(buttons)
   end
 
   private
@@ -19,7 +31,7 @@ class Content::NodeDecorator < Draper::Decorator
     if published_mode
       if authorized
         if has_draft
-          edit_draft + clone_and_export
+          [:edit_draft] << clone_and_export
         else
           create_draft + clone_and_export
         end
@@ -33,7 +45,7 @@ class Content::NodeDecorator < Draper::Decorator
         publish_casebook + edit_casebook + clone_and_export
       end
     else draft_mode
-      if draft_of_published_casebook
+      if published_casebook_draft
         publish_changes_to_casebook + preview_casebook + draft_buttons
       else
         publish_casebook + preview_casebook + draft_buttons
@@ -88,7 +100,44 @@ class Content::NodeDecorator < Draper::Decorator
         publish_casebook + edit_casebook + clone_and_export
       end
     elsif draft_mode
-      preview_resource + save_resource + cancel_resource + export_resource
+      [:preview_resource, :save_resource, :cancel_resource, :export_resource]
     end
   end
+
+  #condensed button lists
+
+  def clone_and_export
+    [:clone_casebook, :export_casebook]
+  end
+
+  def draft_buttons
+    add_resource + add_section + export_casebook + save_casebook + cancel_casebook
+  end
+
+  #variables
+
+  def draft_mode
+    action.in? %w{edit layout annotate}
+  end
+
+  def published_mode
+    casebook.public
+  end
+
+  def preview_mode
+    authorized? && action == 'show'
+  end
+
+  def has_draft
+    casebook.draft.present?
+  end
+
+  def authorized
+    if current_user.present?
+      casebook.has_collaborator?(current_user.id) || current_user.superadmin?
+    else 
+      false
+    end
+  end
+
 end
