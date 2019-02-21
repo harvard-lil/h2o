c5e64892a987afc5d3a0d923ac50e9cb065ccc7e
Greg Leppert <greg@leppert.me>
2019-02-21 13:17:55 -0500

    Factor out need for ERB in JS files
    The Babel transformation was booting up Rails just to get the env
    which is slow and makes using Jest needlessly complicated.
    
    These process references are dynamically replaced. See: https://cli.vuejs.org/guide/mode-and-env.html#using-env-variables-in-client-side-code

--- app/webpacker/components/ResourceSection.js
+++ app/webpacker/components/ResourceSection.js
@@ -1,4 +1,4 @@
-import store from "../store/index.js.erb";
+import store from "../store/index";
 
 import { isBlockLevel,
          isElement,
--- app/webpacker/store/index.js
+++ app/webpacker/store/index.js
@@ -8,7 +8,7 @@ import createLogger from "vuex/dist/logger";
 
 Vue.use(Vuex);
 
-const debug = <%= Rails.env.development? %>;
+const debug = process.env.NODE_ENV == "development";
 
 export default new Vuex.Store({
   modules: {annotations,
