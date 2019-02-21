c6c0736e20f4d88ff928d035115131d54576260d
Greg Leppert <greg@leppert.me>
2019-02-21 16:52:26 -0500

    Add first Vue component test

--- test/javascript/components/ResourceSection.test.js
+++ test/javascript/components/ResourceSection.test.js
@@ -1,5 +1,51 @@
+import { parseNode } from '../test_helpers';
+import { mount,
+         createLocalVue } from '@vue/test-utils';
+
+import Vuex from 'vuex';
+import annotations from "store/modules/annotations";
+import annotations_ui from "store/modules/annotations_ui";
+import footnotes_ui from "store/modules/footnotes_ui";
+import resources_ui from "store/modules/resources_ui";
+
 import ResourceSection from 'components/ResourceSection';
 
-test('hello world', () => {
-  expect('hello world').toMatch(/hello world/);
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('ResourceSection', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {annotations,
+                annotations_ui,
+                footnotes_ui,
+                resources_ui}
+    });
+  });
+
+  test('correctly places an annotation', () => {
+    const annotation = {
+      "id": 352630,
+      "resource_id": 63432,
+      "start_paragraph": 0,
+      "end_paragraph": 0,
+      "start_offset": 1,
+      "end_offset": 5,
+      "kind": "link",
+      "content": "http://google.com/",
+      "created_at": "2019-02-11T19:16:03.604Z",
+      "updated_at": "2019-02-11T19:16:03.604Z"
+    };
+
+    store.commit('annotations/append', [annotation]);
+
+    const wrapper = mount(ResourceSection, {store, localVue, propsData: {
+      index: 0,
+      el: parseNode('<div>Hello world</div>')
+    }});
+
+    expect(wrapper.find(`a[href="${annotation.content}"]`).text()).toEqual('ello');
+  });
 });
