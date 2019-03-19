fc627782879e2fd6afefc76fec11d2ad9ffc38ba
Rebecca Cremona <rcremona@law.harvard.edu>
2019-03-19 18:39:27 -0400

    Echo some messages before slow commands.

--- docker/test.sh
+++ docker/test.sh
@@ -2,11 +2,11 @@
 set -e
 
 function cleanup {
-  docker-compose exec web rails sunspot:solr:stop
+  echo "Stopping solr ..."  && docker-compose exec web rails sunspot:solr:stop
 }
 trap cleanup EXIT
 
-docker-compose exec web rails sunspot:solr:start
+echo "Starting solr ..."  && docker-compose exec web rails sunspot:solr:start
 docker-compose exec web yarn test
 docker-compose exec web rails test
 docker-compose exec web rails test:system
