2108f1e828417b7fbe0b05cac73072956ad79bc3
Casey Gruppioni <cgruppioni@gmail.com>
2019-03-29 17:15:27 -0400

    Finish note_decorator test for casebook + user scenarios.

--- test/decorators/content/node_decorator_test.rb
+++ test/decorators/content/node_decorator_test.rb
@@ -2,16 +2,16 @@ require "application_system_test_case"
 
 class Content::NodeDecoratorTest < ApplicationSystemTestCase
   describe 'draft of casebook without a published parent' do
-    describe 'in draft mode of casebook that does not have a published parent' do
-      before do
-        sign_in @user = users(:verified_professor)
+    before do
+      sign_in @user = users(:verified_professor)
 
-        @casebook = content_nodes(:draft_casebook)
-        @section = content_nodes(:draft_casebook_section_1)
-        @resource = content_nodes(:draft_resource_1)
-      end
+      @casebook = content_nodes(:draft_casebook)
+      @section = content_nodes(:draft_casebook_section_1)
+      @resource = content_nodes(:draft_resource_1)
+    end
 
-      it 'renders casebook buttons' do
+    describe 'draft mode' do
+      it 'casebook actions' do
         visit layout_casebook_path @casebook
 
         assert_content "Publish"
@@ -23,7 +23,7 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
         assert_content "Cancel"
       end
 
-      it 'renders section buttons' do
+      it 'section actions' do
         visit layout_section_path @casebook, @section
 
         assert_content "Preview"
@@ -34,7 +34,7 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
         assert_content "Cancel"
       end
 
-      it 'renders resource buttons in edit' do
+      it 'resource actions in edit' do
         visit resource_path @casebook, @resource
 
         assert_content "Preview"
@@ -44,16 +44,8 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
       end
     end
 
-    describe 'in preview mode ' do
-      before do
-        sign_in @user = users(:verified_professor)
-
-        @casebook = content_nodes(:draft_casebook)
-        @section = content_nodes(:draft_casebook_section_1)
-        @resource = content_nodes(:draft_resource_1)
-      end
-
-      it 'renders casebook buttons' do
+    describe 'preview mode ' do
+      it 'casebook actions' do
         visit casebook_path @casebook 
 
         assert_content "Publish"
@@ -62,14 +54,14 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
         assert_content "Clone"
       end
 
-      it 'renders section buttons' do
+      it 'section actions' do
         visit section_path @casebook, @section
 
         assert_content "Revise"
         assert_content "Clone"
       end
 
-      it 'renders resource buttons' do
+      it 'resource actions' do
         visit resource_path @casebook, @resource
 
         assert_content "Revise"
@@ -77,16 +69,8 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
       end
     end
 
-    describe 'in published mode as casebook creator' do
-      before do
-        sign_in @user = users(:verified_professor)
-
-        @casebook = content_nodes(:public_casebook)
-        @section = content_nodes(:public_casebook_section_1)
-        @resource = content_nodes(:public_casebook_section_1_1)
-      end
-
-      it 'renders casebook buttons' do
+    describe 'published mode' do
+      it 'casebook actions' do
         visit casebook_path @casebook
 
         assert_content "Revise"
@@ -94,7 +78,7 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
         assert_content "Export"
       end
 
-      it 'renders section buttons' do
+      it 'section actions' do
         visit section_path @casebook, @section
 
         assert_content "Revise"
@@ -102,7 +86,7 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
         assert_content "Export"
       end
 
-      it 'renders resource buttons' do
+      it 'resource actions' do
         visit resource_path @casebook, @resource
         dom = Nokogiri::HTML(decorated_content.action_buttons)
 
@@ -113,7 +97,107 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
     end
   end
 
-  describe 'published casebook with a logged in user' do
+  describe 'draft of casebook with a published parent' do
+    before do
+      sign_in @user = users(:verified_professor)
+
+      @casebook = content_nodes(:draft_merge_casebook)
+      @section = content_nodes(:draft_merge_section)
+      @resource = content_nodes(:draft_merge_section_1)
+    end
+
+    describe 'draft mode' do
+      it 'casebook actions' do
+        visit layout_casebook_path @casebook
+
+        assert_content "Publish Changes"
+        assert_content "Preview"
+        assert_content "Add Resource"
+        assert_content "Add Section"
+        assert_content "Export"
+        assert_content "Save"
+        assert_content "Cancel"
+        refute_content "Clone"
+      end
+
+      it 'section actions' do
+        visit layout_section_path @casebook, @section
+
+        assert_content "Preview"
+        assert_content "Add Resource"
+        assert_content "Add Section"
+        assert_content "Export"
+        assert_content "Save"
+        assert_content "Cancel"
+        refute_content "Clone"
+      end
+
+      it 'resource actions in edit' do
+        visit resource_path @casebook, @resource
+
+        assert_content "Preview"
+        assert_content "Export"
+        assert_content "Save"
+        assert_content "Cancel"
+        refute_content "Clone"
+      end
+    end
+
+    describe 'preview mode ' do
+      it 'casebook actions' do
+        visit casebook_path @casebook 
+
+        assert_content "Publish Changes"
+        assert_content "Return to Draft"
+        assert_content "Export"
+        refute_content "Clone"
+      end
+
+      it 'section actions' do
+        visit section_path @casebook, @section
+
+        assert_content "Return to Draft"
+        assert_content "Export"
+        refute_content "Clone"
+      end
+
+      it 'resource actions' do
+        visit resource_path @casebook, @resource
+
+        assert_content "Revise"
+        refute_content "Clone"
+      end
+    end
+
+    describe 'published mode' do
+      it 'casebook actions' do
+        visit casebook_path @casebook
+
+        assert_content "Return to Draft"
+        assert_content "Clone"
+        assert_content "Export"
+      end
+
+      it 'section actions' do
+        visit section_path @casebook, @section
+
+        assert_content "Return to Draft"
+        assert_content "Clone"
+        assert_content "Export"
+      end
+
+      it 'resource actions' do
+        visit resource_path @casebook, @resource
+        dom = Nokogiri::HTML(decorated_content.action_buttons)
+
+        assert_content "Return to Draft"
+        assert_content "Clone"
+        assert_content "Export"
+      end
+    end
+  end
+
+  describe 'published casebook with a logged in user that is not a collaborator' do
     before do
       sign_in @user = users(:verified_student)
 
@@ -122,23 +206,46 @@ class Content::NodeDecoratorTest < ApplicationSystemTestCase
       @resource = content_nodes(:public_casebook_section_1_1)
     end
 
-    it 'renders casebook buttons' do
+    it 'casebook actions' do
       visit casebook_path @casebook
 
       assert_content "Clone"
       assert_content "Export"
     end
 
-    it 'renders section buttons' do
+    it 'section actions' do
       visit section_path @casebook, @section
 
       assert_content "Clone"
       assert_content "Export"
     end
 
-    it 'renders resource buttons' do
+    it 'resource actions' do
       visit resource_path @casebook, @resource
 
+      assert_content "Clone"
+      assert_content "Export"
+    end
+  end
+
+  describe 'published casebook with an anonymous user' do
+    before do
+      @casebook = content_nodes(:public_casebook)
+      @section = content_nodes(:public_casebook_section_1)
+      @resource = content_nodes(:public_casebook_section_1_1)
+    end
+
+    describe 'casebook actions' do
+      assert_content "Clone"
+      assert_content "Export"
+    end
+
+    describe 'section actions' do
+      assert_content "Clone"
+      assert_content "Export"
+    end
+
+    describe 'resource actions' do
       assert_content "Clone"
       assert_content "Export"
     end
