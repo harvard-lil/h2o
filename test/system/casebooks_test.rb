08331fb9bd5a3c4a282fee1f88824d9335a02e3b
Casey Gruppioni <cgruppioni@gmail.com>
2019-04-05 11:07:30 -0400

    Add action button tests to casebooks_test.

--- test/system/casebooks_test.rb
+++ test/system/casebooks_test.rb
@@ -146,78 +146,213 @@ class CasebookSystemTest < ApplicationSystemTestCase
 
   describe "action buttons" do
     describe 'draft of casebook without a published parent' do
-    before do
-      sign_in @user = users(:verified_professor)
+      before do
+        sign_in @user = users(:verified_professor)
 
-      @casebook = content_nodes(:draft_casebook)
-      @section = content_nodes(:draft_casebook_section_1)
-      @resource = content_nodes(:draft_resource_1)
+        @casebook = content_nodes(:draft_casebook)
+        @section = content_nodes(:draft_casebook_section_1)
+        @resource = content_nodes(:draft_resource_1)
+      end
+
+      describe 'draft mode' do
+        it 'casebook actions' do
+          visit layout_casebook_path @casebook
+
+          assert_button "Publish"
+          assert_button "Preview"
+          assert_button "Add Resource"
+          assert_button "Add Section"
+          assert_button "Export"
+          assert_button "Save"
+          assert_button "Cancel"
+        end
+
+        it 'section actions' do
+          visit layout_section_path @casebook, @section
+
+          assert_button "Preview"
+          assert_button "Add Resource"
+          assert_button "Add Section"
+          assert_button "Export"
+          assert_button "Save"
+          assert_button "Cancel"
+        end
+
+        it 'resource actions in edit' do
+          visit resource_path @casebook, @resource
+
+          assert_button "Preview"
+          assert_button "Export"
+          assert_button "Save"
+          assert_button "Cancel"
+        end
+      end
+
+      describe 'preview mode ' do
+        it 'casebook actions' do
+          visit casebook_path @casebook 
+
+          assert_button "Publish"
+          assert_button "Revise"
+          assert_button "Export"
+          assert_button "Clone"
+        end
+
+        it 'section actions' do
+          visit section_path @casebook, @section
+
+          assert_button "Revise"
+          assert_button "Clone"
+        end
+
+        it 'resource actions' do
+          visit resource_path @casebook, @resource
+
+          assert_button "Revise"
+          assert_button "Clone"
+        end
+      end
+
+      describe 'published mode' do
+        it 'casebook actions' do
+          visit casebook_path @casebook
+
+          assert_button "Revise"
+          assert_button "Clone"
+          assert_button "Export"
+        end
+
+        it 'section actions' do
+          visit section_path @casebook, @section
+
+          assert_button "Revise"
+          assert_button "Clone"
+          assert_button "Export"
+        end
+
+        it 'resource actions' do
+          visit resource_path @casebook, @resource
+          dom = Nokogiri::HTML(decorated_content.action_buttons)
+
+          assert_button "Revise"
+          assert_button "Clone"
+          assert_button "Export"
+        end
+      end
     end
 
-    describe 'draft mode' do
-      it 'casebook actions' do
-        visit layout_casebook_path @casebook
+    describe 'draft of casebook with a published parent' do
+      before do
+        sign_in @user = users(:verified_professor)
 
-        assert_button "Publish"
-        assert_button "Preview"
-        assert_button "Add Resource"
-        assert_button "Add Section"
-        assert_button "Export"
-        assert_button "Save"
-        assert_button "Cancel"
+        @casebook = content_nodes(:draft_merge_casebook)
+        @section = content_nodes(:draft_merge_section)
+        @resource = content_nodes(:draft_merge_section_1)
       end
 
-      it 'section actions' do
-        visit layout_section_path @casebook, @section
+      describe 'draft mode' do
+        it 'casebook actions' do
+          visit layout_casebook_path @casebook
 
-        assert_button "Preview"
-        assert_button "Add Resource"
-        assert_button "Add Section"
-        assert_button "Export"
-        assert_button "Save"
-        assert_button "Cancel"
+          assert_button "Publish Changes"
+          assert_button "Preview"
+          assert_button "Add Resource"
+          assert_button "Add Section"
+          assert_button "Export"
+          assert_button "Save"
+          assert_button "Cancel"
+          refute_button "Clone"
+        end
+
+        it 'section actions' do
+          visit layout_section_path @casebook, @section
+
+          assert_button "Preview"
+          assert_button "Add Resource"
+          assert_button "Add Section"
+          assert_button "Export"
+          assert_button "Save"
+          assert_button "Cancel"
+          refute_button "Clone"
+        end
+
+        it 'resource actions in edit' do
+          visit resource_path @casebook, @resource
+
+          binding.pry
+          assert_button "Preview"
+          assert_button "Export"
+          assert_button "Save"
+          assert_button "Cancel"
+        end
       end
 
-      it 'resource actions in edit' do
-        visit resource_path @casebook, @resource
+      describe 'preview mode ' do
+        it 'casebook actions' do
+          visit casebook_path @casebook 
 
-        assert_button "Preview"
-        assert_button "Export"
-        assert_button "Save"
-        assert_button "Cancel"
+          assert_button "Publish Changes"
+          assert_button "Return to Draft"
+          assert_button "Export"
+          refute_button "Clone"
+        end
+
+        it 'section actions' do
+          visit section_path @casebook, @section
+
+          assert_button "Return to Draft"
+          assert_button "Export"
+          refute_button "Clone"
+        end
+
+        it 'resource actions' do
+          visit resource_path @casebook, @resource
+
+          assert_button "Revise"
+          refute_button "Clone"
+        end
+      end
+
+      describe 'published mode' do
+        it 'casebook actions' do
+          visit casebook_path @casebook
+
+          assert_button "Return to Draft"
+          assert_button "Clone"
+          assert_button "Export"
+        end
+
+        it 'section actions' do
+          visit section_path @casebook, @section
+
+          assert_button "Return to Draft"
+          assert_button "Clone"
+          assert_button "Export"
+        end
+
+        it 'resource actions' do
+          visit resource_path @casebook, @resource
+          dom = Nokogiri::HTML(decorated_content.action_buttons)
+
+          assert_button "Return to Draft"
+          assert_button "Clone"
+          assert_button "Export"
+        end
       end
     end
 
-    describe 'preview mode ' do
-      it 'casebook actions' do
-        visit casebook_path @casebook 
+    describe 'published casebook with a logged in user that is not a collaborator' do
+      before do
+        sign_in @user = users(:verified_student)
 
-        assert_button "Publish"
-        assert_button "Revise"
-        assert_button "Export"
-        assert_button "Clone"
+        @casebook = content_nodes(:public_casebook)
+        @section = content_nodes(:public_casebook_section_1)
+        @resource = content_nodes(:public_casebook_section_1_1)
       end
 
-      it 'section actions' do
-        visit section_path @casebook, @section
-
-        assert_button "Revise"
-        assert_button "Clone"
-      end
-
-      it 'resource actions' do
-        visit resource_path @casebook, @resource
-
-        assert_button "Revise"
-        assert_button "Clone"
-      end
-    end
-
-    describe 'published mode' do
       it 'casebook actions' do
         visit casebook_path @casebook
 
-        assert_button "Revise"
         assert_button "Clone"
         assert_button "Export"
       end
@@ -225,175 +360,39 @@ class CasebookSystemTest < ApplicationSystemTestCase
       it 'section actions' do
         visit section_path @casebook, @section
 
-        assert_button "Revise"
         assert_button "Clone"
         assert_button "Export"
       end
 
       it 'resource actions' do
         visit resource_path @casebook, @resource
-        dom = Nokogiri::HTML(decorated_content.action_buttons)
 
-        assert_button "Revise"
         assert_button "Clone"
         assert_button "Export"
       end
     end
-  end
 
-  describe 'draft of casebook with a published parent' do
-    before do
-      sign_in @user = users(:verified_professor)
+    # describe 'published casebook with an anonymous user' do
+    #   before do
+    #     @casebook = content_nodes(:public_casebook)
+    #     @section = content_nodes(:public_casebook_section_1)
+    #     @resource = content_nodes(:public_casebook_section_1_1)
+    #   end
 
-      @casebook = content_nodes(:draft_merge_casebook)
-      @section = content_nodes(:draft_merge_section)
-      @resource = content_nodes(:draft_merge_section_1)
-    end
+    #   describe 'casebook actions' do
+    #     assert_button "Clone"
+    #     assert_button "Export"
+    #   end
 
-    describe 'draft mode' do
-      it 'casebook actions' do
-        visit layout_casebook_path @casebook
+    #   describe 'section actions' do
+    #     assert_button "Clone"
+    #     assert_button "Export"
+    #   end
 
-        assert_button "Publish Changes"
-        assert_button "Preview"
-        assert_button "Add Resource"
-        assert_button "Add Section"
-        assert_button "Export"
-        assert_button "Save"
-        assert_button "Cancel"
-        refute_button "Clone"
-      end
-
-      it 'section actions' do
-        visit layout_section_path @casebook, @section
-
-        assert_button "Preview"
-        assert_button "Add Resource"
-        assert_button "Add Section"
-        assert_button "Export"
-        assert_button "Save"
-        assert_button "Cancel"
-        refute_button "Clone"
-      end
-
-      it 'resource actions in edit' do
-        visit resource_path @casebook, @resource
-
-        assert_button "Preview"
-        assert_button "Export"
-        assert_button "Save"
-        assert_button "Cancel"
-        refute_button "Clone"
-      end
-    end
-
-    describe 'preview mode ' do
-      it 'casebook actions' do
-        visit casebook_path @casebook 
-
-        assert_button "Publish Changes"
-        assert_button "Return to Draft"
-        assert_button "Export"
-        refute_button "Clone"
-      end
-
-      it 'section actions' do
-        visit section_path @casebook, @section
-
-        assert_button "Return to Draft"
-        assert_button "Export"
-        refute_button "Clone"
-      end
-
-      it 'resource actions' do
-        visit resource_path @casebook, @resource
-
-        assert_button "Revise"
-        refute_button "Clone"
-      end
-    end
-
-    describe 'published mode' do
-      it 'casebook actions' do
-        visit casebook_path @casebook
-
-        assert_button "Return to Draft"
-        assert_button "Clone"
-        assert_button "Export"
-      end
-
-      it 'section actions' do
-        visit section_path @casebook, @section
-
-        assert_button "Return to Draft"
-        assert_button "Clone"
-        assert_button "Export"
-      end
-
-      it 'resource actions' do
-        visit resource_path @casebook, @resource
-        dom = Nokogiri::HTML(decorated_content.action_buttons)
-
-        assert_button "Return to Draft"
-        assert_button "Clone"
-        assert_button "Export"
-      end
-    end
-  end
-
-  describe 'published casebook with a logged in user that is not a collaborator' do
-    before do
-      sign_in @user = users(:verified_student)
-
-      @casebook = content_nodes(:public_casebook)
-      @section = content_nodes(:public_casebook_section_1)
-      @resource = content_nodes(:public_casebook_section_1_1)
-    end
-
-    it 'casebook actions' do
-      visit casebook_path @casebook
-
-      assert_button "Clone"
-      assert_button "Export"
-    end
-
-    it 'section actions' do
-      visit section_path @casebook, @section
-
-      assert_button "Clone"
-      assert_button "Export"
-    end
-
-    it 'resource actions' do
-      visit resource_path @casebook, @resource
-
-      assert_button "Clone"
-      assert_button "Export"
-    end
-  end
-
-  # describe 'published casebook with an anonymous user' do
-  #   before do
-  #     @casebook = content_nodes(:public_casebook)
-  #     @section = content_nodes(:public_casebook_section_1)
-  #     @resource = content_nodes(:public_casebook_section_1_1)
-  #   end
-
-  #   describe 'casebook actions' do
-  #     assert_button "Clone"
-  #     assert_button "Export"
-  #   end
-
-  #   describe 'section actions' do
-  #     assert_button "Clone"
-  #     assert_button "Export"
-  #   end
-
-  #   describe 'resource actions' do
-  #     assert_button "Clone"
-  #     assert_button "Export"
-  #   end
-  # end
-
+    #   describe 'resource actions' do
+    #     assert_button "Clone"
+    #     assert_button "Export"
+    #   end
+    # end
   end
 end
