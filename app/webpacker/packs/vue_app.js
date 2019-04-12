ec10d141161a47f8cbcdd6160ac97e527fe2b665
Casey <cgruppioni@users.noreply.github.com>
2019-04-12 09:40:29 -0400

    Revert "Set up VueRouter in vue_app."
    This reverts commit 463bc6ddd22ac7ea58a1bbe7e35167084f665821.

--- app/webpacker/packs/vue_app.js
+++ app/webpacker/packs/vue_app.js
@@ -1,7 +1,5 @@
 import Vue from "vue/dist/vue.esm";
-import VueRouter from 'vue-router';
 Vue.config.productionTip = process.env.NODE_ENV == "development";
-Vue.use(VueRouter);
 
 import store from "../store/index";
 import "../config/axios";
@@ -13,16 +11,9 @@ Vue.use(contenteditableDirective);
 import TheResourceBody from "../components/TheResourceBody";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const routes = [];
-
-  const router = new VueRouter({
-    routes
-  });
-
   const app = new Vue({
     el: "#app",
     store,
-    router,
     components: {
       TheResourceBody
     }
