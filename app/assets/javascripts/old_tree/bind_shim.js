a58c95062a5ac9fc367095a2ff3c8494cd4a47ca
Greg Leppert <greg@leppert.me>
2019-04-24 11:29:59 -0400

    Remove unused old_tree js files
    These were previously referenced in layouts/print.html.erb, which has
    since been removed.

--- app/assets/javascripts/old_tree/bind_shim.js
+++
@@ -1,7 +0,0 @@
-//This helps prevent masking JavaScript error messages as wkhtmltopdf reports them
-Function.prototype.bind = Function.prototype.bind || function (thisp) {
-  var fn = this;
-  return function () {
-    return fn.apply(thisp, arguments);
-  };
-};
