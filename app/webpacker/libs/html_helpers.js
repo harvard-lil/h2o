3c86d638fbde20d23a2875fb4f4c966ac4dfef58
Greg Leppert <greg@leppert.me>
2019-05-20 11:16:43 -0400

    Fix opening of xpath selector

--- app/webpacker/libs/html_helpers.js
+++ app/webpacker/libs/html_helpers.js
@@ -37,7 +37,7 @@ export const emptyULToP = (doc) => {
 };
 
 export const wrapBareInlineTags = (doc) => {
-  doc.querySelectorAll(`body > :not(center${BLOCK_LEVEL_ELEMENTS.join('):not(')})`)
+  doc.querySelectorAll(`body > :not(center):not(${BLOCK_LEVEL_ELEMENTS.join('):not(')})`)
       .forEach(el => wrap(el, "p"));
   return doc;
 };
