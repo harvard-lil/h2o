daef6a2837cc9dfc21539c9890ff1615e735e1aa
Greg Leppert <greg@leppert.me>
2019-05-24 10:22:10 -0400

    Add Nokogiri based sanitizers to compliment Tidy

--- app/models/raw_content.rb
+++ app/models/raw_content.rb
@@ -2,6 +2,40 @@ class RawContent < ApplicationRecord
   belongs_to :source, polymorphic: true
 
   def sanitized_content
-    HTMLHelpers::parse_and_process_nodes(content).to_html
+    html = Tidy.exec(content)
+    doc = Nokogiri::HTML(html)
+
+    wrap_bare_inline_tags!(
+      unnest!(doc)
+    ).at('body').inner_html
+  end
+
+  private
+
+  def unnest! html
+    html
+      .xpath('//article | //section | //aside')
+      .each { |el| el.replace el.children }
+    html
+  end
+
+  # Tidy won't wrap these when using the enclose-text flag since, though they are inline elements in some cases, they're block elements in others.
+  # via: https://github.com/htacg/tidy-html5/blob/a71031f9e529f0aa74a819576411594e21767be4/src/tags.c
+  CM_MIXED = ["a",
+              "del",
+              "ins",
+              "math",
+              "noscript",
+              "script",
+              "svg",
+              "nolayer",
+              "server",
+              "menuitem"]
+
+  def wrap_bare_inline_tags! html
+    html
+      .xpath("//body/*[self::#{CM_MIXED.map(&:downcase).join('|self::')}]")
+      .each { |el| el.wrap "<p>" }
+    html
   end
 end
