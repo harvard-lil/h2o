59ccc4d439b2708338e68b7151fb21e679cadaaa
Greg Leppert <greg@leppert.me>
2019-05-30 13:47:23 -0400

    Add annotation offset update callback to content_annotatable

--- app/models/concerns/content_annotatable.rb
+++ app/models/concerns/content_annotatable.rb
@@ -4,19 +4,20 @@ module ContentAnnotatable
   included do
     has_one :raw_content, as: :source, dependent: :destroy
     before_create :build_raw_content_and_assign_sanitized
-    after_update :update_annotation_offsets
+    after_update :update_annotation_offsets, if: :saved_change_to_content?
+  end
+
+  class_methods do
+    def annotated
+      where('id IN (SELECT DISTINCT content_nodes.resource_id FROM content_nodes INNER JOIN content_annotations ON content_nodes.id = content_annotations.resource_id WHERE content_nodes.resource_type = ?)', self.class.name)
+    end
   end
 
   def annotations
     Content::Annotation
       .joins(:resource)
-      .where(content_nodes: {resource_type: self.class.name, resource_id: id})
-  end
-
-  class_methods do
-    def annotated
-      where('id IN (SELECT DISTINCT content_nodes.resource_id FROM content_nodes INNER JOIN content_annotations ON content_nodes.id = content_annotations.resource_id WHERE content_nodes.resource_type = ?)', self.name)
-    end
+      .where(content_nodes: {resource_type: self.class.name,
+                             resource_id: id})
   end
 
   private
@@ -28,5 +29,20 @@ module ContentAnnotatable
   end
 
   def update_annotation_offsets
+    diffs = DiffHelpers.get_diffs(Nokogiri::HTML(previous_changes[:content][0]).text,
+                                  Nokogiri::HTML(previous_changes[:content][1]).text)
+
+    # the where() selects only annotations that might be affected by the changes
+    annotations
+      .where("global_end_offset >= ?", DiffHelpers.get_first_delta_offset(diffs))
+      .each do |a|
+      range = (a.global_start_offset..a.global_end_offset)
+      new_range = DiffHelpers.adjust_range(diffs, range)
+      if range != new_range
+        a.update global_start_offset: new_range.min,
+                 global_end_offset: new_range.max
+      end
+    end
+    true
   end
 end
