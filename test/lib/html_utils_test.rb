e156e06970cc51e8b86de6a6a77f6e452303fae5
Greg Leppert <greg@leppert.me>
2019-06-04 15:16:58 -0400

    Rename HTMLUtils.process to HTMLUtils.sanitize

--- test/lib/html_utils_test.rb
+++ test/lib/html_utils_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class HTMLUtilsTest < ActiveSupport::TestCase
-  describe HTMLUtils, :process do
+  describe HTMLUtils, :sanitize do
     it "should remove comment nodes from HTML" do
       comments = "<!-- foo --><!-- bar -->"
       html = "#{comments}<div>foobar</div>"
-      assert_equal html.sub(comments, ""), HTMLUtils.process(html)
+      assert_equal html.sub(comments, ""), HTMLUtils.sanitize(html)
     end
 
     it "should pull content out of specific tags and place it at the parent level" do
@@ -13,32 +13,32 @@ class HTMLUtilsTest < ActiveSupport::TestCase
        "section"].each do |tag|
         after = "<p>foobar</p>"
         before = "<#{tag}>#{after}</#{tag}>"
-        assert_equal after, HTMLUtils.process(before)
+        assert_equal after, HTMLUtils.sanitize(before)
       end
     end
 
     it "should find ul elements with text but no li's and convert them to p's" do
       before = "<ul>foobar</ul>"
       after = "<p>foobar</p>"
-      assert_equal after, HTMLUtils.process(before)
+      assert_equal after, HTMLUtils.sanitize(before)
     end
 
     it "should find inline elements at the top level and wrap them in a paragraph" do
       before = "<span>foobar</span>"
       after = "<p>#{before}</p>"
-      assert_equal after, HTMLUtils.process(before)
+      assert_equal after, HTMLUtils.sanitize(before)
     end
 
     it "should not wrap block level elements" do
       html = "<div>foobar</div>"
-      assert_equal html, HTMLUtils.process(html)
+      assert_equal html, HTMLUtils.sanitize(html)
     end
 
     it "should remove whitespace between top level nodes" do
       before = "<div>foo</div>  \n  \n<div>bar</div>  \n  \n"
       after = "<div>foo</div><div>bar</div>"
 
-      assert_equal after, HTMLUtils.process(before)
+      assert_equal after, HTMLUtils.sanitize(before)
     end
   end
 end
