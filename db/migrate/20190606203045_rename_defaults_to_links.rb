c83c9b9b948f402838e2e8321d6767dc0dbe2306
Casey Gruppioni <cgruppioni@gmail.com>
2019-06-07 15:57:04 -0400

    Add update query to rename_defaults migration.

--- db/migrate/20190606203045_rename_defaults_to_links.rb
+++ db/migrate/20190606203045_rename_defaults_to_links.rb
@@ -1,9 +1,13 @@
 class RenameDefaultsToLinks < ActiveRecord::Migration[5.2]
   def up
     rename_table :defaults, :links
+
+    Content::Resource.where(resource_type: "Default").map {|resource| resource.update(resource_type: "Link")}
   end
 
   def down
     rename_table :links, :defaults
+
+    Content::Resource.where(resource_type: "Link").map {|resource| resource.update(resource_type: "Default")}
   end
 end
