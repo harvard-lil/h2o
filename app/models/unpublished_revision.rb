e8f4d6cdf23f3851a44ffa0d76048dd352e6d86d
Casey Gruppioni <cgruppioni@gmail.com>
2019-06-07 13:07:27 -0400

    Delete unused default views.

--- app/models/unpublished_revision.rb
+++ app/models/unpublished_revision.rb
@@ -4,9 +4,3 @@ class UnpublishedRevision < ApplicationRecord
   belongs_to :node_parent, class_name: 'Content::Node' #annotation parent ancestor
   belongs_to :annotation, class_name: 'Content::Annotation', optional: true
 end
-
-# deleted annotation example:
-# node_id: published_resource_id
-# field: 'deleted_annotation'
-# value: nil
-# annotation_id: published_annotation_id
