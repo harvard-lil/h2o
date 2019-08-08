f9d0f5ce1434b9acfe31c00f44ba62fca4ab815d
Casey Gruppioni <cgruppioni@gmail.com>
2019-08-08 15:28:49 -0400

    Delete taggings table form schema.

--- db/schema.rb
+++ db/schema.rb
@@ -244,26 +244,6 @@ ActiveRecord::Schema.define(version: 2019_08_08_191224) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "taggings", id: :serial, force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "taggable_id"
-    t.integer "tagger_id"
-    t.string "tagger_type", limit: 255
-    t.string "taggable_type", limit: 255
-    t.string "context", limit: 255
-    t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
-    t.index ["tagger_type"], name: "index_taggings_on_tagger_type"
-  end
-
   create_table "text_blocks", id: :serial, force: :cascade do |t|
     t.string "name", limit: 255, null: false
     t.string "content", limit: 5242880, null: false
