2cd5c8bb532a6263ead458161a9d46d3f8223eeb
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-08-08 13:34:11 -0400

    Remove legacy tables and superfluous id columns from models

--- _python/main/models.py
+++ _python/main/models.py
@@ -27,34 +27,6 @@ class RailsModel(models.Model):
         abstract = True
 
 
-class Annotation(RailsModel):
-    """
-    Legacy table; does not hold currently-used annotations.
-    """
-    collage_id = models.IntegerField(blank=True, null=True)
-    annotation = models.CharField(max_length=10240, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    cloned = models.BooleanField()
-    xpath_start = models.CharField(max_length=255, blank=True, null=True)
-    xpath_end = models.CharField(max_length=255, blank=True, null=True)
-    start_offset = models.IntegerField()
-    end_offset = models.IntegerField()
-    link = models.CharField(max_length=255, blank=True, null=True)
-    hidden = models.BooleanField()
-    highlight_only = models.CharField(max_length=255, blank=True, null=True)
-    annotated_item_id = models.IntegerField()
-    annotated_item_type = models.CharField(max_length=255)
-    error = models.BooleanField()
-    feedback = models.BooleanField()
-    discussion = models.BooleanField()
-    user_id = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'annotations'
-
-
 class ArInternalMetadata(RailsModel):
     key = models.CharField(primary_key=True, max_length=255)
     value = models.CharField(max_length=255, blank=True, null=True)
@@ -102,57 +74,7 @@ class Case(RailsModel):
         db_table = 'cases'
 
 
-class CkeditorAsset(RailsModel):
-    """
-    Legacy table, from when people could embed assets in books.
-    """
-    data_file_name = models.CharField(max_length=255)
-    data_content_type = models.CharField(max_length=255, blank=True, null=True)
-    data_file_size = models.IntegerField(blank=True, null=True)
-    assetable_id = models.IntegerField(blank=True, null=True)
-    assetable_type = models.CharField(max_length=30, blank=True, null=True)
-    type = models.CharField(max_length=30, blank=True, null=True)
-    width = models.IntegerField(blank=True, null=True)
-    height = models.IntegerField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'ckeditor_assets'
-
-
-class Collage(RailsModel):
-    """
-    Legacy table
-    """
-    annotatable_type = models.CharField(max_length=255, blank=True, null=True)
-    annotatable_id = models.IntegerField(blank=True, null=True)
-    name = models.CharField(max_length=250)
-    description = models.CharField(max_length=5120, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    word_count = models.IntegerField(blank=True, null=True)
-    ancestry = models.CharField(max_length=255, blank=True, null=True)
-    public = models.BooleanField(blank=True, null=True)
-    readable_state = models.CharField(max_length=5242880, blank=True, null=True)
-    words_shown = models.IntegerField(blank=True, null=True)
-    user_id = models.IntegerField()
-    annotator_version = models.IntegerField()
-    featured = models.BooleanField()
-    created_via_import = models.BooleanField()
-    version = models.IntegerField()
-    enable_feedback = models.BooleanField()
-    enable_discussions = models.BooleanField()
-    enable_responses = models.BooleanField()
-
-    class Meta:
-        managed = False
-        db_table = 'collages'
-
-
 class ContentAnnotation(RailsModel):
-    id = models.BigAutoField(primary_key=True)
     resource = models.ForeignKey('ContentNode', models.DO_NOTHING, related_name='annotations')
     start_paragraph = models.IntegerField()
     end_paragraph = models.IntegerField(blank=True, null=True)
@@ -171,7 +93,6 @@ class ContentAnnotation(RailsModel):
 
 
 class ContentCollaborator(RailsModel):
-    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey('User', models.DO_NOTHING, blank=True, null=True)
     content = models.ForeignKey('ContentNode', models.DO_NOTHING, blank=True, null=True)
     role = models.CharField(max_length=255, blank=True, null=True)
@@ -185,26 +106,7 @@ class ContentCollaborator(RailsModel):
         unique_together = (('user', 'content'),)
 
 
-class ContentImage(RailsModel):
-    """
-    Legacy table
-    """
-    name = models.CharField(max_length=255, blank=True, null=True)
-    page_id = models.IntegerField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    image_file_name = models.CharField(max_length=255, blank=True, null=True)
-    image_content_type = models.CharField(max_length=255, blank=True, null=True)
-    image_file_size = models.IntegerField(blank=True, null=True)
-    image_updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'content_images'
-
-
 class ContentNode(RailsModel):
-    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=10000, blank=True, null=True)
     slug = models.CharField(max_length=10000, blank=True, null=True)
     subtitle = models.CharField(max_length=10000, blank=True, null=True)
@@ -359,105 +261,6 @@ class Default(RailsModel):
         db_table = 'defaults'
 
 
-class DelayedJob(RailsModel):
-    """
-    Legacy table
-    """
-    priority = models.IntegerField(blank=True, null=True)
-    attempts = models.IntegerField(blank=True, null=True)
-    handler = models.TextField(blank=True, null=True)
-    last_error = models.TextField(blank=True, null=True)
-    run_at = models.DateTimeField(blank=True, null=True)
-    locked_at = models.DateTimeField(blank=True, null=True)
-    failed_at = models.DateTimeField(blank=True, null=True)
-    locked_by = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    queue = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'delayed_jobs'
-
-
-class FrozenItem(RailsModel):
-    """
-    Legacy table
-    """
-    content = models.TextField(blank=True, null=True)
-    version = models.IntegerField()
-    item_id = models.IntegerField()
-    item_type = models.CharField(max_length=255)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'frozen_items'
-
-
-class MediaType(RailsModel):
-    """
-    Legacy table
-    """
-    label = models.CharField(max_length=255, blank=True, null=True)
-    slug = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'media_types'
-
-
-class Media(RailsModel):
-    """
-    Legacy table
-    """
-    name = models.CharField(max_length=255, blank=True, null=True)
-    content = models.TextField(blank=True, null=True)
-    media_type_id = models.IntegerField(blank=True, null=True)
-    public = models.BooleanField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    description = models.CharField(max_length=5242880, blank=True, null=True)
-    user_id = models.IntegerField()
-    created_via_import = models.BooleanField()
-
-    class Meta:
-        managed = False
-        db_table = 'medias'
-
-
-class Metadata(RailsModel):
-    """
-    Legacy table
-    """
-    contributor = models.CharField(max_length=255, blank=True, null=True)
-    coverage = models.CharField(max_length=255, blank=True, null=True)
-    creator = models.CharField(max_length=255, blank=True, null=True)
-    date = models.DateField(blank=True, null=True)
-    description = models.CharField(max_length=5242880, blank=True, null=True)
-    format = models.CharField(max_length=255, blank=True, null=True)
-    identifier = models.CharField(max_length=255, blank=True, null=True)
-    language = models.CharField(max_length=255, blank=True, null=True)
-    publisher = models.CharField(max_length=255, blank=True, null=True)
-    relation = models.CharField(max_length=255, blank=True, null=True)
-    rights = models.CharField(max_length=255, blank=True, null=True)
-    source = models.CharField(max_length=255, blank=True, null=True)
-    subject = models.CharField(max_length=255, blank=True, null=True)
-    title = models.CharField(max_length=255, blank=True, null=True)
-    dc_type = models.CharField(max_length=255, blank=True, null=True)
-    classifiable_type = models.CharField(max_length=255, blank=True, null=True)
-    classifiable_id = models.IntegerField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'metadata'
-
-
 class Page(RailsModel):
     page_title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
@@ -498,60 +301,6 @@ class Permission(RailsModel):
     class Meta:
         managed = False
         db_table = 'permissions'
-
-
-class PlaylistItem(RailsModel):
-    """
-    Legacy table
-    """
-    playlist_id = models.IntegerField(blank=True, null=True)
-    position = models.IntegerField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    notes = models.TextField(blank=True, null=True)
-    public_notes = models.BooleanField()
-    actual_object_type = models.CharField(max_length=255, blank=True, null=True)
-    actual_object_id = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'playlist_items'
-
-
-class Playlist(RailsModel):
-    """
-    Legacy table
-    """
-    name = models.CharField(max_length=1024, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    public = models.BooleanField(blank=True, null=True)
-    ancestry = models.CharField(max_length=255, blank=True, null=True)
-    position = models.IntegerField(blank=True, null=True)
-    counter_start = models.IntegerField()
-    location_id = models.IntegerField(blank=True, null=True)
-    when_taught = models.CharField(max_length=255, blank=True, null=True)
-    user_id = models.IntegerField()
-    primary = models.BooleanField()
-    featured = models.BooleanField()
-    created_via_import = models.BooleanField()
-
-    class Meta:
-        managed = False
-        db_table = 'playlists'
-
-
-class PlaylistsUserCollection(RailsModel):
-    """
-    Legacy table
-    """
-    playlist_id = models.IntegerField(blank=True, null=True)
-    user_collection_id = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'playlists_user_collections'
 
 
 # in Greg PR
@@ -617,36 +366,6 @@ class Session(RailsModel):
         db_table = 'sessions'
 
 
-class Tagging(RailsModel):
-    """
-    Legacy table
-    """
-    tag_id = models.IntegerField(blank=True, null=True)
-    taggable_id = models.IntegerField(blank=True, null=True)
-    tagger_id = models.IntegerField(blank=True, null=True)
-    tagger_type = models.CharField(max_length=255, blank=True, null=True)
-    taggable_type = models.CharField(max_length=255, blank=True, null=True)
-    context = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'taggings'
-        unique_together = (('tag_id', 'taggable_id', 'taggable_type', 'context', 'tagger_id', 'tagger_type'),)
-
-
-class Tag(RailsModel):
-    """
-    Legacy table
-    """
-    name = models.CharField(max_length=255, blank=True, null=True)
-    taggings_count = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'tags'
-
-
 class TextBlock(RailsModel):
     name = models.CharField(max_length=255)
     content = models.CharField(max_length=5242880)
@@ -668,7 +387,6 @@ class TextBlock(RailsModel):
 
 
 class UnpublishedRevision(RailsModel):
-    id = models.BigAutoField(primary_key=True)
     node_id = models.IntegerField(blank=True, null=True)
     field = models.CharField(max_length=255)
     value = models.CharField(max_length=50000, blank=True, null=True)
@@ -681,33 +399,6 @@ class UnpublishedRevision(RailsModel):
     class Meta:
         managed = False
         db_table = 'unpublished_revisions'
-
-
-class UserCollection(RailsModel):
-    """
-    Legacy table
-    """
-    user_id = models.IntegerField(blank=True, null=True)
-    name = models.CharField(max_length=255, blank=True, null=True)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'user_collections'
-
-
-class UserCollectionsUser(RailsModel):
-    """
-    Legacy table
-    """
-    user_id = models.IntegerField(blank=True, null=True)
-    user_collection_id = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'user_collections_users'
 
 
 class User(RailsModel):
