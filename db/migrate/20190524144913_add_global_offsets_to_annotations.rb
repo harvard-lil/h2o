63682088f04c2a71e964a3312173f469a8f33544
Greg Leppert <greg@leppert.me>
2019-08-09 09:00:52 -0400

    Update migration to calc annotation offsets relative to date created

--- db/migrate/20190524144913_add_global_offsets_to_annotations.rb
+++ db/migrate/20190524144913_add_global_offsets_to_annotations.rb
@@ -6,10 +6,22 @@ class AddGlobalOffsetsToAnnotations < ActiveRecord::Migration[5.2]
     add_column :content_annotations, :global_end_offset, :integer
 
     ANNOTATABLES.each do |klass|
+      # Calculate global annotation offsets using the version of HTMLUtils
+      # sanitization in place at the time the annotation was created.
       klass.annotated.find_each do |instance|
-        nodes = HTMLUtils.parse(instance.content).at('body').children
-        breakpoints = AnnotationConverter.nodes_to_breakpoints(nodes)
-        instance.annotations.find_each do |a|
+        # cache these to reduce DB access and parsing time
+        utils = nil
+        breakpoints = nil
+
+        instance.annotations.order(created_at: :asc).find_each do |a|
+          # invalidate cached items if needed
+          date = [a.created_at, HTMLUtils::V3::EFFECTIVE_DATE].min
+          new_utils = HTMLUtils.at(date)
+          if new_utils != utils
+            utils = new_utils
+            nodes = HTMLUtils.parse(utils.sanitize(instance.raw_content.content)).at('body').children
+            breakpoints = AnnotationConverter.nodes_to_breakpoints(nodes)
+          end
           # use update_columns to avoid touching the timestamps
           a.update_columns [:start, :end].map { |position|
             ["global_#{position}_offset",
