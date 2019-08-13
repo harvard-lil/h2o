0b761184fdcc2b38cdcd2cc32edbbbe787eb642f
Casey Gruppioni <cgruppioni@gmail.com>
2019-08-13 14:28:43 -0400

    Remove old admin roles from Ability model.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,22 +4,19 @@ class Ability
   def initialize(user)
     can [:show, :index], :users
     can :show, :pages
-    can [:landing, :index, :author_playlists, :search, :not_found, :load_more_users], :base
+    can [:landing, :index, :search, :not_found], :base
     can [:new, :create, :edit, :update], :password_resets
-    can [:new, :create], :login_notifiers
     can [:new, :create], :user_sessions
     can :index, [:cases, :text_blocks, :defaults]
 
-    can [:show, :export, :export_as], Content::Node, :public => true
-    can [:show, :export, :export_as], Case, :public => true
-    can [:show, :export, :export_as], TextBlock, :public => true
+    can [:show, :export], [Content::Node, Case, TextBlock], :public => true
+    # can [:show, :export], Case, :public => true
+    # can [:show, :export], TextBlock, :public => true
 
     if user.nil?
       can [:new, :create], :users
       return
     else
-      can :create, :responses
-
       can :destroy, :user_sessions
       can [:verification_request, :verify], :users
       can :new, [Content::Casebook, TextBlock, Default]
@@ -29,11 +26,7 @@ class Ability
 
       # Can do things on owned items
       if !user.has_role? :superadmin
-        can [:edit, :show, :update, :destroy, :export, :export_as, :export_unique], [TextBlock, Default], :user_id => user.id
-      end
-
-      can :destroy, Response do |response|
-        response.resource.user == user
+        can [:edit, :show, :update, :destroy, :export], [TextBlock, Default], :user_id => user.id
       end
 
       # superadmins can edit/update any id, not just their own
@@ -44,44 +37,17 @@ class Ability
 
     if user.has_role? :superadmin
       can [:edit, :update], User
-
       can :access, :rails_admin
-      can [:create], :"ckeditor/pictures"
-      can [:create], :"ckeditor/assets"
-      can [:create], :"ckeditor/attachment_files"
+      can [:create], [:"ckeditor/pictures", :"ckeditor/assets", :"ckeditor/attachment_files"]
       can :dashboard, :all
       can [:index, :show, :export, :export_as, :export_unique, :bulk_delete, :destroy, :edit, :update, :position_update, :update_notes, :save_readable_state], :all
       can [:import], [Default]
       can :show_in_app, [Case, User, Content::Casebook]
-
       can [:new, :edit], Page
-
-      can :approve, Case
       can [:new], CaseCourt
       can [:create], :case_courts
-
       can [:new, :edit, :update, :show], [Case, CaseCourt, Default, TextBlock, User]
-
       can :manage_collaborators, [Content::Casebook]
-      can :manage_annotations, [Content::Resource]
-    elsif user.has_role? :case_admin
-      can :access, :rails_admin
-      can [:index, :show, :export, :export_as, :export_unique, :show_in_app], Case
-      can :dashboard, :all
-
-      can [:new, :show, :export, :export_as], Case
-      can :create, :cases
-
-      can :approve, Case
-      can [:new], CaseCourt
-      can [:create], :case_courts
-      # Add functionality, ability to modify case requests
-    elsif user.has_role? :rep
-      user_ids = []
-      associated_user_ids = user_ids.flatten.uniq
-      can :access, :rails_admin
-      can :dashboard, :all
-      can [:index, :show, :export, :export_as, :export_unique, show_in_app], [TextBlock, Default], :user_id => associated_user_ids
     end
   end
 end
