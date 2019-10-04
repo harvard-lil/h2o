f125cc07bc08a07f478a2a15d11670d14df641ca
Ben Steinberg <bsteinberg@law.harvard.edu>
2019-10-04 09:49:56 -0400

    Try STATIC_ROOT

--- _python/config/settings/settings_base.py
+++ _python/config/settings/settings_base.py
@@ -129,8 +129,9 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 RAILS_SECRET_KEY_BASE = None
 
