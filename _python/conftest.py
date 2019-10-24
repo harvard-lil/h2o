be41fccd68f022e4fec143d456f3a3e599a1fde8
Rebecca Cremona <rcremona@law.harvard.edu>
2019-10-24 12:50:37 -0400

    Tweak wording.

--- _python/conftest.py
+++ _python/conftest.py
@@ -249,8 +249,7 @@ def full_casebook(casebook_factory):
 def full_private_casebook(full_casebook):
     """
     >>> private, published = [getfixture(f) for f in ['full_private_casebook', 'full_casebook']]
-    >>> assert published != private
-    >>> assert private.is_private
+    >>> assert private.is_private and not published.is_private
     >>> assert all(node.is_private for node in private.contents.all())
     """
     casebook = full_casebook.clone()
@@ -259,13 +258,11 @@ def full_private_casebook(full_casebook):
 @pytest.fixture
 def full_casebook_with_draft(full_casebook):
     """
-    >>> with_draft, without_draft = [getfixture(f) for f in ['full_casebook_with_draft', 'full_casebook']]
-    >>> assert with_draft != without_draft
-    >>> assert not without_draft.has_draft
-    >>> assert with_draft.has_draft
-    >>> assert with_draft.is_public
-    >>> assert with_draft.drafts().is_private
-    >>> assert all(node.has_draft for node in with_draft.contents.all())
+    >>> has_draft, draftless = [getfixture(f) for f in ['full_casebook_with_draft', 'full_casebook']]
+    >>> assert has_draft.has_draft and not draftless.has_draft
+    >>> assert all(node.has_draft for node in has_draft.contents.all())
+    >>> assert has_draft.is_public
+    >>> assert has_draft.drafts().is_private
     """
     casebook = full_casebook.clone()
     casebook.public = True
