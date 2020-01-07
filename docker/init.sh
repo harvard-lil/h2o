8d3437eab30b528ce5e82f5570338507dc73407f
Rebecca Cremona <rcremona@law.harvard.edu>
2020-01-07 10:54:23 -0500

    File now mandatory.

--- docker/init.sh
+++ docker/init.sh
@@ -2,10 +2,10 @@
 set -e
 
 display_usage() {
-    echo "Create dev and test databases. Dev database may optionally be seeded with data from a pg_dump file."
+    echo "Seed dev database with data from a pg_dump file."
     echo
     echo "Usage:"
-    echo "  bash docker/init.sh [-f dump_file]"
+    echo "  bash docker/init.sh -f dump_file"
 }
 
 # Help
@@ -14,42 +14,38 @@ if [[ ( $1 == "--help") ||  ($1 == "-h")]]; then
     exit 0
 fi
 
-# Optional flag to use dump file
-if [[ $# -gt 0 ]]; then
-    getopts ":f:" opt || true;
-    case $opt in
-        f)
-            if [ -f "$OPTARG" ]
-            then
-                FILE=$OPTARG
-            else
-                echo "Invalid path."
-                exit 1
-            fi
-            ;;
-        \?)
-            # illegal option or argument
-            display_usage
+
+getopts ":f:" opt || true;
+case $opt in
+    f)
+        if [ -f "$OPTARG" ]
+        then
+            FILE=$OPTARG
+        else
+            echo "Invalid path."
             exit 1
-            ;;
-        :)
-            # -f present, but no path provided
-            echo "Please specify the path."
-            exit 1
-            ;;
-    esac
-    if [[ $((OPTIND - $#)) -ne 1 ]]; then
-        # too many args
+        fi
+        ;;
+    \?)
+        # illegal option or argument
         display_usage
         exit 1
-    fi
+        ;;
+    :)
+        # -f present, but no path provided
+        echo "Please specify the path."
+        exit 1
+        ;;
+esac
+if [[ $((OPTIND - $#)) -ne 1 ]]; then
+    # wrong number of args
+    display_usage
+    exit 1
 fi
 
-echo "Creating databases ..."
-if [ "$FILE" ]; then
-    echo "Loading data from $FILE ..."
-    docker cp "$FILE" "$(docker-compose ps -q db)":/tmp/data.dump
-    docker-compose exec db bash -c "pg_restore -l /tmp/data.dump | grep -v schema_migrations | grep -v ar_internal_metadata > /tmp/restore.list"
-    docker-compose exec db pg_restore -L /tmp/restore.list --disable-triggers --username=postgres --verbose --no-owner -h localhost -d postgres /tmp/data.dump
-    docker-compose exec db rm -f /tmp/data.dump /tmp/restore.list
-fi
+echo "Loading data from $FILE ..."
+docker cp "$FILE" "$(docker-compose ps -q db)":/tmp/data.dump
+docker-compose exec db bash -c "pg_restore -l /tmp/data.dump | grep -v schema_migrations | grep -v ar_internal_metadata > /tmp/restore.list"
+docker-compose exec db pg_restore -L /tmp/restore.list --disable-triggers --username=postgres --verbose --no-owner -h localhost -d postgres /tmp/data.dump
+docker-compose exec db rm -f /tmp/data.dump /tmp/restore.list
+
