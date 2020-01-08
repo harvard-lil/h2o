315c8d4c7b6e314ec7ced886daca8cce33135821
Rebecca Cremona <rcremona@law.harvard.edu>
2020-01-08 10:36:33 -0500

    Rename tables to Django's defaults.

--- web/search/create_search_index.sql
+++ web/search/create_search_index.sql
@@ -9,7 +9,7 @@ CREATE MATERIALIZED VIEW search_view AS
                 setweight(to_tsvector(coalesce(c.name_abbreviation, '')), 'D') ||
                 setweight(to_tsvector(coalesce(docket_number, '')), 'D') ||
                 setweight(to_tsvector(string_agg(cite, ', ')), 'A') ||
-                setweight(to_tsvector(coalesce(min(case_courts.name), '')), 'D')
+                setweight(to_tsvector(coalesce(min(main_casecourt.name), '')), 'D')
             )  AS document,
            jsonb_build_object(
                'display_name', coalesce(c.name_abbreviation, c.name),
@@ -19,11 +19,11 @@ CREATE MATERIALIZED VIEW search_view AS
            ) AS metadata,
            'case'::text AS category
     FROM
-        cases c
+        main_case c
         LEFT JOIN (
-            select id as case_id, jsonb_array_elements(citations) ->> 'cite' as cite from cases
+            select id as case_id, jsonb_array_elements(citations) ->> 'cite' as cite from main_case
         ) as citations on citations.case_id = c.id
-        LEFT JOIN case_courts ON c.case_court_id = case_courts.id
+        LEFT JOIN main_casecourt ON c.case_court_id = main_casecourt.id
     WHERE
         c.public = true
     GROUP BY c.id
@@ -47,9 +47,9 @@ UNION ALL
         ) AS metadata,
         'casebook'::text AS category
     FROM
-        content_nodes c
-        LEFT JOIN content_collaborators cc ON cc.content_id = c.id AND cc.role = 'owner' AND cc.has_attribution = true
-        LEFT JOIN users u ON cc.user_id = u.id
+        main_contentnode c
+        LEFT JOIN main_contentcollaborator cc ON cc.content_id = c.id AND cc.role = 'owner' AND cc.has_attribution = true
+        LEFT JOIN main_user u ON cc.user_id = u.id
     WHERE
         casebook_id IS NULL AND
         public = true AND
@@ -70,36 +70,12 @@ UNION ALL
            ) AS metadata,
            'user'::text AS category
     FROM
-        users u
-        INNER JOIN content_collaborators cc ON cc.user_id = u.id AND cc.role = 'owner'
-        INNER JOIN content_nodes cn ON cc.content_id = cn.id AND cn.public = TRUE
+        main_user u
+        INNER JOIN main_contentcollaborator cc ON cc.user_id = u.id AND cc.role = 'owner'
+        INNER JOIN main_contentnode cn ON cc.content_id = cn.id AND cn.public = TRUE
     WHERE
           u.verified_professor = true AND
           u.attribution != ''
     GROUP BY u.id
 ;
 CREATE UNIQUE INDEX search_view_refresh_index ON search_view (result_id, category);
-
--- -- get search results
--- SELECT category, result_id, metadata
--- FROM search_view
--- WHERE
---     document @@ to_tsquery('court')
---     AND category = 'case'
--- ORDER BY ts_rank(document,to_tsquery('court')) desc, result_id;
---
--- -- get search counts
--- SELECT category, count(*)
--- FROM search_view
--- WHERE document @@ to_tsquery('court')
--- GROUP BY category;
---
--- --   (this could be a single query for multiple facets in postgres 9.6, using array_to_tsvector and ts_stat --
--- --    see https://roamanalytics.com/2019/04/16/faceted-search-with-postgres-using-tsvector/ )
--- -- get a single facet
--- SELECT DISTINCT metadata->>'attribution'
--- FROM search_view
--- WHERE
---     document @@ to_tsquery('court')
---     AND category = 'casebook';
-
