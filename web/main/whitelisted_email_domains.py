17044ac0da239339f0ce0b3fb89b7130d2d22703
Brennan Chesley <bchesley@law.harvard.edu>
2020-03-03 09:30:08 -0500

    Whitelist fldm.edu.mx

--- web/main/whitelisted_email_domains.py
+++ web/main/whitelisted_email_domains.py
@@ -1,4 +1,5 @@
 # Some european universities use email addresses not ending in edu.
 whitelisted_email_domains = set(
     ['uclouvain.be', # Université catholique de Louvain
+     'fldm.edu.mx', # Facultdad Libre De Derecho De Monterrey
      ])
