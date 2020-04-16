896c81173d87ad3e1c6359982a04a7a063e94fea
Brennan Chesley <bchesley@law.harvard.edu>
2020-04-16 15:59:24 -0400

    Quick CSS fix

--- web/frontend/components/SectionCloner.vue
+++ web/frontend/components/SectionCloner.vue
@@ -58,7 +58,7 @@ export default {
 
 <style lang="scss">
 .casebook-actions button.action.clone-casebook-nodes {
-    background-image: url(http://localhost:8080/static/dist/img/clone.fc6eb4b4.svg);
+    background-image: url(/static/dist/img/clone.fc6eb4b4.svg);
     border: none;
 }
 
