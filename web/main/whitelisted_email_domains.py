c552337bb89ffbbcd824d40c97331d3d5c849047
Brennan Chesley <bchesley@law.harvard.edu>
2020-04-27 14:51:20 -0400

    Whitelist University of Nairobi

--- web/main/whitelisted_email_domains.py
+++ web/main/whitelisted_email_domains.py
@@ -2,4 +2,6 @@
 whitelisted_email_domains = set(
     ['uclouvain.be', # Université catholique de Louvain
      'fldm.edu.mx', # Facultdad Libre De Derecho De Monterrey
-     ])
+     'students.uonbi.ac.ke', # University of Nairobi
+     'uonbi.ac.ke',
+    ])
