b6216e0c1088acbc9ca4dbee6b9aa38d29be22f2
Brennan Chesley <bchesley@law.harvard.edu>
2020-07-08 20:46:44 -0400

    Casebook TOC and audit performance improvements

--- web/frontend/store/modules/table_of_contents.js
+++ web/frontend/store/modules/table_of_contents.js
@@ -4,11 +4,38 @@ import Vue from "vue";
 
 const state = {
     toc: {},
-    augments: {},
-    augmentedToc: {}
+    augments: {}
 };
 
+function augmentNode(node, traits) {
+    _.keys(traits).forEach(k => Vue.set(node, k, traits[k]));
+}
+
 const helpers = {
+    addCSSClass: (cls) => {
+        return (node) => {
+            node.cssClasses = _.get(node, 'cssClasses', []).filter(x => x !== cls).concat([cls]);
+            return node;
+        }
+    },
+    removeCSSClass: (cls) => {
+        return (node) => {
+            node.cssClasses = _.get(node, 'cssClasses', []).filter(x => x !== cls);
+            return node;
+        }
+    },
+    addFlag: (flagName) => {
+        return (node) => {
+            node[flagName] = true;
+            return node;
+        };
+    },
+    removeFlag: (flagName) => {
+        return (node) => {
+            node[flagName] = false;
+            return node;
+        }
+    },
     flatFilter: (tree, filterFn) => {
         // Return any node for which filterFn returns true
         function dive(node) {
@@ -26,7 +53,7 @@ const helpers = {
     },
     firstMatching: (tree, pred) => {
         function dive(node) {
-            return pred(node) ? node : _.find(_.flatMap(node.children || [],dive).filter(_.identity), pred);
+            return pred(node) ? node : _.find(_.flatMap(node.children || [], dive).filter(_.identity), pred);
         }
         return dive(tree);
     },
@@ -40,14 +67,10 @@ const helpers = {
     resourcePath: (casebook, subsection) => {
         return !subsection ? `/casebook/${casebook}/toc` : `/casebook/${casebook}/toc/${subsection}`;
     },
-    findNode: (toc, casebook, id) => {
-        if (!(casebook in toc)) {
-            return null;
-        }
-        if (!id) {
-            return toc[casebook];
-        }
-        return helpers.firstMatching(toc[casebook], (node) => node.id === id);
+    findNode: (toc, id) => {
+        const matchingNode = (node) => node.id === id;
+        let trees = _.values(toc).map((tree) => helpers.firstMatching(tree, matchingNode));
+        return _.find(trees, _.identity);
     },
     auditIDs: (toc, casebook) => {
         function isTemp(node) {
@@ -58,37 +81,48 @@ const helpers = {
         }
         return helpers.flatFilter(toc[casebook], isTemp).map(node => node.id);
     },
+    augmentNode,
     augmentNodes: (tree, augments) => {
         const base = {};
         function dive(node) {
             let traits = _.get(augments, node.id, base);
-            let modified = _.merge(_.omit(_.cloneDeep(node), _.keys(traits)), traits);
-            modified.children = _.map(modified.children || [], dive);
-            return modified;
+            augmentNode(node, traits);
+            (node.children || []).forEach(dive);
         }
-        return dive(tree);
+        dive(tree);
     }
 };
 
 const getters = {
-    getRawNode: state => (casebook, id) => helpers.findNode(state.toc, casebook, id),
-    getAugmentedNode: state => (casebook, id) => {
-        let node = helpers.findNode(state.toc, casebook, id);
-        return node && helpers.augmentNodes(node, state.augments);
-    },
+    getNode: state => (id) => helpers.findNode(state.toc, id),
     auditTargets: state => (casebook) => helpers.auditIDs(state.toc, casebook)
 };
 
+const collapseNode = (node) => helpers.addCSSClass('collapsed')(helpers.addFlag('collapsed')(node))
+const expandNode = (node) => helpers.removeCSSClass('collapsed')(helpers.removeFlag('collapsed')(node))
+//const setLoading = helpers.addFlag('loading')
+const setLoaded = (node) => helpers.removeCSSClass('loading')(helpers.addCSSClass('loaded')(node))
+const setAudit = helpers.addFlag('audit')
+const removeAudit = helpers.removeFlag('audit')
 
 const mutations = {
     overwrite: (state, payload) => {
+        helpers.augmentNodes(payload, state.augments);
         Vue.set(state.toc, payload.id, payload);
-        Vue.set(state.augmentedToc, payload.id, helpers.augmentNodes(payload, state.augments));
     },
     delete: (state, payload) => {
         function deleteRecursive(node, id) {
+            function childStream(node) {
+                return [node.id].concat(_.flatMap(node.children || [], childStream));
+            }
             for (let ii = 0; ii < node.children.length; ii++) {
                 if (node.children[ii].id === id) {
+                    let children = childStream(node.children[ii]) || [];
+                    children.forEach(child => {
+                        if (_.has(state.augments, child)) {
+                            Vue.delete(state.augments, child);
+                        }
+                    })
                     Vue.delete(node.children, ii);
                     ii--;
                 } else {
@@ -99,92 +133,73 @@ const mutations = {
         if (!(payload.casebook in state.toc)) {
             return false;
         }
-        let rawToc = deleteRecursive(state.toc[payload.casebook], payload.id);
-        Vue.set(state.augmentedToc, payload.casebook, helpers.augmentNodes(state.toc[payload.casebook], state.augments));
-        return rawToc;
+        deleteRecursive(state.toc[payload.casebook], payload.id);
+        return state.toc;
     },
     shuffle: (state, { id, children }) => {
         if (id in state.toc) {
+            children.forEach(node => helpers.augmentNodes(node, state.augments));
             Vue.set(state.toc[id], 'children', children);
-            Vue.set(state.augmentedToc, id, helpers.augmentNodes(state.toc[id], state.augments));
         }
     },
     modifyAugment: (state, { id, ids, modifyFn }) => {
-        if (id) {
-            let updatedNode = modifyFn(_.get(state.augments, id, {}));
-            Vue.set(state.augments, id, updatedNode);
-        }
-        if (ids) {
-            ids.forEach(ii => {
-                let updatedNode = modifyFn(_.get(state.augments, ii, {}));
-                Vue.set(state.augments, ii, updatedNode);
-            })
-        }
-        for (let k in state.toc) {
-            Vue.set(state.augmentedToc, k, helpers.augmentNodes(state.toc[k], state.augments));
-        }
+        let toUpdate = (id ? [id] : []).concat(ids || []);
+
+        toUpdate.forEach(ii => {
+            let updatedNode = modifyFn(_.get(state.augments, ii, {}));
+            Vue.set(state.augments, ii, updatedNode);
+            let node = helpers.findNode(state.toc, ii);
+            if (!node) {
+                if (_.has(state.augments, ii)) {
+                    Vue.delete(state.augments, ii);
+                }
+                return;
+            }
+            helpers.augmentNode(node, updatedNode);
+        })
     }
 };
 
 const actions = {
     revealNode: ({ commit, state }, { casebook, id }) => {
         let ids = helpers.findPath(state.toc[casebook], node => node.id === id);
-        commit('modifyAugment', {
-            ids, modifyFn: (node) => {
-                delete node['collapsed'];
-                delete node['children'];
-            }
-        })
+        commit('modifyAugment', { ids, modifyFn: expandNode })
     },
     toggleCollapsed: ({ commit }, { id }) => {
         commit('modifyAugment', {
-            id, modifyFn: (node) => {
-                if ('collapsed' in node) {
-                    delete node['children'];
-                    delete node['collapsed'];
-                } else {
-                    node.collapsed = true;
-                    node.children = [];
-                }
-                return node;
-            }
+            id,
+            modifyFn: (node) => _.get(node, 'collapsed', false) ? expandNode(node) : collapseNode(node)
         });
     },
     setAudit: ({ commit, state }, { id }) => {
-        let currentAudits = _.keys(state.augments).filter(k => _.has(state.augments[k], 'audit'));
-        commit('modifyAugment', { ids: currentAudits, modifyFn: (node) => { delete node['audit']; return node; } });
-        commit('modifyAugment', { id, modifyFn: (node) => { node.audit = true; return node; } });
+        let currentAudits = _.keys(state.augments).filter(k => _.get(state.augments[k], 'audit', false));
+        commit('modifyAugment', { ids: currentAudits, modifyFn: removeAudit });
+        commit('modifyAugment', { id, modifyFn: setAudit });
     },
-    clearAudit:({ commit }, { id }) => {
-        commit('modifyAugment', { id, modifyFn: (node) => { delete node['audit']; return node; } });
+    clearAudit: ({ commit }, { id }) => {
+        commit('modifyAugment', { id, modifyFn: removeAudit });
     },
     slowMerge: ({ commit, state }, { casebook, newToc }) => {
         // Compare the current state to the newToc and get a list of new nodes
         // Cascade a delay for the new nodes, first node immediate, each after that appearing in a sec.
         // animationState: 'loading' -> 'loaded'
         function notInPreviousTree(node) {
-            return !helpers.findNode(state.toc, casebook, node.id);
+            return !helpers.findNode(state.toc, node.id);
         }
 
         let newNodes = helpers.flatFilter(newToc, notInPreviousTree);
-        newNodes.forEach(node => node.animationState = 'loading');
-        commit('modifyAugment', {
-            ids: newNodes.filter(node => node.children.length > 0).map(node => node.id),
-            modifyFn: (node) => {
-                node.collapsed = true;
-                node.children = [];
-                return node;
-            }
-        });
+        
         const delayGap = 250;
+        newNodes.forEach(node => {
+            state.augments[node.id] = {cssClasses: ['loading', 'collapsed'], collapsed: node.children.length > 0};
+            node.collapsed = true;
+            node.cssClasses = ['loading', 'collapsed'];
+        })
         commit('overwrite', newToc);
         setTimeout(() => {
             commit('modifyAugment', {
                 ids: newNodes.map(x => x.id),
-                modifyFn: (node) => {
-                    node.animationState = 'loaded';
-                    return node;
-                }
+                modifyFn: setLoaded
             })
         }, delayGap);
     },
@@ -204,7 +219,7 @@ const actions = {
         if (!targetId || targetId === "") {
             return false;
         }
-        let node = helpers.findNode(state.toc, rootNode, targetId);
+        let node = helpers.findNode(state.toc, targetId);
         if (node === null) {
             return false;
         }
