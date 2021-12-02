1a0d61c5c974f0561ae60b4faa16aef403cd3bd1
Brennan Chesley <bchesley@law.harvard.edu>
2021-12-02 13:08:04 -0600

    Include TinyMCE icons after update

--- web/frontend/pages/rich_text_editor.js
+++ web/frontend/pages/rich_text_editor.js
@@ -1,6 +1,7 @@
 import tinymce from 'tinymce/tinymce';
 window.tinymce = tinymce;
 import 'tinymce/themes/silver';
+import 'tinymce/icons/default';
 import 'tinymce/plugins/link';
 import 'tinymce/plugins/lists';
 import 'tinymce/plugins/image';
