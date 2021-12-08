1cc087f393af8ebb2e20f376cb73a142b17e2796
Rebecca Cremona <rcremona@law.harvard.edu>
2021-12-08 17:03:12 -0500

    Allow for S3 auth by key locally, but by IAM role in prod.

--- docker/pandoc-lambda/function/app.py
+++ docker/pandoc-lambda/function/app.py
@@ -9,14 +9,16 @@ def handler(event, context):
     input_s3_key = event['filename']
     is_casebook = event['is_casebook']
 
+    s3_config = {}
+    if os.environ.get('USE_S3_CREDENTIALS'):
+        s3_config['endpoint_url'] = os.environ['S3_ENDPOINT_URL']
+        s3_config['aws_access_key_id'] = os.environ['AWS_ACCESS_KEY_ID']
+        s3_config['aws_secret_access_key'] = os.environ['AWS_SECRET_ACCESS_KEY']
+
     with tempfile.NamedTemporaryFile(suffix='.docx') as pandoc_in:
 
         # get the source html
-        s3 = boto3.resource('s3',
-            endpoint_url=os.environ['S3_ENDPOINT_URL'],
-            aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-            aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        )
+        s3 = boto3.resource('s3', **s3_config)
         s3.Bucket(os.environ['EXPORT_BUCKET']).download_fileobj(input_s3_key, pandoc_in)
         pandoc_in.seek(0)
 
