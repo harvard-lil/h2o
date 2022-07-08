9be29ea73ca651add5fd45dfbc49cc75f8dfe170
Varun Iyer <varun_iyer@posteo.net>
2022-07-08 13:39:07 -0400

    Expand future possibility to show more headlines

--- web/main/models.py
+++ web/main/models.py
@@ -1038,10 +1038,13 @@ class FullTextSearchIndex(models.Model):
         ids = sorted([r.result_id for r in results[0]])
         query_class = ({"legal_doc_fulltext": LegalDocument, "textblock": TextBlock, "link":Link})[category]
         content_name = "description" if category == "link" else "content"
-        headlines = query_class.objects.filter(id__in=ids).order_by('id').annotate(headline=SearchHeadline(content_name, query, max_fragments=1)).values_list("headline")
+        headlines = query_class.objects.filter(id__in=ids).order_by('id').annotate(headlines=SearchHeadline(content_name, query, max_fragments=10, min_words=10, max_words=20)).values_list("headlines")
         headlines = {i: h for i, h in zip(ids, headlines)}
         for r in results[0]:
-            r.metadata["headline"] = headlines[r.result_id][0]
+            try:
+                r.metadata["headlines"] = headlines[r.result_id][0].split("...")
+            except AttributeError:
+                continue
         return results
 
 class USCodeIndex(models.Model):
