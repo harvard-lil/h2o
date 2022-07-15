1ec5490839a3aceca60cdf11254c450b764b7ae4
Varun Iyer <varun_iyer@posteo.net>
2022-07-15 15:16:35 -0400

    Simplify casebook_fts test cases

--- web/main/models.py
+++ web/main/models.py
@@ -1055,24 +1055,13 @@ class FullTextSearchIndex(models.Model):
         >>> FullTextSearchIndex().create_search_index()
 
         Search in casebook by query:
-        >>> assert dump_search_results([FullTextSearchIndex().casebook_fts(casebooks[0].id, "legal_doc_fulltext", query_str='Dubious'),]) == (
-        ...     [
-        ...         {'citations': ['Adventures in criminality, 1 Fake 1, (2001)',], 'display_name': 'Legal Doc 0', 'jurisdiction': None, 'effective_date': '1900-01-01T00:00:00+00:00', 'effective_date_formatted': 'January   1, 1900', 'headlines': ['<b>Dubious</b> legal claim'], 'ordinals': '', 'year': '1900'},
-        ...         {'citations': ['Adventures in criminality, 1 Fake 1, (2001)',], 'display_name': 'Legal Doc 1', 'jurisdiction': None, 'effective_date': '1900-01-01T00:00:00+00:00', 'effective_date_formatted': 'January   1, 1900', 'headlines': ['<b>Dubious</b> legal claim'], 'ordinals': '', 'year': '1900'},
-        ...         {'citations': ['Adventures in criminality, 1 Fake 1, (2001)',], 'display_name': 'Legal Doc 2', 'jurisdiction': None, 'effective_date': '1900-01-01T00:00:00+00:00', 'effective_date_formatted': 'January   1, 1900', 'headlines': ['<b>Dubious</b> legal claim'], 'ordinals': '', 'year': '1900'}
-        ...     ],
-        ... )
-
         >>> assert dump_search_results([FullTextSearchIndex().casebook_fts(casebooks[0].id, 'legal_doc_fulltext', query_str='2'),]) == (
         ...     [
         ...         {'citations': ['Adventures in criminality, 1 Fake 1, (2001)',], 'display_name': 'Legal Doc 2', 'jurisdiction': None, 'effective_date': '1900-01-01T00:00:00+00:00', 'effective_date_formatted': 'January   1, 1900', 'headlines': ['Dubious legal claim <b>2</b>'], 'ordinals': '', 'year': '1900'}
         ...     ],
         ... )
-        >>> assert dump_search_results([FullTextSearchIndex().casebook_fts(casebooks[0].id, 'textblock', query_str='2'),]) == (
-        ...     [
-        ...         {'name': 'Some TextBlock Name 2', 'description': 'Some TextBlock Description 2', 'ordinals': '', 'headlines': ['Some TextBlock Content <b>2</b>'], 'casebook_id': casebooks[0].id}
-        ...     ],
-        ... )
+        >>> assert len(dump_search_results([FullTextSearchIndex().casebook_fts(casebooks[0].id, "legal_doc_fulltext", query_str='Dubious'),])[0]) == 3
+        >>> assert len(dump_search_results([FullTextSearchIndex().casebook_fts(casebooks[0].id, 'textblock', query_str='2'),])[0]) == 1
         """
 
         casebook = Casebook.objects.get(id=casebook_id)
