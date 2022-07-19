e887009318e0d43267ce7e0f09a01009b954f799
shljessie <sl994@cornell.edu>
2022-07-19 10:21:09 -0400

    fixing black lint error

--- web/main/models.py
+++ web/main/models.py
@@ -2064,9 +2064,14 @@ class ContentNode(
     @property
     # gets the next node ordinals
     def next_node(self) -> Optional[Tuple[int, ...]]:
-        ordinals = [_[0] for _ in ContentNode.objects.filter(casebook_id=self.casebook_id).order_by("ordinals").values_list("ordinals")]
+        ordinals = [
+            _[0]
+            for _ in ContentNode.objects.filter(casebook_id=self.casebook_id)
+            .order_by("ordinals")
+            .values_list("ordinals")
+         ]
         idx = ordinals.index(self.ordinals)
-        
+
         if idx + 1 >= len(self.ordinals):
             return None
         return ordinals[idx + 1]
@@ -2074,7 +2079,12 @@ class ContentNode(
     @property
     # gets the previous node ordinals
     def prev_node(self) -> Optional[Tuple[int, ...]]:
-        ordinals = [_[0] for _ in ContentNode.objects.filter(casebook_id=self.casebook_id).order_by("ordinals").values_list("ordinals")]
+        ordinals = [
+            _[0]
+            for _ in ContentNode.objects.filter(casebook_id=self.casebook_id)
+            .order_by("ordinals")
+            .values_list("ordinals")
+        ]
         idx = ordinals.index(self.ordinals)
 
         if idx > 0:
