8475e51fb2876b70d1cabbf73be6c9beb5a9ce4e
Liza Daly <lizadaly@users.noreply.github.com>
2022-10-24 13:03:29 -0400

    Add PDF generation for #1555 (#1809)
    * Add PDF generation for #1555
    
    * Commit missing files

---
+++ web/main/pdf.py
@@ -0,0 +1,52 @@
+# Generate a PDF via playwright
+from pathlib import Path
+from playwright.sync_api import sync_playwright, expect, Page
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def generate_pdf(
+    url: str,
+    output_file: Path,
+    page: Page,
+    selector: str = "main.preview-ready",
+    timeout=120_000,
+):
+    """Generate a PDF from a given URL, return a Path object to the pdf on the filesystem"""
+    logger.info(f"Requesting {url}...")
+
+    resp = page.goto(url)
+    assert resp
+    assert resp.ok
+    assert "/accounts/login" not in resp.url
+
+    logger.info(
+        f"Got status code {resp.status}, waiting for printable page and selector {selector}"
+    )
+    page.on("console", lambda msg: logger.warning(f"From browser console: {msg}"))
+    expect(page.locator(selector)).to_be_visible(timeout=timeout)
+    pdf = page.pdf()
+    output_file.write_bytes(pdf)
+
+    logger.info(f"Wrote output to {output_file}")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "url", help="The URL to the pdf endpoint. Must be a publicly-accessible URL"
+    )
+    parser.add_argument("pdf", help="Fully-qualified path including filename for the PDF output")
+    parser.add_argument("--headed", action="store_true", help="Run in headed mode")
+    args = parser.parse_args()
+    output_file = Path(args.pdf)
+
+    with sync_playwright() as p:
+        logger.info("Launching browser")
+        browser = p.chromium.launch(headless=not args.headed)
+        page = browser.new_page()
+        generate_pdf(args.url, output_file, page)
--- web/main/test/functional/test_functional.py
+++ web/main/test/functional/test_functional.py
@@ -1,8 +1,12 @@
 # These tests are run through LiveServerTestCase and playwright
+from pathlib import Path
 import pytest
 from django.core.management import call_command
 from playwright.sync_api import Page, expect
 from pytest_django.live_server_helper import LiveServer
+from main.pdf import generate_pdf
+from main.models import Casebook
+from django.urls import reverse
 
 
 @pytest.fixture(autouse=True, scope="function")
@@ -32,7 +36,7 @@ def static_live_server(request, settings):
 
 
 @pytest.fixture
-def login(static_live_server, page: Page, user="test@example.edu", password="changeme"):
+def login(static_live_server, page: Page, user="functional-test@example.edu", password="changeme"):
     """Do the login step for the default user"""
     page.goto(static_live_server.url)
     page.get_by_role("link", name="Sign In").click()
@@ -41,12 +45,14 @@ def login(static_live_server, page: Page, user="test@example.edu", password="cha
     page.get_by_role("button", name="Sign in").click()
 
 
+@pytest.mark.xdist_group("functional")
 def test_home(static_live_server, page: Page):
     """The unauthenticated home page should have the expected content"""
     page.goto(static_live_server.url)
     expect(page).to_have_title("Open Casebooks | H2O")
 
 
+@pytest.mark.xdist_group("functional")
 def test_auth(static_live_server, page: Page):
     """A user with an account should be able to log in"""
     page.goto(static_live_server.url)
@@ -54,13 +60,14 @@ def test_auth(static_live_server, page: Page):
     expect(page).to_have_url(f"{static_live_server.url}/accounts/login/")
 
     page.get_by_role("link", name="Sign In").click()
-    page.get_by_label("Email address*").fill("test@example.edu")
+    page.get_by_label("Email address*").fill("functional-test@example.edu")
     page.get_by_label("Password*").fill("changeme")
     page.get_by_role("button", name="Sign in").click()
     assert page.locator("text=Please enter a correct email address and password*").count() == 0
     expect(page).to_have_url(f"{static_live_server.url}/")
 
 
+@pytest.mark.xdist_group("functional")
 def test_view_casebook(static_live_server, page: Page, login):
     """An authenticated user should be able to view their casebooks in edit mode"""
     page.goto(static_live_server.url)
@@ -69,5 +76,16 @@ def test_view_casebook(static_live_server, page: Page, login):
     page.get_by_role("link", name="First content").click()
 
     expect(page).to_have_url(
-        f"{static_live_server.url}/casebooks/6108-simple-casebook/resources/1-first-content/annotate/"
+        f"{static_live_server.url}/casebooks/1-simple-casebook/resources/1-first-content/annotate/"
     )
+
+
+@pytest.mark.xdist_group("functional")
+def test_pdf_export(static_live_server, page: Page, tmp_path: Path):
+    """The PDF helper function should generate a PDF for a public casebook"""
+    casebook = Casebook.objects.get(title="Public casebook")
+    assert casebook
+    url = static_live_server.url + reverse("printable_all", args=[casebook])
+    output_file = tmp_path / "example.pdf"
+    generate_pdf(url, output_file, page)
+    assert output_file.read_bytes()[:4] == b"%PDF"
