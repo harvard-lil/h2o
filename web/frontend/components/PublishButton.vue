b794047e176af35abd02eccc34dfe257ec37f718
Liza Daly <lizadaly@gmail.com>
2022-12-05 14:01:43 -0500

    Address feedback

--- web/frontend/components/PublishButton.vue
+++ web/frontend/components/PublishButton.vue
@@ -25,27 +25,26 @@
         <div v-if="!publishSuccess">
           <div v-if="publishCheck.isVerifiedProfessor" id="prof-prompt">
             <p>
-              You're almost ready to publish your book! The following elements
-              are optional, but can make your book more discoverable by students
+              You're almost ready to publish your book! The following element
+              is optional, but can make your book more discoverable by students
               and colleagues:
             </p>
             <ul class="fa-ul">
               <li v-if="this.publishCheck.coverImageFlag">
-                <span v-if="this.publishCheck.coverImageExists" class="fa-li"
-                  ><font-awesome-icon icon="fa-solid fa-check fa-fw"
-                /></span>
-                <span v-else class="fa-li"
-                  ><font-awesome-icon icon="fa-solid fa-xmark fa-fw"
-                /></span>
-                Cover Photo
+                <span v-if="this.publishCheck.coverImageExists" class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-check fa-fw" /></span>
+                <span v-else class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-xmark fa-fw"/>
+                </span>
+                Cover image
               </li>
               <li>
-                <span v-if="this.publishCheck.descriptionExists" class="fa-li"
-                  ><font-awesome-icon icon="fa-solid fa-check fa-fw"
-                /></span>
-                <span v-else class="fa-li"
-                  ><font-awesome-icon icon="fa-solid fa-xmark fa-fw"
-                /></span>
+                <span v-if="this.publishCheck.descriptionExists" class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-check fa-fw"/>
+                </span>
+                <span v-else class="fa-li">
+                  <font-awesome-icon icon="fa-solid fa-xmark fa-fw"/>
+                </span>
                 Description
               </li>
             </ul>
@@ -66,7 +65,7 @@
           </p>
           <p>
             Because you are a verified professor, your book will be
-            surfaced in H2O's search page and will appear in web searches. We
+            surfaced in H2O's <a href="/search/">search page</a> and will appear in web searches. We
             also encourage you to share your book with your colleagues, however
             you prefer to reach them. There is a growing community around open
             education for law and we hope you'll help others find and use your
