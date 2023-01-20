e60bf9c6177a8db9b7450e4c07f14b3e895af759
Rebecca Cremona <rcremona@law.harvard.edu>
2023-01-20 11:34:52 -0500

    Fine to use "0" in this project.

--- web/config/settings/settings_base.py
+++ web/config/settings/settings_base.py
@@ -239,7 +239,7 @@ WEBPACK_LOADER = {
 ### CELERY settings ###
 ###
 
-CELERY_BROKER_URL = "redis://redis:6379/1"
+CELERY_BROKER_URL = "redis://redis:6379/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
